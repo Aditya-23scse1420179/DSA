21365ff65ff204b35f34843c934981d7abc9ffe1
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-10-15 11:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (94.16%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -18,7 +18,7 @@ class Solution {
         if (root ==null) return true;
         return isMirror(root.left,root.right);
     }
-    public boolean isMirror(TreeNode left, TreeNode right) {
+    public boolean isMirror(TreeNode left,TreeNode right) {
         if (left==null||right== null) return left==right;
         if (left.val!=right.val) return false;
         return isMirror(left.left,right.right)&&isMirror(left.right,right.left);
