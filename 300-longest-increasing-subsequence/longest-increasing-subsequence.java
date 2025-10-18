03753649a53d395514cee3ab13b2ec4e274431ba
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-10-18 10:34:33 +0530

    Time: 6 ms (88.22%) | Memory: 44.8 MB (32.22%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLIS(int[]nums) {
+        List<Integer> tails =new ArrayList<>();
+        for (int num:nums){
+            int idx = Collections.binarySearch(tails, num);
+            if (idx<0) idx =-(idx+1);
+            if (idx== tails.size()){
+                tails.add(num);
+            } else{
+                tails.set(idx,num);
+            }
+        }
+        return tails.size();
+    }
+}
