f1b790de047627c79447e2d1d235488b8c874534
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-11-24 13:29:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (67.23%) - LeetSync

---
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char findKthBit(int n, int k) {
+        if(n==1)return '0';
+        int len = (1<<n) -1;
+        int half= len/2+1;
+        if(k==half)return '1';
+        if(k<half){
+            return findKthBit(n-1,k);
+        }else {
+            char ch = findKthBit(n-1,len-k+1);
+                if(ch=='0'){
+                    return '1';
+                }else{
+                    return '0';
+                }   
+        }
+    }
+}
