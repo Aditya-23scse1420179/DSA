cbd092d9698f83a584dfdfbf2900fbd4b045d6d8
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-11-26 12:10:18 +0530

    Time: 410 ms (6.98%) | Memory: 46.6 MB (24.60%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -7,17 +7,18 @@ class Solution {
         return ans; 
     }
     public void helper(int[] nums, List<List<Integer>> ans,List<Integer> curr, boolean[] used) {
-        if (curr.size() == nums.length) {
+        if (curr.size() ==nums.length) {
             if(ans.contains(curr))return;
             ans.add(new ArrayList<>(curr));
             return;
         }
-        for (int i = 0; i < nums.length; i++) {
+        for (int i= 0;i< nums.length;i++) {
+            if(i>0&&nums[i]==nums[i-1]&&!used[i-1]) continue;
             if (used[i]) continue;
             curr.add(nums[i]);
             used[i] = true;
-            helper(nums, ans, curr, used);
-            curr.remove(curr.size() - 1);
+            helper(nums, ans, curr,used);
+            curr.remove(curr.size() -1);
             used[i] = false;
         }    
     }
