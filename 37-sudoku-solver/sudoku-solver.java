005e0306016bccad32d56ba0b8266d23dac4cd27
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-11-27 12:13:47 +0530

    Time: 174 ms (69.60%) | Memory: 42.8 MB (17.04%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    public boolean isSafe(char[][] board, int row, int col, char num){
+        // row
+        for(int j=0; j<9; j++){
+            if(board[row][j] == num) return false;
+        }
+        // col
+        for(int i=0; i<9; i++){
+            if(board[i][col] == num) return false;
+        }
+
+        int nRow = row/3 * 3;
+        int nCol = col/3 * 3;
+
+        for(int i=nRow; i<nRow+3; i++){
+            for(int j=nCol; j<nCol+3; j++){
+                if(board[i][j] == num) return false;
+            }
+        }
+        return true;
+    }
+    public void solveSudoku(char[][] board) {
+        solve(board, 0, 0);
+    }
+
+    public boolean solve(char[][] board, int row, int col){
+        if(row == 9) return true;   
+        if(col == 9) return solve(board, row+1, 0);
+        if(board[row][col] != '.') 
+            return solve(board, row, col+1);
+        for(char ch='1'; ch<='9'; ch++){
+            if(isSafe(board, row, col, ch)){
+                board[row][col] = ch;
+                if(solve(board, row, col+1))
+                    return true;
+                board[row][col] = '.';
+            }
+        }
+        return false; 
+    }
+}
