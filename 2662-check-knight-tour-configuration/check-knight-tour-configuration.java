66a2a87408f59707b9e6010bea36a630708d7cc8
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-11-29 00:53:05 +0530

    Time: 1 ms (89.78%) | Memory: 46.2 MB (31.38%) - LeetSync

--- 2662-check-knight-tour-configuration/check-knight-tour-configuration.java
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.java
@@ -1,36 +1,36 @@
 class Solution {
     public boolean checkValidGrid(int[][] grid) {
-        if(grid[0][0]!=0)return false;
+        if(grid[0][0]!=0)return false ;
         return isSafe(grid,0,0,0);
     }
-    public boolean isSafe(int[][]grid,int row,int col,int num){
+    public boolean isSafe(int [][]grid,int row,int col,int num){
         int n = grid.length;
-        if(num==n*n-1)return true;
-        int i,j;
-        i=row-2;
-        j=col+1;
-        if(i>=0&&j<n&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
-        i=row-2;
-        j=col-1;
-        if(i>=0&&j>=0&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
+        if(num == n*n-1) return true;
+        int i=row,j=col;
         i=row-1;
         j=col-2;
         if(i>=0&&j>=0&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
         i=row+1;
         j=col-2;
         if(i<n&&j>=0&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
-        i=row-1;
-        j=col+2;
-        if(i>=0&&j<n&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
-        i=row+1;
-        j=col+2;
-        if(i<n&&j<n&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
-        i=row+2;//2down 1 right
+        i=row-2;
+        j=col-1;
+        if(i>=0&&j>=0&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
+        i=row-2;
         j=col+1;
-        if(i<n&&j<n&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
-        i=row+2;
+        if(i>=0&&j<n&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
+        i=row+2;//
         j=col-1;
         if(i<n&&j>=0&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
+        i=row+2;//
+        j=col+1;
+        if(i<n&&j<n&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
+        i=row-1;//
+        j=col+2;
+        if(i>=0&&j<n&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
+        i=row+1;//
+        j=col+2;
+        if(i<n&&j<n&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
         return false;
     }
 }
