570e0ea3546ce55cfc1a4c95a53ca93dededaaf7
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-11-30 14:56:49 +0530

    Time: 25 ms (96.70%) | Memory: 98.1 MB (50.55%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
@@ -1,26 +1,30 @@
 class Solution {
     public int minSubarray(int[] nums, int p) {
-        long total = 0L;
-        for (int x :nums) total += x;
-        int need = (int)(total % p);
-        if (need ==0) return 0;         
-
         int n = nums.length;
-        Map<Integer,Integer> lastIndex = new HashMap<>();
-        lastIndex.put(0,-1);               
-        int best = Integer.MAX_VALUE;
-        int prefix = 0;          
-        for (int i =0;i < n;i++) {
-            prefix =(prefix+ nums[i]) %p;
-            int want =(prefix- need)% p;
-            if(want <0)want +=p;
-
-            if(lastIndex.containsKey(want)){
-                int j =lastIndex.get(want);
-                best =Math.min(best,i- j);
-            }
-            lastIndex.put(prefix,i);
+        long totalSum = 0;
+        for (int x : nums) {
+            totalSum += x;
         }
-        return (best ==Integer.MAX_VALUE ||best== n)?-1 :best;
+        int requiredRemainder = (int) (totalSum % p);
+        
+        if (requiredRemainder == 0) {
+            return 0; 
+        }
+        Map<Integer, Integer> remainderMap = new HashMap<>();
+        remainderMap.put(0, -1); 
+        
+        int currentPrefixMod = 0;
+        int minLength = n; 
+
+        for (int i = 0; i < n; i++){
+            currentPrefixMod = (currentPrefixMod + nums[i]) % p;
+            int targetRemainder = (currentPrefixMod - requiredRemainder + p) % p;
+            if (remainderMap.containsKey(targetRemainder)){
+                int prevIndex = remainderMap.get(targetRemainder);
+                minLength = Math.min(minLength, i - prevIndex);
+            }
+            remainderMap.put(currentPrefixMod, i);
+        }
+        return (minLength == n) ? -1 : minLength;
     }
 }
