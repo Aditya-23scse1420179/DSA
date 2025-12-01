65b59a30a886f2c9904cd84ae939aea9adb14301
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-01 22:25:21 +0530

    Time: 2 ms (90.41%) | Memory: 45.7 MB (29.68%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,22 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> current = new ArrayList<>();
-        solve(nums, target, 0, current, result);
+        solve(candidates, target, 0, current, result);
         return result;
     }
 
-    private void solve(int[] nums, int target, int index,List<Integer> current, List<List<Integer>> result) {
+    private void solve(int[] candidates, int target, int index,List<Integer> current, List<List<Integer>> result) {
         if (target == 0) {
             result.add(new ArrayList<>(current));
             return;
         }
-        if (target < 0 || index >= nums.length) {
+        if (target < 0 || index >= candidates.length) {
             return;
         }
-        current.add(nums[index]);
-        solve(nums, target - nums[index], index, current, result);
+        current.add(candidates[index]);
+        solve(candidates, target - candidates[index], index, current, result);
         current.remove(current.size() - 1);
-        solve(nums, target, index + 1, current, result);
+        solve(candidates, target, index + 1, current, result);
     }
 }
