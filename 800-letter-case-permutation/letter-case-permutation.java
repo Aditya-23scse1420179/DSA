140a301c3f2ab966ca80e6eda99eb4fb88bd09a5
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-01 20:55:45 +0530

    Time: 13 ms (6.08%) | Memory: 53.3 MB (5.04%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> ans = new ArrayList<>();
+        ans.add("");
+        for(char c : s.toCharArray()){
+            int size = ans.size();
+            if(Character.isDigit(c)){
+                for(int i=0;i<size;i++){
+                    ans.set(i,ans.get(i)+c);
+                }
+            }else{
+                List<String>temp = new ArrayList<>();
+                for(int i=0;i<size;i++){
+                    temp.add(ans.get(i)+Character.toLowerCase(c));
+                    temp.add(ans.get(i)+Character.toUpperCase(c));
+                }
+                ans=temp;
+            }
+        }
+        return ans ;
+    }
+}
