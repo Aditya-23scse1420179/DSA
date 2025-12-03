b4e36d2a28c4a7ac1610c1a877023728b9e4d7bb
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-03 22:02:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (50.39%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -9,9 +9,8 @@ class Solution {
         }
         for(int i=m-2;i>=0;i--){
             for(int j=n-2;j>=0;j--){
-                dp[i][j]=dp[i][j+1]+dp[i+1][j];
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
             }
-            
         }
         return dp[0][0];
     }
