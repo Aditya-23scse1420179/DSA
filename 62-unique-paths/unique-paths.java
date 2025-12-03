ced2547e93289c81fc812d637f08a5cd4ba668df
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-03 10:53:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (10.71%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int [][]dp=new int[m][n];
+        for(int []row:dp)Arrays.fill(row,-1); 
+        return helper(0,0,m,n,dp);
+    }
+    public static int helper(int i,int j,int m,int n,int[][]dp){
+        if(i==m-1||j==n-1){
+            return 1;
+        }
+        if(dp[i][j]!=-1)return dp[i][j];
+        int right=helper(i,j+1,m,n,dp);
+        int down = helper(i+1,j,m,n,dp);
+        return dp[i][j]=right+down;
+    }
+
+}
