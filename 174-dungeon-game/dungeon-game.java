ff789363569efa0642aa61a044ad46eec5571505
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-05 00:24:06 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (55.19%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,29 +1,25 @@
 class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
-        int m = dungeon.length;
-        int n = dungeon[0].length;
-        int[][] dp = new int[m][n];
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
-        return solve(0, 0, dungeon, dp);
+        int m=dungeon.length;
+        int n=dungeon[0].length;
+        int [][]dp=new int [m][n];
+        for(int[] row:dp) Arrays.fill(row,-1);
+        return helper(dungeon,0,0,dp);
     }
-    public int solve(int i, int j, int[][] dungeon , int[][] dp) {
-        int m =dungeon.length;
-        int n = dungeon[0].length;
-        if (i >= m ||j >= n)
-            return Integer.MAX_VALUE;
-        if (i == m -1 &&j ==n -1 ) {
-            if ((dungeon[i][j])<= 0)
-                return Math.abs(dungeon[i][j]) + 1;
+    public int helper (int[][]dungeon,int r,int c,int[][]dp){
+        int m=dungeon.length;
+        int n=dungeon[0].length;
+        if(r>=m||c>=n)return Integer.MAX_VALUE;
+        if (r == m -1 &&c ==n -1 ) {
+            if ((dungeon[r][c])<= 0)
+                return Math.abs(dungeon[r][c]) + 1;
             else
                 return 1;
         }
-        if (dp[i][j]!= -1)
-            return dp[i][j];
-        int right = solve(i, j + 1, dungeon, dp);
-        int down = solve(i + 1, j, dungeon, dp);
-        int need = Math.min(right, down) - dungeon[i][j];
-        return dp[i][j] = need <= 0 ? 1 : need;
-
+        if(dp[r][c]!=-1)return dp[r][c];
+        int right=helper(dungeon,r,c+1,dp);
+        int down= helper(dungeon,r+1,c,dp);
+        int need= Math.min(right,down)-dungeon[r][c];
+        return dp[r][c]=need<=0?1:need;
     }
 }
