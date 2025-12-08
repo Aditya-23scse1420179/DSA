17eed5670558081d694083047b4fd72e33bc0379
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-08 11:38:45 +0530

    Time: 2 ms (63.57%) | Memory: 45.3 MB (54.54%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,29 +1,25 @@
 class Solution {
-    public int calculateMinimumHP(int[][] dungeon) {
-        int m = dungeon.length;
-        int n = dungeon[0].length;
-        Integer[][] dp = new Integer[m][n];//Intger use karo taki -1 na fill karna pade aur niche null se compare hoga.
-        // for (int[] row : dp)
-        //     Arrays.fill(row, -1);
-        return solve(0, 0, dungeon, dp);
-    }
-    public int solve(int i, int j, int[][] dungeon , Integer[][] dp) {
-        int m =dungeon.length;
-        int n = dungeon[0].length;
-        if (i >= m ||j >= n)
-            return Integer.MAX_VALUE;
-        if (i == m -1&&j ==n -1) {
-            if ((dungeon[i][j])<= 0)
-                return Math.abs(dungeon[i][j]) + 1;
-            else
-                return 1;
+    public int calculateMinimumHP(int[][] power) {
+        int m = power.length;
+        int n = power[0].length;
+        int [][] dp = new int[m][n];
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+                if(i == m-1 && j == n-1){
+                    if(power[i][j] > 0){
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.abs(power[i][j]) + 1;
+                    }
+                }
+                else{
+                    int down =(i+1>= m) ? (int)1e9 :dp[i+1][j];
+                    int right = (j+1>=n) ?(int)1e9: dp[i][j+1];
+                    int need = Math.min(down, right) - power[i][j];
+                    dp[i][j] =(need<= 0) ? 1 : need;
+                }
+            }
         }
-        if (dp[i][j]!= null)
-            return dp[i][j];
-        int right = solve(i, j + 1, dungeon, dp);
-        int down = solve(i + 1, j, dungeon, dp);
-        int need = Math.min(right, down) - dungeon[i][j];
-        return dp[i][j] = need <= 0 ? 1 : need;
-
+        return dp[0][0];
     }
 }
