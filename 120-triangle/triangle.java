a0bc9cd6056f2f01a2787714ab14af958c7042ac
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-08 22:19:31 +0530

    Time: 4 ms (55.32%) | Memory: 46.4 MB (15.52%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,20 +1,15 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> t) {
-        int n = t.size();
-        Integer[][] dp = new Integer[n][n];
-
-        // ✅ Base case: last row copy
-        for (int j = 0; j < n; j++) {
-            dp[n-1][j] = t.get(n-1).get(j);
+        int n=t.size();
+        Integer[]dp=new Integer[n];
+        for(int j=0;j<n;j++){
+            dp[j]=t.get(n-1).get(j);
         }
-        for (int i = n-2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                int down = dp[i+1][j];
-                int diag = dp[i+1][j+1];
-                dp[i][j] = Math.min(down, diag) + t.get(i).get(j);
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                dp[j]=Math.min(dp[j],dp[j+1])+t.get(i).get(j);
             }
         }
-
-        return dp[0][0];
+        return dp[0];
     }
 }
