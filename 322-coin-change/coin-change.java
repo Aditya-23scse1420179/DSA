0aa053ce39ff51052d22358c062a226c43fb4646
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-09 23:34:40 +0530

    Time: 15 ms (78.61%) | Memory: 46.7 MB (22.22%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,18 +1,25 @@
 class Solution {
-    public int helper(int []coins,int idx,int amount,Integer[][]dp){
-        if(amount==0)return 0;
-        if(idx==coins.length)return (int)1e9;
-        if(amount<0)return (int)1e9;
-        if(dp[idx][amount]!=null)return dp[idx][amount];
-        int take = 1+helper(coins,idx,amount-coins[idx],dp);
-        int take1 = 1+helper(coins,idx+1,amount-coins[idx],dp);
-        int nottake=helper(coins,idx+1,amount,dp);
-        dp[idx][amount]= Math.min(take,Math.min(nottake,take1));
-        return dp[idx][amount];
-    }
-    public int coinChange(int[] coins, int amount) {
-        Integer[][]dp=new Integer[coins.length][amount+1];
-        int ans=helper(coins,0,amount,dp);
-        return ans>= (int)1e9 ? -1 :ans;
+    public int coinChange(int[] arr, int amount) {
+        int n = arr.length;
+        int INF = (int)1e9;
+        int[][] dp =new int[n][amount + 1];
+        for (int s = 0; s <= amount; s++) {
+            if (s % arr[0] ==0)
+                dp[0][s] = s/arr[0];   
+            else
+                dp[0][s] =(int)1e9;       
+        }
+        for (int i =1;i< n;i++) {
+            for (int s= 0;s <=amount;s++) {
+                int notTake = dp[i-1][s];
+                int take = (int)(1e9);
+                if (s >= arr[i]){
+                    take =1 +dp[i][s-arr[i]];
+                }
+                dp[i][s] =Math.min(take,notTake);
+            }
+        }
+        int ans =dp[n-1][amount];
+        return ans >= (int)1e9?-1 :ans;
     }
 }
