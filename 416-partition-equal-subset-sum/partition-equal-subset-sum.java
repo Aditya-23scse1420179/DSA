9fe5326c54d75c4b628effd8957f4910b0df0d79
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-09 09:27:18 +0530

    Time: 22 ms (89.27%) | Memory: 72.9 MB (18.56%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum =0;
+        for(int x:nums){
+            sum+=x;
+        }
+        if(sum%2!=0)return false;
+        int target = sum/2;
+        Integer[][]dp= new Integer[nums.length][target+1];
+        return helper(target,nums,0,dp);
+    }
+    public boolean helper(int target,int[]nums,int idx,Integer[][]dp){
+        if(target<0)return false;
+        if(target==0)return true;
+        if(idx>=nums.length)return false;
+        if(dp[idx][target]!=null){
+            if(dp[idx][target]==-1){
+                return false;
+            }else{
+                return true;
+            }
+        }
+        boolean res=helper(target-nums[idx],nums,idx+1,dp)||helper(target,nums,idx+1,dp);
+        if(res){
+            dp[idx][target]=1;
+        }else{
+            dp[idx][target]=-1;
+        }
+        return res;
+        
+    }
+}
