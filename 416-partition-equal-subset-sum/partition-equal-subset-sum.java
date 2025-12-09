22f3bb00472ef844147726965508dfeaa049f799
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-09 20:15:29 +0530

    Time: 24 ms (86.82%) | Memory: 72.7 MB (19.72%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,42 +1,22 @@
-class Solution {
-        static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try {
-                java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt");
-                fw.write("000");
-                fw.close();
-            } catch (Exception e) {}
-        }));
-    }
+class Solution{
     public boolean canPartition(int[] nums) {
-
         int sum =0;
         for(int x:nums){
             sum+=x;
         }
         if(sum%2!=0)return false;
         int target = sum/2;
-        Integer[][]dp= new Integer[nums.length][target+1];
+        Boolean [][]dp= new Boolean[nums.length][target+1];
         return helper(target,nums,0,dp);
     }
-    public boolean helper(int target,int[]nums,int idx,Integer[][]dp){
+    public boolean helper(int target,int[]nums,int idx,Boolean[][]dp){
         if(target<0)return false;
         if(target==0)return true;
         if(idx>=nums.length)return false;
         if(dp[idx][target]!=null){
-            if(dp[idx][target]==-1){
-                return false;
-            }else{
-                return true;
-            }
+            return dp[idx][target];
         }
-        boolean res=helper(target-nums[idx],nums,idx+1,dp)||helper(target,nums,idx+1,dp);
-        if(res){
-            dp[idx][target]=1;
-        }else{
-            dp[idx][target]=-1;
-        }
-        return res;
-        
+        dp[idx][target]=helper(target-nums[idx],nums,idx+1,dp)||helper(target,nums,idx+1,dp);
+        return dp[idx][target];
     }
 }
