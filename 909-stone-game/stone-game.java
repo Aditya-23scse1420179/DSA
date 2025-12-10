b954508e6e09f30ff234eba38a96a847ed1b20c7
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-10 11:46:41 +0530

    Time: 9 ms (17.17%) | Memory: 51.3 MB (5.47%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,15 @@
 class Solution {
+    public int helper(int []piles,int i,int j,Integer[][]dp){
+        if(i==j)return piles[i];
+        if(dp[i][j]!=null)return dp[i][j];
+        int alice=piles[i]-helper(piles,i+1,j,dp);
+        int bob = piles[j]-helper(piles,i,j-1,dp);
+        return dp[i][j]= Math.min(alice,bob);
+    }
     public boolean stoneGame(int[] piles) {
-        return true;
+        int i=0;
+        int j=piles.length-1;
+        Integer[][]dp= new Integer[piles.length][piles.length];
+        return helper(piles,i,j,dp)<0;
     }
 }
