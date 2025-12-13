8dd15e20686fe231693189f1c29c67b706891434
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-13 19:38:44 +0530

    Time: 279 ms (81.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,9 @@
+select id,count(*)as num
+from (
+    select requester_id as id from RequestAccepted
+    union all
+    select accepter_id as id from requestAccepted
+)as all_ids
+group by id
+order by num desc
+limit 1;
