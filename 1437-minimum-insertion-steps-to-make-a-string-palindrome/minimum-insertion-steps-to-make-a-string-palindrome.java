afc44df7e7741fd261f4664a0dc5c86d2e39bc7f
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-18 15:35:29 +0530

    Time: 28 ms (35.38%) | Memory: 47.8 MB (12.92%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,19 +1,17 @@
 class Solution {
     public int minInsertions(String s) {
         int n =s.length();
-        int[] dp =new int[n];
-        for (int i =n- 2;i >=0;i--) {
-            int prev =0;
-            for (int j = i+1;j <n;j++) {
-                int temp =dp[j];
-                if (s.charAt(i)== s.charAt(j)) {
-                    dp[j]= prev;
-                } else{
-                    dp[j] =Math.min(dp[j], dp[j-1]) + 1;
+        int [][]dp=new int[n+1][n+1];
+        String sb=new StringBuilder(s).reverse().toString();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==sb.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
-                prev =temp;
             }
         }
-        return dp[n-1];
+        return(n-dp[n][n]);
     }
 }
