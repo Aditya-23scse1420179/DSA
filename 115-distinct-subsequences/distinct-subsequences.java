6653343c527655cd1d80a5b0f4046b212311550d
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-19 21:21:22 +0530

    Time: 33 ms (6.02%) | Memory: 58 MB (8.44%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -3,16 +3,17 @@ class Solution {
         int n=s.length();
         int m=t.length();
         Integer[][]dp=new Integer[n+1][m+1];
-        return helper(s,t,s.length()-1,t.length()-1,dp);
+        return helper(s,t,0,0,dp);
     }
     public static int helper(String s,String t,int i,int j,Integer[][]dp){
-        if(j<0)return 1;
-        if(i<0)return 0;
-        if(i<j)return 0;
+        int n=s.length();
+        int m=t.length();
+        if(j==m)return 1;
+        if(i==n)return 0;
         if(dp[i][j]!=null)return dp[i][j];
         if(s.charAt(i)==t.charAt(j)){
-            return dp[i][j]= helper(s,t,i-1,j-1,dp)+helper(s,t,i-1,j,dp);
+            return dp[i][j]= helper(s,t,i+1,j+1,dp)+helper(s,t,i+1,j,dp);
         }
-        return dp[i][j]=helper(s,t,i-1,j,dp);
+        return dp[i][j]=helper(s,t,i+1,j,dp);
     }
 }
