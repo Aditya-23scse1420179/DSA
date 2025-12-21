1643c6a035f3f8e3865354b21b46600230a796df
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-21 10:48:33 +0530

    Time: 15 ms (77.97%) | Memory: 63.7 MB (8.54%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -4,19 +4,19 @@ class Solution {
         return helper(s,p,s.length(),p.length(),dp);
     }
     public boolean helper(String s,String p,int i,int j,Boolean[][]dp){
-        if(dp[i][j]!=null)return dp[i][j];
-        if(j==0)return dp[i][j]= (i==0);
+        if(j==0)return (i==0);
         if(i==0){
             for(int k=0;k<j;k++){
-                if(p.charAt(k)!='*')return dp[i][j]=false;
-            }return dp[i][j]=true;
+                if(p.charAt(k)!='*')return false;
+            }return true;
         }
+        if(dp[i][j]!=null)return dp[i][j];
         char ch =p.charAt(j-1);
         if(s.charAt(i-1)==ch||ch=='?')return dp[i][j]= helper(s,p,i-1,j-1,dp);
         else if (ch=='*'){
             return dp[i][j]=helper(s,p,i-1,j,dp)||helper(s,p,i,j-1,dp);
         }else{
-            return dp[i][j]=false;
+            return false;
         }
     }
 }
