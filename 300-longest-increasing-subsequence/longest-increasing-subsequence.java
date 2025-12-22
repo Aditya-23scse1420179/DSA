d51a6bc905e1d8b082da016797bbbab4154c0b68
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-22 22:58:38 +0530

    Time: 438 ms (5.02%) | Memory: 273.1 MB (6.86%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,13 +1,13 @@
 class Solution {
-    public int helper(int []nums,int idx,int prev,Integer dp[][]){
-        if(idx==nums.length)return 0;
-        if(dp[idx][prev+1]!=null)return dp[idx][prev+1];
-        int notTake = helper(nums,idx+1,prev,dp);
+    public int helper(int []nums,int curr,int prev,Integer dp[][]){
+        if(curr==nums.length)return 0;
+        if(dp[curr][prev+1]!=null)return dp[curr][prev+1];
+        int notTake = helper(nums,curr+1,prev,dp);
         int take=0;
-        if(prev==-1|| nums[idx]>nums[prev]){
-            take =1+ helper(nums,idx+1,idx,dp);
+        if(prev==-1|| nums[curr]>nums[prev]){
+            take =1+ helper(nums,curr+1,curr,dp);
         }
-        return dp[idx][prev+1]=Math.max(notTake,take);
+        return dp[curr][prev+1]=Math.max(notTake,take);
     }
     public int lengthOfLIS(int[] nums) {
         Integer dp[][]=new Integer[nums.length][nums.length+1];
