7d1c01d0cb41637922909b395230bae90e2ed8d0
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-22 15:03:57 +0530

    Time: 451 ms (5.02%) | Memory: 272.9 MB (6.86%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,15 +1,23 @@
 class Solution {
-    public int lengthOfLIS(int[]nums) {
-        List<Integer> tails =new ArrayList<>();
-        for (int num:nums){
-            int idx = Collections.binarySearch(tails, num);
-            if (idx<0) idx =-(idx+1);
-            if (idx== tails.size()){
-                tails.add(num);
-            } else{
-                tails.set(idx,num);
-            }
+    int[] nums;
+    int n;
+    Integer[][] dp;
+    public int lengthOfLIS(int[] nums) {
+        this.nums = nums;
+        this.n = nums.length;
+        dp = new Integer[n][n + 1]; 
+        return dfs(0, -1);
+    }
+    private int dfs(int i, int prevIndex) {
+        if (i == n) return 0;
+        if (dp[i][prevIndex + 1] != null)
+            return dp[i][prevIndex + 1];
+        // not take
+        int res = dfs(i + 1, prevIndex);
+        // take
+        if (prevIndex == -1 || nums[i] > nums[prevIndex]) {
+            res = Math.max(res, 1 + dfs(i + 1, i));
         }
-        return tails.size();
+        return dp[i][prevIndex + 1] = res;
     }
 }
