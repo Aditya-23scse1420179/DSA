380f56fcf05c500f3afbf372dd8dd9e88c95cc41
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2025-12-24 09:09:23 +0530

    Time: 17 ms (54.66%) | Memory: 45.4 MB (16.33%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -1,32 +1,35 @@
-public class Solution {
+class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        int n=nums.length;
-        if (n==0) return new ArrayList<>();
         Arrays.sort(nums);
-        int[] dp = new int[n];       
-        int[] prev = new int[n];    
+        int dp[]=new int[nums.length];
+        int parent[]= new int[nums.length];
         Arrays.fill(dp,1);
-        Arrays.fill(prev,-1);
-        int maxIdx = 0;
-        for (int i =1;i < n; i++) {
-            for (int j= 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0) {
-                    if (dp[j]+ 1>dp[i]) {
-                        dp[i]=dp[j] + 1;
-                        prev[i]=j;
+        Arrays.fill(parent,-1);
+        int max=-1;
+        for(int curr =1;curr<nums.length;curr++){
+            for(int prev=0;prev<curr;prev++){
+                if(nums[curr]%nums[prev]==0){
+                    if(dp[prev]+1>dp[curr]){
+                        dp[curr]=dp[prev]+1;
+                        parent[curr]=prev;
+                        max=Math.max(max,dp[curr]);
                     }
                 }
             }
-            if (dp[i]>dp[maxIdx]) {
-                maxIdx=i;
+            
+        }
+        int lastIdx=0;
+        for(int i=0;i<dp.length;i++){
+            if(dp[i]==max){
+                lastIdx=i;
+                break;
             }
         }
-        List<Integer> result = new ArrayList<>();
-        while (maxIdx>=0) {
-            result.add(nums[maxIdx]);
-            maxIdx =prev[maxIdx];
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=lastIdx;i!=-1;i=parent[i]){
+            list.add(nums[i]);
         }
-        Collections.reverse(result);
-        return result;
+        Collections.reverse(list);
+        return list;
     }
 }
