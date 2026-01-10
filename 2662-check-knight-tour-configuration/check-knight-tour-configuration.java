03d965f636af739d680291647b3be40b466d5771
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-10 23:10:42 +0530

    Time: 1 ms (96.45%) | Memory: 46.5 MB (28.00%) - LeetSync

--- 2662-check-knight-tour-configuration/check-knight-tour-configuration.java
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.java
@@ -1,36 +1,37 @@
 class Solution {
     public boolean checkValidGrid(int[][] grid) {
-        if(grid[0][0]!=0)return false ;
-        return isSafe(grid,0,0,0);
+        if(grid[0][0]!=0)return false;
+        return helper(grid,0,0,0);
+        
     }
-    public boolean isSafe(int [][]grid,int row,int col,int num){
-        int n = grid.length;
-        if(num == n*n-1) return true;
+    public boolean helper(int[][]grid,int row,int col,int num){
+        int n =grid.length;
+        if(num==n*n-1)return true;
         int i=row,j=col;
+        i=row+2;
+        j=col-1;
+        if(i<n&&j>=0&&grid[i][j]==num+1)return helper(grid,i,j,num+1);
+        i=row+2;
+        j=col+1;
+        if(i<n&&j<n&&grid[i][j]==num+1)return helper(grid,i,j,num+1);
+        i=row+1;//
+        j=col-2;
+        if(i<n&&j>=0&&grid[i][j]==num+1)return helper(grid,i,j,num+1);
+        i=row+1;
+        j=col+2;
+        if(i<n&&j<n&&grid[i][j]==num+1)return helper(grid,i,j,num+1);
+        i=row-1;
+        j=col+2;
+        if(i>=0&&j<n&&grid[i][j]==num+1)return helper(grid,i,j,num+1);
+        i=row-2;
+        j=col-1;
+        if(i>=0&&j>=0&&grid[i][j]==num+1)return helper(grid,i,j,num+1);
         i=row-1;
         j=col-2;
-        if(i>=0&&j>=0&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
-        i=row+1;
-        j=col-2;// yahan baar baar galti ho rahi 
-        if(i<n&&j>=0&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
-        i=row-2;
-        j=col-1;
-        if(i>=0&&j>=0&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
+        if(i>=0&&j>=0&&grid[i][j]==num+1)return helper(grid,i,j,num+1);
         i=row-2;
         j=col+1;
-        if(i>=0&&j<n&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
-        i=row+2;//
-        j=col-1;
-        if(i<n&&j>=0&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
-        i=row+2;//
-        j=col+1;
-        if(i<n&&j<n&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
-        i=row-1;//
-        j=col+2;
-        if(i>=0&&j<n&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
-        i=row+1;//
-        j=col+2;
-        if(i<n&&j<n&&grid[i][j]==num+1)return isSafe(grid,i,j,num+1);
+        if(i>=0&&j<n&&grid[i][j]==num+1)return helper(grid,i,j,num+1);
         return false;
     }
 }
