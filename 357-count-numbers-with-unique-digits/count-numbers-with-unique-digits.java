d69fcec3f12ecdba32c532456daccf21daad3712
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-10 11:55:50 +0530

    Time: 1 ms (20.11%) | Memory: 43.4 MB (12.55%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -1,22 +1,24 @@
 class Solution {
     public int countNumbersWithUniqueDigits(int n) {
         String s="9".repeat(n);
-        return helper(s,0,1,1,0);
-    }public int helper(String s,int idx,int tight,int lz,int mask){
+        Integer[][][][]dp=new Integer[s.length()+1][3][2][1024];
+        return helper(s,0,1,1,0,dp);
+    }public int helper(String s,int idx,int tight,int lz,int mask,Integer[][][][]dp){
         if(idx==s.length())return 1;
+        if(dp[idx][tight][lz][mask]!=null)return dp[idx][tight][lz][mask];
         int lb=0;
         int ub=(tight==1)?s.charAt(idx)-'0':9;
         int res=0;
         for(int digit=lb;digit<=ub;digit++){
             int newtight=(tight==1&&digit==ub)?1:0;
             if(lz==1&&digit==0){
-                res+=helper(s,idx+1,newtight,1,mask);
+                res+=helper(s,idx+1,newtight,1,mask,dp);
             }else{
                 if((mask&(1<<digit))!=0) continue;
                 int newmask=mask|(1<<(digit));
-                res+=helper(s,idx+1,newtight,0,newmask);
+                res+=helper(s,idx+1,newtight,0,newmask,dp);
             }
         }
-        return res;
+        return dp[idx][tight][lz][mask]= res;
     }
 }
