89787f1adb18167e9f4fc128a60f86f4d745712d
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-11 12:50:32 +0530

    Time: 51 ms (31.55%) | Memory: 101.4 MB (38.59%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,46 +1,32 @@
 class Solution {
-    public int search(List<Integer> arr, int k) {
-        int low =0;int high=arr.size()-1;
-        int ans=-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(arr.get(mid)==k)return mid;
-            if(arr.get(mid)<k){
-                low=mid+1;
-            }else{
-                high=mid-1;
-            }
-        }
-        return low;
-        
-    }
-    // public int helper(int[][]pairs,int idx,int prev){
-    //     if(idx==pairs.length)return 0;
-    //     int notTake = helper(pairs,idx+1,prev);
-    //     int take = 0;
-    //     if(prev==-1 || pairs[prev][1]<pairs[idx][1]){
-    //         take = 1+helper(pairs,idx+1,idx);
-    //     }
-    //     return Math.max(notTake,take);
-    // }
     public int maxEnvelopes(int[][] e) {
         Arrays.sort(e,(a,b)->{
             if(a[0]!=b[0])return a[0]-b[0];
             else return b[1]-a[1];
         });
-        int nums[]=new int[e.length];
+        List<Integer>ans=new ArrayList<>();
         for(int i=0;i<e.length;i++){
-            nums[i]=e[i][1];
-        }
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(nums[0]);
-        for(int i=1;i<nums.length;i++){
-            if(arr.get(arr.size()-1)<nums[i]){
-                arr.add(nums[i]);
+            int h=e[i][1];
+            int idx=lb(ans,h);
+            if(idx==ans.size()){
+                ans.add(h);
             }else{
-                arr.set(search(arr,nums[i]),nums[i]);
+                ans.set(idx,h);
             }
         }
-        return arr.size();
+        return ans.size();
+    }
+    public int lb(List<Integer>ans,int target){
+        int low=0;
+        int high=ans.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(ans.get(mid)>=target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
     }
 }
