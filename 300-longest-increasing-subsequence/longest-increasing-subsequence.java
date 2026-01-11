1fbb7dcd073a8c616ea37268d0f4998faf8a0710
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-11 14:54:26 +0530

    Time: 5 ms (89.65%) | Memory: 46.5 MB (28.35%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,16 +1,26 @@
 class Solution {
-    public int helper(int []nums,int curr,int prev,Integer dp[][]){
-        if(curr==nums.length)return 0;
-        if(dp[curr][prev+1]!=null)return dp[curr][prev+1];
-        int notTake = helper(nums,curr+1,prev,dp);
-        int take=0;
-        if(prev==-1|| nums[curr]>nums[prev]){
-            take =1+ helper(nums,curr+1,curr,dp);
-        }
-        return dp[curr][prev+1]=Math.max(notTake,take);
-    }
     public int lengthOfLIS(int[] nums) {
-        Integer dp[][]=new Integer[nums.length][nums.length+1];
-        return helper(nums,0,-1,dp);
+        List<Integer>ans=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            int idx=lb(ans,nums[i]);
+            if(idx==ans.size()){
+                ans.add(nums[i]);
+            }else{
+                ans.set(idx,nums[i]);
+            }
+        }
+        return ans.size();
+    }public int lb(List<Integer>ans,int target){
+        int low=0;
+        int high=ans.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(ans.get(mid)>=target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
     }
 }
