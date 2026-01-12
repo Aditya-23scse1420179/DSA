9dc004ac05a32c2589a46d0f9b2e08888fc0292e
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-12 20:18:05 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (72.70%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,16 +1,14 @@
 class Solution {
-    public double myPow(double x, long n) { 
+    public double myPow(double x, long n) {
         if(n<0){
             x=1/x;
             n=-n;
         }
-        return power(x,n);
-    }
-    double power(double x, long n){
-        if(n==0)return 1.0;
-        double half=power(x,n/2);
+        return helper(x,n);
+    }public static double helper(double x,long n){
+        if(n==0)return 1;
+        double half=helper(x,n/2);
         if(n%2==0)return half*half;
         else return half*half*x;
-
     }
 }
