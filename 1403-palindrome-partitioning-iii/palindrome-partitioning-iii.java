7fdaf51bfa9263f1a4199cbbec713d4ab4682370
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-13 23:30:26 +0530

    Time: 22 ms (13.14%) | Memory: 43.1 MB (40.15%) - LeetSync

--- 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.java
+++ 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.java
@@ -1,26 +1,27 @@
 class Solution {
     public int palindromePartition(String s, int k) {
-        Integer[][]dp=new Integer[s.length()][k+1];
-        return helper(s,0,k,dp);
-    }public int helper(String s,int idx,int k,Integer[][]dp){
+        Integer[][]dp=new Integer[s.length()+1][s.length()+1
+        ];
+        return helper(0,s,k,dp);
+    }public static int helper(int idx,String s,int k,Integer[][]dp){
         if(idx==s.length()&&k==0)return 0;
         if(idx==s.length()||k==0)return (int)1e9;
         if(dp[idx][k]!=null)return dp[idx][k];
         int min=(int)1e9;
         for(int i=idx;i<s.length();i++){
-            int a=ispal(idx,i,s)+helper(s,i+1,k-1,dp);
-            min=Math.min(min,a);
+            int cost=ispal(idx,i,s)+helper(i+1,s,k-1,dp);
+            min=Math.min(min,cost);
         }
         return dp[idx][k]=min;
-
-    }public int ispal(int i,int j,String s){
-        int count=0;
+    }public static int ispal(int i,int j,String s){
+        int changes=0;
         while(i<j){
             if(s.charAt(i)!=s.charAt(j)){
-                count++;
+                changes++;
             }
-            i++;j--;
-        }    
-        return count;
+            i++;
+            j--;
+        }
+        return changes;
     }
-}
+}    
