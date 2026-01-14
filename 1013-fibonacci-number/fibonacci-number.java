701198bbaee15794bca3d3e91d02cea3e4703c85
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-14 18:55:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.76%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,13 +1,13 @@
 class Solution {
     public int fib(int n) {
-        if(n<2)return n;
-        int prev1=1;
+        if(n<=1)return n;
         int prev2=0;
+        int prev=1;
         for(int i=2;i<=n;i++){
-            int curr=prev1+prev2;
-            prev2=prev1;
-            prev1=curr;
+            int curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
         }
-        return prev1;
+        return prev;   
     }
 }
