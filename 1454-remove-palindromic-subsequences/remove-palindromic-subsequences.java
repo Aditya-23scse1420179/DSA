e7d2e9ff04da84fbc4bc6c4f968ae1d27d713845
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-14 16:00:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (31.28%) - LeetSync

--- 1454-remove-palindromic-subsequences/remove-palindromic-subsequences.java
+++ 1454-remove-palindromic-subsequences/remove-palindromic-subsequences.java
@@ -1,18 +1,12 @@
 class Solution {
     public int removePalindromeSub(String s) {
-        int[][]dp=new int[s.length()+1][s.length()+1];
-        String r= new StringBuilder(s).reverse().toString();
-        for(int i=1;i<=s.length();i++){
-            for(int j=1;j<=s.length();j++){
-                if(s.charAt(i-1)==r.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j))return 2;
+            i++;
+            j--;
         }
-        int lps= dp[s.length()][s.length()];
-        if(lps==s.length())return 1;
-        else return 2;
+        return 1;
     }
 }
