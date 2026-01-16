22241341e6b44046f8127ca4dfb996168f9bc3ae
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-17 00:58:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (58.24%) - LeetSync

--- 938-numbers-at-most-n-given-digit-set/numbers-at-most-n-given-digit-set.java
+++ 938-numbers-at-most-n-given-digit-set/numbers-at-most-n-given-digit-set.java
@@ -1,15 +1,15 @@
 class Solution {
     public int atMostNGivenDigitSet(String[] digits, int n) {
-        HashSet<Integer>set=new HashSet<>();
+        int mask=0;
         for(String x:digits){
-            set.add(Integer.parseInt(x));
+            mask|=(1<<Integer.parseInt(x));
         }
         String s=String.valueOf(n);
         Integer[][][]dp=new Integer[s.length()+1][2][2];
-        int ans =solve(s,0,1,1,set,dp);
+        int ans =solve(s,0,1,1,mask,dp);
         return ans-1;
         
-    }public int solve(String s,int idx,int tight,int lz,HashSet<Integer>set,Integer[][][]dp){
+    }public int solve(String s,int idx,int tight,int lz,int mask,Integer[][][]dp){
         if(idx==s.length())return 1;
         if(dp[idx][tight][lz]!=null)return dp[idx][tight][lz];
         int lb=0;
@@ -18,10 +18,10 @@ class Solution {
         for(int digit=lb;digit<=ub;digit++){
             int newtight=(tight==1&&digit==ub)?1:0;
             if(lz==1&&digit==0){
-                res+=solve(s,idx+1,newtight,lz,set,dp);
+                res+=solve(s,idx+1,newtight,lz,mask,dp);
             }else{
-                if(!set.contains(digit))continue;
-                res+=solve(s,idx+1,newtight,0,set,dp);
+                if(((mask>>digit)&1)==0)continue;
+                res+=solve(s,idx+1,newtight,0,mask,dp);
             }    
         }
         return dp[idx][tight][lz]=res;
