83d040f3f561e1c48a55d66e16dc750a583f79a6
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-16 11:49:16 +0530

    Time: 0 ms (100.00%) | Memory: 61 MB (93.46%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -2,8 +2,8 @@ class Solution {
     public void rotate(int[] nums, int k) {
         k=k%nums.length;
         helper(nums,0,nums.length-1);
-        helper(nums,0,k-1);
         helper(nums,k,nums.length-1);
+        helper(nums,0,k-1);
         
     }
     public void helper(int[]nums,int left,int right){
