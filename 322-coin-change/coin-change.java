2965bd29dfb1e8cadbc4800c0a97aa87a4ff0163
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-17 00:48:26 +0530

    Time: 36 ms (13.01%) | Memory: 48.6 MB (5.09%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -8,9 +8,8 @@ class Solution {
         if(amount<0)return (int)1e9;
         if(amount==0)return 0;
         if(dp[i][amount]!=null)return dp[i][amount];
-        int take=1+helper(i+1,amount-coins[i],coins,dp);
-        int take1=1+helper(i,amount-coins[i],coins,dp);
+        int take=1+helper(i,amount-coins[i],coins,dp);
         int ntake=helper(i+1,amount,coins,dp);
-        return dp[i][amount]=Math.min(take,Math.min(take1,ntake));
+        return dp[i][amount]=Math.min(take,ntake);
     }
 }
