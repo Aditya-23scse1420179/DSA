ab86a0fe2120bf60cf35c785889f82b1ec6a6360
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-17 00:26:32 +0530

    Time: 11 ms (62.91%) | Memory: 57 MB (34.89%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,11 +1,9 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int[][]dp=new int[coins.length+1][amount+1];
-        for (int i = 0; i <= coins.length; i++) {
-            dp[i][0] = 1;
-        }
+        dp[0][0]=1;
         for(int i=1;i<=coins.length;i++){
-            for(int j=1;j<=amount;j++){
+            for(int j=0;j<=amount;j++){
                 int ntake=dp[i-1][j];
                 int take=0;
                 if(coins[i-1]<=j){
