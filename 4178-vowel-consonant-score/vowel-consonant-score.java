698d5e34eda3d7faba490f9cfe6409e546261198
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-18 18:58:20 +0530

    Time: 1 ms (99.95%) | Memory: 43.8 MB (27.26%) - LeetSync

--- 4178-vowel-consonant-score/vowel-consonant-score.java
+++ 4178-vowel-consonant-score/vowel-consonant-score.java
@@ -2,7 +2,7 @@ class Solution {
     public int vowelConsonantScore(String s) {
         int v=0,c=0;
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)>='a'&&s.charAt(i)<='z'){
+            if(s.charAt(i)>=97&&s.charAt(i)<=122){
                 if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
                     v++;
                 }else{
