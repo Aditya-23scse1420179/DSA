e1c855df7ae6d8ba6d428cbf3a8b1c72118598a3
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-22 00:14:52 +0530

    Time: 15 ms (24.77%) | Memory: 46.6 MB (19.56%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length())return false;
-        int []freq= new int[26];
-        for(int i=0;i<s.length();i++){
-            freq[s.charAt(i)-'a']++;
-            freq[t.charAt(i)-'a']--;
+        Map<Character,Integer>map=new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        for(int count:freq){
-            if(count!=0)return false;
+        for(char c:t.toCharArray()){
+            if(!map.containsKey(c))return false;
+            map.put(c,map.get(c)-1);
+            if(map.get(c)<0)return false;
         }
         return true;
     }
