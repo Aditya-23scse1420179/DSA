58b3f27e22be704067ccacaf905063b12783c719
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-01-27 18:54:02 +0530

    Time: 16 ms (66.40%) | Memory: 47.1 MB (29.38%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,23 +2,21 @@ class Solution {
     public int coinChange(int[] arr, int amount) {
         int n = arr.length;
         int INF = (int)1e9;
-        int[][] dp =new int[n][amount + 1];
-        for (int s = 0; s <= amount; s++) {
-            if (s % arr[0] ==0)
-                dp[0][s] = s/arr[0];   
-            else
-                dp[0][s] =(int)1e9;       
+        int[][] dp =new int[n+1][amount + 1];
+        for (int s = 1; s <= amount; s++) {
+            dp[0][s]=INF;
         }
-        for (int i =1;i< n;i++) {
+        dp[0][0]=0;
+        for (int i =1;i<= n;i++) {
             for (int s= 0;s <=amount;s++) {
                 int notTake = dp[i-1][s];
                 int take = (int)(1e9);
-                if (s >= arr[i]){
-                    take =1 +dp[i][s-arr[i]];
+                if (s >= arr[i-1]){
+                    take =1 +dp[i][s-arr[i-1]];
                 }
                 dp[i][s] =Math.min(take,notTake);
             }
         }
-        return dp[n-1][amount] >= (int)1e9?-1 :dp[n-1][amount];
+        return dp[n][amount]>=(int)1e9?-1 :dp[n][amount];
     }
 }
