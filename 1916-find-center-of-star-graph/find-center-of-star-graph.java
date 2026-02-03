3b3b9386b22e7ee8e89f51e11c77ac7b5310cffa
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-03 13:46:01 +0530

    Time: 8 ms (21.45%) | Memory: 118.5 MB (33.73%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,16 +1,23 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        int []freq=new int [edges.length+2];
-        for(int i=0;i<edges.length;i++){
-            freq[edges[i][0]]++;
-            freq[edges[i][1]]++;
-        }
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            int n=edges.length;
+            int e=edges[0].length;
+	        for(int i=0; i<=n+1; i++){
+        	    adj.add(new ArrayList<>());
+        	}
+        	for(int i=0; i<e; i++){
+            		int u = edges[i][0];
+            		int v = edges[i][1];
+            		adj.get(u).add(v);
+            		adj.get(v).add(u);
+        	}
         int max=0;
-        int maxfreq=0;
-        for(int i=0;i<freq.length;i++){
-            if(freq[i]>maxfreq){
-                maxfreq=freq[i];
-                max=i;
+        int maxSize=0;
+        for(int i=0 ; i<adj.size() ; i++){
+            if(adj.get(i).size() > maxSize){
+                maxSize = adj.get(i).size();
+                max = i;
             }
         }
         return max;
