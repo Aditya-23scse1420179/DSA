16fd95915a8cc590690eb8957690ae669a1d4016
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-04 12:16:08 +0530

    Time: 1 ms (84.66%) | Memory: 45.9 MB (98.32%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,21 +1,29 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         boolean []visit=new boolean[rooms.size()];
-        Queue<Integer>q=new LinkedList<>();
-        visit[0]=true;
-        q.offer(0);
-        while(!q.isEmpty()){
-            int node=q.remove();
-            for(int i:rooms.get(node)){
-                if(visit[i]==false){
-                    visit[i]=true;
-                    q.offer(i);
-                }
-            }
-        }
+        // Queue<Integer>q=new LinkedList<>();
+        // visit[0]=true;
+        // q.offer(0);
+        // while(!q.isEmpty()){
+        //     int node=q.remove();
+        //     for(int i:rooms.get(node)){
+        //         if(visit[i]==false){
+        //             visit[i]=true;
+        //             q.offer(i);
+        //         }
+        //     }
+        // }
+        dfs(rooms,0,visit);
         for(boolean b:visit){
             if(b==false)return false;
         }
         return true;
+    }public void dfs(List<List<Integer>>rooms,int src,boolean[]visit){
+        visit[src]=true;
+        for(int a:rooms.get(src)){
+            if(visit[a]==false){
+                dfs(rooms,a,visit);
+            }
+        }
     }
 }
