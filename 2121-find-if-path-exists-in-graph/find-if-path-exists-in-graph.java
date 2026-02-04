8cf84882c63283a07994e32babf161bd0d5ec9f2
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-04 19:30:37 +0530

    Time: 107 ms (59.29%) | Memory: 287 MB (30.70%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,37 +1,29 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>>list=new ArrayList<>();
-        int e=edges.length;
+        ArrayList<ArrayList<Integer>>list=new ArrayList<>();
         for(int i=0;i<=n+1;i++){
             list.add(new ArrayList<>());
         }
-        for(int i=0;i<e;i++){
+        for(int i=0;i<edges.length;i++){
             int u=edges[i][0];
             int v=edges[i][1];
             list.get(u).add(v);
             list.get(v).add(u);
         }
+        Queue<Integer>q=new LinkedList<>();
         boolean []visit=new boolean [n];
-        // Queue<Integer>q=new LinkedList<>();
-        // visit[source]=true;
-        // q.offer(source);
-        // while(!q.isEmpty()){
-        //     int node=q.remove();
-        //     for(int i:list.get(node)){
-        //         if(visit[i]==false){
-        //             visit[i]=true;
-        //             q.offer(i);
-        //         }
-        //     }
-        // }
-        dfs(list,source,visit);
-        return visit[destination];
-    }public void dfs(List<List<Integer>>list,int src,boolean[]visit){
-        visit[src]=true;
-        for(int a:list.get(src)){
-            if(visit[a]==false){
-                dfs(list,a,visit);
+        visit[source]=true;
+        q.offer(source);
+        while(!q.isEmpty()){
+            int node=q.remove();
+            for(int a:list.get(node)){
+                if(visit[a]==false){
+                    visit[a]=true;
+                    q.offer(a);
+                }
             }
         }
+        if(visit[destination]==true)return true;
+        return false;
     }
 }
