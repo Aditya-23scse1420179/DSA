fbfdfbdb7f1b2b8a3069c89bb99701a0753eb409
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-06 00:42:27 +0530

    Time: 3 ms (43.82%) | Memory: 46.3 MB (93.37%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.java
+++ 1380-number-of-closed-islands/number-of-closed-islands.java
@@ -1,34 +1,37 @@
 class Solution {
-    static int[]dr={-1,1,0,0};
-    static int[]dc={0,0,1,-1};
-    static int rl;
-    static int cl;
+    static int dr [] = {-1 , 1 , 0 ,0};
+    static int dc [] = {0 , 0 , -1 , 1}; 
+    static int rowL;
+    static int colL;
     public int closedIsland(int[][] grid) {
-        rl=grid.length;
-        cl=grid[0].length;
-        boolean[][]visit=new boolean[rl][cl];
-        for(int i=0;i<rl;i++){
-            if(grid[i][0]==0)dfs(i,0,grid,visit);
-            if(grid[i][cl-1]==0)dfs(i,cl-1,grid,visit);
-        }for(int j=0;j<cl;j++){
-            if(grid[0][j]==0)dfs(0,j,grid,visit);
-            if(grid[rl-1][j]==0)dfs(rl-1,j,grid,visit);
+        rowL = grid.length;
+        colL = grid[0].length;
+        boolean [][] visited = new boolean [rowL][colL];
+        
+        for(int i = 0 ; i < rowL ; i++){
+            if(grid[i][0]==0)dfs(i,0,visited,grid);
+            if(grid[i][colL-1] == 0) dfs(i , colL-1 , visited , grid);
+            
+        }for(int j=0;j<colL;j++){
+            if(grid[0][j]==0)dfs(0,j,visited,grid);
+            if(grid[rowL-1][j]==0)dfs(rowL-1,j,visited,grid);
         }int count=0;
-        for(int i=0;i<rl;i++){
-            for(int j=0;j<cl;j++){
-                if(grid[i][j]==0&&!visit[i][j]){
-                    dfs(i,j,grid,visit);
+        for(int i=0;i<rowL;i++){
+            for(int j=0;j<colL;j++){
+                if(grid[i][j]==0&&!visited[i][j]){
+                    dfs(i,j,visited,grid);
                     count++;
                 }
             }
         }
         return count;
-    }public void dfs(int r,int c,int[][]grid,boolean[][]visit){
-        if(r<0||c<0||r>=rl||c>=cl||grid[r][c]==1)return;
-        if(visit[r][c]==true)return;
-        visit[r][c]=true;
-        for(int i=0;i<4;i++){
-            dfs(r+dr[i],c+dc[i],grid,visit);
+    }
+    public void dfs(int r , int c , boolean[][] visited , int [][] grid ){
+        if(r <0 || c < 0 || r >= rowL || c >= colL || grid[r][c] == 1) return ;
+        if(visited[r][c]==true)return;
+        visited[r][c]=true;
+        for(int i = 0 ; i < 4 ; i++){
+            dfs(r+dr[i] , c + dc[i] , visited , grid);
         }
     }
 }
