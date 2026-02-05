1f43d2c1afef4faebc774be06807f670cbda57dc
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-06 01:04:28 +0530

    Time: 8 ms (72.93%) | Memory: 60.1 MB (54.95%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,61 +1,43 @@
 class Solution {
-    static class Pair {
-        int r, c;
-        Pair(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-    }
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
+    static int dr[] = { -1, 1, 0, 0 };
+    static int dc[] = { 0, 0, -1, 1 };
+    static int rowL;
+    static int colL;
+
     public int numEnclaves(int[][] grid) {
-        int rl = grid.length;
-        int cl = grid[0].length;
-        boolean[][] visit = new boolean[rl][cl];
-        Queue<Pair> q = new LinkedList<>();
-        // Add boundary land cells
-        for (int i = 0; i < rl; i++) {
-            if (grid[i][0] == 1) {
-                q.add(new Pair(i, 0));
-                visit[i][0] = true;
-            }
-            if (grid[i][cl - 1] == 1) {
-                q.add(new Pair(i, cl - 1));
-                visit[i][cl - 1] = true;
-            }
+        rowL = grid.length;
+        colL = grid[0].length;
+        boolean [][] visited = new boolean[rowL][colL];
+        
+        for(int i = 0 ; i < rowL ; i++ ){
+            if(grid[i][0] == 1) dfs(i , 0 , visited , grid);
+            if(grid[i][colL-1] == 1) dfs(i , colL-1 , visited , grid);
         }
-        for (int j = 0; j < cl; j++) {
-            if (grid[0][j] == 1) {
-                q.add(new Pair(0, j));
-                visit[0][j] = true;
-            }
-            if (grid[rl - 1][j] == 1){
-                q.add(new Pair(rl - 1, j));
-                visit[rl - 1][j] = true;
-            }
-        }
-        // BFS to mark boundary-connected land
-        while (!q.isEmpty()) {
-            Pair cur = q.poll();
-            for (int k = 0; k < 4; k++) {
-                int nr = cur.r + dr[k];
-                int nc = cur.c + dc[k];
-                if (nr >= 0 && nc >= 0 && nr < rl && nc < cl &&
-                    grid[nr][nc] == 1 && !visit[nr][nc]) {
-                    visit[nr][nc] = true;
-                    q.add(new Pair(nr, nc));
-                }
-            }
-        }
-        // Count enclaves (land not visited)
-        int count = 0;
-        for (int i = 0; i < rl; i++) {
-            for (int j = 0; j < cl; j++) {
-                if (grid[i][j] == 1 && !visit[i][j]) {
+        
+        for(int j = 0 ; j < colL;j++){
+            if(grid[0][j] == 1) dfs(0 , j , visited , grid);
+            if(grid[rowL-1][j] == 1) dfs(rowL-1 , j , visited ,grid);
+           
+        }int count=0;
+        for(int i=0;i<rowL;i++){
+            for(int j=0;j<colL;j++){
+                if(grid[i][j]==1&&!visited[i][j]){
                     count++;
                 }
             }
         }
         return count;
+        
+    }
+
+    public void dfs(int r, int c, boolean[][] visited, int[][] grid) {
+        if (r < 0 || c < 0 | r >= rowL || c >= colL || grid[r][c] == 0)
+            return;
+        if (visited[r][c] == true)
+            return;
+        visited[r][c] = true;
+        for (int i = 0; i < 4; i++) {
+            dfs(r + dr[i], c + dc[i], visited, grid);
+        }
     }
 }
