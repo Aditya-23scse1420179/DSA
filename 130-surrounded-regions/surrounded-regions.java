e05cb40d4a9c3fa6b52893b867befc06d7bfb498
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-05 14:02:03 +0530

    Time: 2 ms (82.64%) | Memory: 47.8 MB (69.90%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,6 +1,6 @@
 class Solution {
-    static int[]dr={-1,1,0,0};
-    static int[]dc={0,0,1,-1};
+    static int[] dr={-1,1,0,0};
+    static int[] dc={0,0,1,-1};
     static int rl;
     static int cl;
     public void solve(char[][] board) {
@@ -8,24 +8,19 @@ class Solution {
         cl=board[0].length;
         boolean[][]visit=new boolean[rl][cl];
         for(int i=0;i<rl;i++){
-            if(board[i][0]=='O') dfs(i,0,board,visit);
-            if(board[i][cl-1]=='O') dfs(i,cl-1,board,visit);
-        }
-        for(int j=0;j<cl;j++){
+            if(board[i][0]=='O')dfs(i,0,board,visit);
+            if(board[i][cl-1]=='O')dfs(i,cl-1,board,visit);
+        }for(int j=0;j<cl;j++){
             if(board[0][j]=='O')dfs(0,j,board,visit);
             if(board[rl-1][j]=='O')dfs(rl-1,j,board,visit);
-        }
-        for (int i=0;i<rl;i++){
-            for (int j=0; j< cl; j++){
-                if(board[i][j]=='O'&&!visit[i][j]) {
-                    board[i][j]='X'; 
-                }
+        }for(int i=0;i<rl;i++){
+            for(int j=0;j<cl;j++){
+                if(board[i][j]=='O'&&!visit[i][j])board[i][j]='X';
             }
         }
         
-    }
-    public void dfs(int r,int c,char[][]board,boolean[][]visit){
-        if(r<0||c<0||r>=rl||c>=cl||board[r][c]=='X')return;
+    }public void dfs(int r,int c,char[][]board,boolean [][]visit){
+        if(r<0||c<0|r>=rl||c>=cl||board[r][c]=='X')return;
         if(visit[r][c]==true)return;
         visit[r][c]=true;
         for(int i=0;i<4;i++){
