d5e5b9ae592f4239a3f7cd78cfa36a1d65bc5d9d
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-06 11:37:28 +0530

    Time: 4 ms (18.26%) | Memory: 46.3 MB (93.76%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.java
+++ 1380-number-of-closed-islands/number-of-closed-islands.java
@@ -3,35 +3,54 @@ class Solution {
     static int dc [] = {0 , 0 , -1 , 1}; 
     static int rowL;
     static int colL;
+    static char[]dir={'U','D','R','L'};
+    static class pair{
+        int row;
+        int col;
+        public pair(int row,int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
     public int closedIsland(int[][] grid) {
         rowL = grid.length;
         colL = grid[0].length;
         boolean [][] visited = new boolean [rowL][colL];
-        
         for(int i = 0 ; i < rowL ; i++){
-            if(grid[i][0]==0)dfs(i,0,visited,grid);
-            if(grid[i][colL-1] == 0) dfs(i , colL-1 , visited , grid);
+            if(grid[i][0]==0)bfs(i,0,visited,grid);
+            if(grid[i][colL-1] == 0) bfs(i , colL-1 , visited , grid);
             
         }for(int j=0;j<colL;j++){
-            if(grid[0][j]==0)dfs(0,j,visited,grid);
-            if(grid[rowL-1][j]==0)dfs(rowL-1,j,visited,grid);
+            if(grid[0][j]==0)bfs(0,j,visited,grid);
+            if(grid[rowL-1][j]==0)bfs(rowL-1,j,visited,grid);
         }int count=0;
         for(int i=0;i<rowL;i++){
             for(int j=0;j<colL;j++){
                 if(grid[i][j]==0&&!visited[i][j]){
-                    dfs(i,j,visited,grid);
+                    bfs(i,j,visited,grid);
                     count++;
                 }
             }
         }
         return count;
     }
-    public void dfs(int r , int c , boolean[][] visited , int [][] grid ){
-        if(r <0 || c < 0 || r >= rowL || c >= colL || grid[r][c] == 1) return ;
-        if(visited[r][c]==true)return;
+    public void bfs(int r , int c , boolean[][] visited ,int [][] grid ){
+        Queue<pair>q=new LinkedList<>();
         visited[r][c]=true;
-        for(int i = 0 ; i < 4 ; i++){
-            dfs(r+dr[i] , c + dc[i] , visited , grid);
+        q.offer(new pair(r,c));
+        while(!q.isEmpty()){
+            pair curr=q.remove();
+            int r1=curr.row;
+            int c1=curr.col;
+            for(int i=0;i<4;i++){
+                int nrow=r1+dr[i];
+                int ncol=c1+dc[i];
+                if(nrow>=0&&ncol>=0&&nrow<rowL&&ncol<colL&&!visited[nrow][ncol]&&grid[nrow][ncol]==0){
+                    visited[nrow][ncol]=true;
+                    q.offer(new pair(nrow,ncol));
+                }
+            }
         }
+        
     }
 }
