7b436ecff526d80eb1d74b65f1a6835e591b5798
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-06 11:49:06 +0530

    Time: 9 ms (54.75%) | Memory: 57.8 MB (67.25%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,43 +1,52 @@
 class Solution {
-    static int dr[] = { -1, 1, 0, 0 };
-    static int dc[] = { 0, 0, -1, 1 };
-    static int rowL;
-    static int colL;
-
+    static int[]dr={-1,1,0,0};
+    static int[]dc={0,0,-1,1};
+    static int rl;
+    static int cl;
+    static class pair{
+        int row;
+        int col;
+        public pair(int row,int col){
+            this.row=row;
+            this.col=col;
+        } 
+    }
     public int numEnclaves(int[][] grid) {
-        rowL = grid.length;
-        colL = grid[0].length;
-        boolean [][] visited = new boolean[rowL][colL];
-        
-        for(int i = 0 ; i < rowL ; i++ ){
-            if(grid[i][0] == 1) dfs(i , 0 , visited , grid);
-            if(grid[i][colL-1] == 1) dfs(i , colL-1 , visited , grid);
-        }
-        
-        for(int j = 0 ; j < colL;j++){
-            if(grid[0][j] == 1) dfs(0 , j , visited , grid);
-            if(grid[rowL-1][j] == 1) dfs(rowL-1 , j , visited ,grid);
-           
+        rl=grid.length;
+        cl=grid[0].length;
+        boolean[][]visit=new boolean[rl][cl];
+        for(int i=0;i<rl;i++){
+            if(grid[i][0]==1)bfs(i,0,visit,grid);
+            if(grid[i][cl-1]==1)bfs(i,cl-1,visit,grid);
+        }for(int j=0;j<cl;j++){
+            if(grid[0][j]==1)bfs(0,j,visit,grid);
+            if(grid[rl-1][j]==1)bfs(rl-1,j,visit,grid);
         }int count=0;
-        for(int i=0;i<rowL;i++){
-            for(int j=0;j<colL;j++){
-                if(grid[i][j]==1&&!visited[i][j]){
+        for(int i=0;i<rl;i++){
+            for(int j=0;j<cl;j++){
+                if(grid[i][j]==1&&!visit[i][j]){
                     count++;
                 }
             }
         }
         return count;
-        
-    }
+    }public void bfs(int r,int c,boolean[][]visit,int [][]grid){
+        Queue<pair>q=new LinkedList<>();
+        visit[r][c]=true;
+        q.offer(new pair(r,c));
+        while(!q.isEmpty()){
+            pair curr=q.remove();
+            int r1=curr.row;
+            int c1=curr.col;
+            for(int i=0;i<4;i++){
+                int nrow=r1+dr[i];
+                int ncol=c1+dc[i];
+                if(nrow>=0&&ncol>=0&&nrow<rl&&ncol<cl&&!visit[nrow][ncol]&&grid[nrow][ncol]==1){
+                    visit[nrow][ncol]=true;
+                    q.offer(new pair(nrow,ncol));
 
-    public void dfs(int r, int c, boolean[][] visited, int[][] grid) {
-        if (r < 0 || c < 0 | r >= rowL || c >= colL || grid[r][c] == 0)
-            return;
-        if (visited[r][c] == true)
-            return;
-        visited[r][c] = true;
-        for (int i = 0; i < 4; i++) {
-            dfs(r + dr[i], c + dc[i], visited, grid);
+                }
+            }
         }
     }
 }
