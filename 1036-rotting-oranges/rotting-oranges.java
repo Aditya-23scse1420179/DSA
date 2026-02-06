cff029087eb9157efe815aca10eb334e1bfd05ae
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-07 01:20:00 +0530

    Time: 2 ms (91.09%) | Memory: 44.5 MB (24.64%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,9 +1,8 @@
 class Solution {
     static int[]dr={-1,1,0,0};
     static int[]dc={0,0,-1,1};
-    static int rowL;
-    static int colL;
-    int maxt=0;
+    static int rl;
+    static int cl;
     class pair{
         int row;
         int col;
@@ -14,18 +13,18 @@ class Solution {
             this.time=time;
         }
     }
+    int maxt=0;
     public int orangesRotting(int[][] grid) {
-        rowL=grid.length;
-        colL=grid[0].length;
-        boolean[][]visit=new boolean[rowL][colL];
+        rl=grid.length;
+        cl=grid[0].length;
         int fresh=0;
+        boolean[][]visit=new boolean[rl][cl];
         Queue<pair>q=new LinkedList<>();
-        //add all rotten oranges
-        for(int i=0;i<rowL;i++){
-            for(int j=0;j<colL;j++){
-                if(grid[i][j]==2){//2 represnt rotten orange
-                    visit[i][j]=true;
+        for(int i=0;i<rl;i++){
+            for(int j=0;j<cl;j++){
+                if(grid[i][j]==2){
                     q.offer(new pair(i,j,0));
+                    visit[i][j]=true;
                 }else if(grid[i][j]==1){
                     fresh++;
                 }
@@ -36,11 +35,11 @@ class Solution {
             int r=curr.row;
             int c=curr.col;
             int t=curr.time;
-            maxt=Math.max(maxt,t);
             for(int i=0;i<4;i++){
                 int nrow=r+dr[i];
                 int ncol=c+dc[i];
-                if(nrow>=0&&ncol>=0&&nrow<rowL&&ncol<colL&&grid[nrow][ncol]==1&&!visit[nrow][ncol]){
+                maxt=Math.max(maxt,t);
+                if(nrow>=0&&ncol>=0&&nrow<rl&&ncol<cl&&grid[nrow][ncol]==1&&!visit[nrow][ncol]){
                     visit[nrow][ncol]=true;
                     fresh--;
                     q.offer(new pair(nrow,ncol,t+1));
@@ -48,6 +47,5 @@ class Solution {
             }
         }
         return(fresh==0)?maxt:-1;
-
     }
 }
