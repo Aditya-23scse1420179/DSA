14d56f0310f3866c1750bbf601d55eeba7a9997f
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-11 15:30:40 +0530

    Time: 67 ms (81.01%) | Memory: 48.4 MB (50.33%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,42 +1,41 @@
 class Solution {
     static class pair{
         String str;
-        int step;
-        public pair(String str,int step){
+        int level;
+        public pair(String str,int level){
             this.str=str;
-            this.step=step;
+            this.level=level;
         }
     }
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         HashSet<String>set=new HashSet<>();
-        for(String word:wordList){
-            set.add(word);
+        for(String wd:wordList){
+            set.add(wd);
         }set.remove(beginWord);
-        if(set.contains(endWord)==false){
+        if(!set.contains(endWord)){
             return 0;
-        }
-        Queue<pair>q=new LinkedList<>();
+        }Queue<pair>q=new LinkedList<>();
         q.offer(new pair(beginWord,1));
         while(!q.isEmpty()){
             pair curr=q.remove();
-            String word=curr.str;
-            int level=curr.step;
-            if(word.equals(endWord)){
-                return level;
-            }int len=word.length();
+            String s=curr.str;
+            int step=curr.level;
+            if(s.equals(endWord))return step;
+            int len=s.length();
             for(int i=0;i<len;i++){
-                char[]arr=word.toCharArray();
+                char[]arr=s.toCharArray();
                 for(char ch='a';ch<='z';ch++){
-                    if(arr[i]==ch)continue;//h-->>h skip karna hai 
+                    if(arr[i]==ch)continue;
                     arr[i]=ch;
                     String replace=new String(arr);
                     if(set.contains(replace)){
-                        q.offer(new pair(replace,level+1));
+                        q.offer(new pair(replace,step+1));
                         set.remove(replace);
                     }
                 }
             }
         }
         return 0;
+
     }
 }
