cc9828a0c977cb994efe433af5998f266c1d0a5f
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-13 11:27:37 +0530

    Time: 39 ms (84.93%) | Memory: 47.8 MB (73.15%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,46 @@
+class Solution {
+    static int[]dr={-1,1,0,0};
+    static int[]dc={0,0,-1,1};
+    static class pair{
+        int row;
+        int col;
+        int dist;
+        public pair(int row,int col,int dist){
+            this.row=row;
+            this.col=col;
+            this.dist=dist;
+        }
+    }
+    public int minimumEffortPath(int[][] heights) {
+        PriorityQueue<pair>q=new PriorityQueue<>((a,b)->Integer.compare(a.dist,b.dist));
+        int n=heights.length;
+        int m=heights[0].length;
+        int [][]distance=new int[n][m];
+        for(int []a:distance){
+            Arrays.fill(a,(int)1e9);
+        }
+        distance[0][0]=0;
+        q.offer(new pair(0,0,0));
+        while(!q.isEmpty()){
+            pair curr=q.remove();
+            int r=curr.row;
+            int c=curr.col;
+            int w=curr.dist;
+            if(w>distance[r][c])continue;
+            if(r==n-1&&c==m-1)return w;
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0&&nr<n&&nc>=0&&nc<m){
+                    int newEfforts=Math.max(w,Math.abs(heights[nr][nc]-heights[r][c]));
+                    if(newEfforts<distance[nr][nc]){
+                        distance[nr][nc]=newEfforts;
+                        q.offer(new pair(nr,nc,newEfforts));
+                    }
+                }
+            }
+        }
+        return -1;
+
+    }
+}
