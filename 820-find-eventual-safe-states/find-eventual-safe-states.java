cff2480e317a2e5fe2db48f26a24bc150864f092
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-14 15:53:15 +0530

    Time: 26 ms (36.66%) | Memory: 61.7 MB (63.13%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,31 +1,32 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int[]outdegree=new int[graph.length];
+        int n=graph.length;
+        int m=graph[0].length;
         ArrayList<ArrayList<Integer>>list=new ArrayList<>();
+        int[]outdegree=new int[graph.length];
         for(int i=0;i<graph.length;i++){
             list.add(new ArrayList<>());
-        }Queue<Integer>q=new LinkedList<>();
-        for(int i=0;i<graph.length;i++){
+        }for(int i=0;i<graph.length;i++){
             outdegree[i]=graph[i].length;
             for(int a:graph[i]){
                 list.get(a).add(i);
             }
-        }for(int i=0;i<graph.length;i++){
+        }Queue<Integer>q=new LinkedList<>();
+        ArrayList<Integer>path=new ArrayList<>();
+        for(int i=0;i<n;i++){
             if(outdegree[i]==0){
                 q.offer(i);
             }
-        }ArrayList<Integer>ans=new ArrayList<>();
-        while(!q.isEmpty()){
+        }while(!q.isEmpty()){
             int curr=q.remove();
-            ans.add(curr);
+            path.add(curr);
             for(int a:list.get(curr)){
                 outdegree[a]--;
                 if(outdegree[a]==0){
                     q.offer(a);
                 }
             }
-        }
-        Collections.sort(ans);
-        return ans;
+        }Collections.sort(path);
+        return path;
     }
 }
