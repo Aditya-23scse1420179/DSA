a930369e7e7fe6f83fe6bed8bbda2b0c1bf542cf
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-15 18:27:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (46.53%) - LeetSync

--- 458-poor-pigs/poor-pigs.java
+++ 458-poor-pigs/poor-pigs.java
@@ -3,11 +3,11 @@ class Solution
     public int poorPigs(int buckets, int tdie, int ttest)
     {
         int test = ttest/tdie+1;
-        int i=0;
-        while(Math.pow(test,i)< buckets)
+        int pig=0;
+        while(Math.pow(test,pig)< buckets)
         {
-            i++;
+            pig++;
         }
-        return i;
+        return pig;
     }
 }
