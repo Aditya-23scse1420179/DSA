2de95c74f36f8e80ecfff66887b929401e5dd182
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-17 14:54:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (70.50%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -3,28 +3,29 @@ class Solution {
     int[]rank;
     public int[] findRedundantConnection(int[][] edges) {
         parent=new int[edges.length+1];
-        for(int i=0;i<=edges.length;i++){
+        for(int i=0;i<edges.length;i++){
             parent[i]=i;
         }
         rank=new int[edges.length+1];
         int[]ans=new int[2];
         for(int i=0;i<edges.length;i++){
-            if(!unionrank(edges[i][0],edges[i][1])){
+            if(unionrank(edges[i][0],edges[i][1])==false){
                 ans[0]=edges[i][0];
                 ans[1]=edges[i][1];
             }
         }
         return ans;
+        
     }public int find(int x){
         if(x==parent[x])return x;
         return parent[x]=find(parent[x]);
     }public boolean unionrank(int x,int y){
-        int p_x=find(parent[x]);
-        int p_y=find(parent[y]);
-        if(p_x==p_y)return false;
+        int p_x=find(x);
+        int p_y=find(y);
+        if(p_x==p_y)return false;//cycle 
         if(rank[p_x]>rank[p_y]){
             parent[p_y]=p_x;
-        }else if(parent[p_x]<parent[p_y]){
+        }else if(rank[p_y]>rank[p_x]){
             parent[p_x]=p_y;
         }else{
             parent[p_x]=p_y;
