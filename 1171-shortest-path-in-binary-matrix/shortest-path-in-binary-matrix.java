73caf9ca07a9bc45b0970cb39f634ba0d4444ae8
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-17 09:57:46 +0530

    Time: 17 ms (47.87%) | Memory: 47.6 MB (77.62%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,5 +1,5 @@
 class Solution {
-    static int[]dr={-1,1,0,0,-1,-1,1,1};
+    static int []dr={-1,1,0,0,-1,-1,1,1};
     static int[]dc={0,0,-1,1,-1,1,-1,1};
     static class pair{
         int row;
@@ -14,13 +14,13 @@ class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
+        if(grid[0][0]==1)return -1;
         int[][]dist=new int[n][m];
-        for(int[]a:dist){
+        for(int []a:dist){
             Arrays.fill(a,(int)1e9);
         }
         dist[0][0]=1;
         Queue<pair>q=new LinkedList<>();
-        if(grid[0][0]==1)return -1;
         q.offer(new pair(0,0,1));
         while(!q.isEmpty()){
             pair curr=q.remove();
@@ -28,18 +28,17 @@ class Solution {
             int c=curr.col;
             int w=curr.dist;
             for(int i=0;i<8;i++){
-                int nr=r+dr[i];
+                int nr= r+dr[i];
                 int nc=c+dc[i];
                 if(nr<0||nr>=n||nc<0||nc>=m)continue;
                 if(grid[nr][nc]==0){
                     if(dist[nr][nc]>w+1){
                         dist[nr][nc]=w+1;
                         q.offer(new pair(nr,nc,dist[nr][nc]));
-                    }else 
-                        continue;
+                    }
                 }
             }
         }
-        return dist[n-1][m-1]<(int)1e9?dist[n-1][m-1]:-1;
+        return dist[n-1][m-1]==(int)1e9?-1:dist[n-1][m-1];
     }
 }
