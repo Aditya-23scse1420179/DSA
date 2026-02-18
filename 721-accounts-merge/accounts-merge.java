6cbf9619fb94a0380bdba85f19f31922cc6a949d
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-18 14:13:11 +0530

    Time: 33 ms (54.77%) | Memory: 49.8 MB (51.83%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,37 +1,53 @@
 class Solution {
-    int []parent;
-    int[]rank;
+    int[]parent;
+    int []rank;
+    int component;
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        parent=new int[accounts.size()];
-        rank=new int[accounts.size()];
-        for(int i=0;i<accounts.size();i++){
-            parent[i]=i;
-        }
-        HashMap<String,Integer>map=new HashMap<>();
-        for(int i=0;i<accounts.size();i++){
-            for(int j=1;j<accounts.get(i).size();j++){
-                String email=accounts.get(i).get(j);
-                if(!map.containsKey(email)){
-                    map.put(email,i);
-                }else{
-                    unionrank(i,map.get(email));
+        //email ko unique id deni hai 
+        HashMap<String,Integer>emailToId=new HashMap<>();
+        // email ko uske account ke sath accosiat ekaro 
+        HashMap<String,String>emailToName=new HashMap<>();
+        int id=0;
+        for(List<String>acc:accounts){
+            String name=acc.get(0);
+            for(int i=1;i<acc.size();i++){
+                String email=acc.get(i);
+                if(!emailToId.containsKey(email)){
+                    emailToId.put(email,id++);
                 }
+                emailToName.put(email,name);
             }
         }
-        HashMap<Integer,TreeSet<String>>set=new HashMap<>();
-        for(String s:map.keySet()){
-            int root=find(map.get(s));
-            set.putIfAbsent(root,new TreeSet<>());
-            set.get(root).add(s);
+        component=id;
+        parent=new int[id];
+        rank=new int[id];
+        for(int i=0;i<id;i++){
+            parent[i]=i;
         }
-        List<List<String>>list=new ArrayList<>();
-        for(int root:set.keySet()){
-            List<String>temp=new ArrayList<>();
-            temp.add(accounts.get(root).get(0));
-            temp.addAll(set.get(root));
-            list.add(temp);
+        // merge same account ke mail
+        for(List<String>acc:accounts){
+            if(acc.size()<=2)continue;
+            int firstAccountId=emailToId.get(acc.get(1));
+            for(int i=2;i<acc.size();i++){
+                unionrank(firstAccountId,emailToId.get(acc.get(i)));
+            }
         }
-        return list;
+        HashMap<Integer,List<String>>grouptoid=new HashMap<>();
+        for(String email:emailToId.keySet()){
+            int EmailId=emailToId.get(email);
+            int root=find(EmailId);
+            if(!grouptoid.containsKey(root)){
+                grouptoid.put(root,new ArrayList<>());
+            }grouptoid.get(root).add(email);
+        }
+        List<List<String>>result=new ArrayList<>();
+        for(Integer node:grouptoid.keySet()){
+            List<String>merge=grouptoid.get(node);
+            Collections.sort(merge);
+            merge.add(0,emailToName.get(merge.get(0)));
+            result.add(merge);
+        }
+        return result;
     }public int find(int x){
         if(x==parent[x])return x;
         return parent[x]=find(parent[x]);
@@ -43,7 +59,7 @@ class Solution {
             parent[p_y]=p_x;
         }else if(rank[p_y]>rank[p_x]){
             parent[p_x]=p_y;
-        }else{
+        }else {
             parent[p_x]=p_y;
             rank[p_y]++;
         }
