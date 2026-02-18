2db94856b05c4de97f5e9c85c82c14a9f58530a9
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-18 22:33:41 +0530

    Time: 40 ms (75.12%) | Memory: 48.2 MB (14.28%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,10 +1,8 @@
 class Solution {
     static int[]dr={-1,1,0,0};
-    static int[]dc={0,0,-1,1};
-    static class pair{
-        int row;
-        int col;
-        int dist;
+    static int []dc={0,0,-1,1};
+    class pair{
+        int row,col,dist;
         public pair(int row,int col,int dist){
             this.row=row;
             this.col=col;
@@ -12,31 +10,29 @@ class Solution {
         }
     }
     public int minimumEffortPath(int[][] heights) {
-        PriorityQueue<pair>q=new PriorityQueue<>((a,b)->Integer.compare(a.dist,b.dist));
         int n=heights.length;
         int m=heights[0].length;
-        int [][]distance=new int[n][m];
-        for(int []a:distance){
+        int[][]dist=new int[n][m];
+        for(int[]a:dist){
             Arrays.fill(a,(int)1e9);
-        }
-        distance[0][0]=0;
+        }dist[0][0]=0;
+        PriorityQueue<pair>q=new PriorityQueue<>((a,b)->Integer.compare(a.dist,b.dist));
         q.offer(new pair(0,0,0));
         while(!q.isEmpty()){
             pair curr=q.remove();
             int r=curr.row;
             int c=curr.col;
             int w=curr.dist;
-            if(w>distance[r][c])continue;
+            if(dist[r][c]<w)continue;
             if(r==n-1&&c==m-1)return w;
             for(int i=0;i<4;i++){
                 int nr=r+dr[i];
                 int nc=c+dc[i];
-                if(nr>=0&&nr<n&&nc>=0&&nc<m){
-                    int newEfforts=Math.max(w,Math.abs(heights[nr][nc]-heights[r][c]));
-                    if(newEfforts<distance[nr][nc]){
-                        distance[nr][nc]=newEfforts;
-                        q.offer(new pair(nr,nc,newEfforts));
-                    }
+                if(nr<0||nr>=n||nc<0||nc>=m)continue;
+                int d=Math.max(w,Math.abs(heights[nr][nc]-heights[r][c]));
+                if(d<dist[nr][nc]){
+                    dist[nr][nc]=d;
+                    q.offer(new pair(nr,nc,dist[nr][nc]));
                 }
             }
         }
