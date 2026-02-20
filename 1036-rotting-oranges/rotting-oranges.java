f8856bb65ba5644e1ddbbe8b0e77d3b1fc5054aa
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-20 19:59:12 +0530

    Time: 2 ms (91.20%) | Memory: 43.9 MB (95.84%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,51 +1,49 @@
 class Solution {
     static int[]dr={-1,1,0,0};
     static int[]dc={0,0,-1,1};
-    static int rl;
-    static int cl;
     class pair{
         int row;
         int col;
-        int time;
-        public pair(int row,int col,int time){
+        public pair(int row,int col){
             this.row=row;
             this.col=col;
-            this.time=time;
         }
     }
-    int maxt=0;
     public int orangesRotting(int[][] grid) {
-        rl=grid.length;
-        cl=grid[0].length;
-        int fresh=0;
-        boolean[][]visit=new boolean[rl][cl];
+        int n=grid.length;
+        int m=grid[0].length;
         Queue<pair>q=new LinkedList<>();
-        for(int i=0;i<rl;i++){
-            for(int j=0;j<cl;j++){
+        int fresh=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.offer(new pair(i,j,0));
-                    visit[i][j]=true;
+                    q.offer(new pair(i,j));
                 }else if(grid[i][j]==1){
                     fresh++;
                 }
             }
-        }if(fresh==0)return 0;
+        }int level=0;
         while(!q.isEmpty()){
-            pair curr=q.remove();
-            int r=curr.row;
-            int c=curr.col;
-            int t=curr.time;
-            for(int i=0;i<4;i++){
-                int nrow=r+dr[i];
-                int ncol=c+dc[i];
-                maxt=Math.max(maxt,t);
-                if(nrow>=0&&ncol>=0&&nrow<rl&&ncol<cl&&grid[nrow][ncol]==1&&!visit[nrow][ncol]){
-                    visit[nrow][ncol]=true;
-                    fresh--;
-                    q.offer(new pair(nrow,ncol,t+1));
+            boolean rotten=false;
+            int size=q.size();
+            while(size-->0){
+                pair curr=q.remove();
+                int r=curr.row;
+                int c=curr.col;
+                for(int i=0;i<4;i++){
+                    int nr=r+dr[i];
+                    int nc=c+dc[i];
+                    if(nr<0||nr>=n||nc<0||nc>=m)continue;
+                    if(grid[nr][nc]==1){
+                        fresh--;
+                        grid[nr][nc]=2;
+                        rotten=true;
+                        q.offer(new pair(nr,nc));
+                    }
                 }
             }
+            if(rotten)level++;
         }
-        return(fresh==0)?maxt:-1;
+        return fresh==0?level:-1;
     }
 }
