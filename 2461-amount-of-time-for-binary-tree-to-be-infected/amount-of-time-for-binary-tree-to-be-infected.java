8d6fb95212c39bedc6a5621a991ace860cd969cc
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-24 22:54:34 +0530

    Time: 85 ms (74.46%) | Memory: 117.7 MB (48.86%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -14,47 +14,47 @@
  * }
  */
 class Solution {
-    public TreeNode find(TreeNode root, int start) {
-    if (root==null) return null;
-    if (root.val== start) return root; 
-    TreeNode Res =find(root.left, start);
-    if(Res!=null)return Res;
-    return find(root.right,start);
-}
+    public TreeNode find(TreeNode root,int start){
+        if(root==null)return null;
+        if(root.val==start)return root;
+        TreeNode ans=find(root.left,start);
+        if(ans!=null)return ans;// ans hoga jisme store kar rahe 
+        return find(root.right,start);
+    }
     public int amountOfTime(TreeNode root, int start) {
         HashMap<TreeNode,TreeNode>map=new HashMap<>();
-        buildParentMap(root,null,map);
+        build(root,null,map);
+        Set<TreeNode>set=new HashSet<>();
+        set.add(find(root,start));
         Queue<TreeNode>q=new LinkedList<>();
-        Set<TreeNode>visit=new HashSet<>();
-        int distance=0;
-        visit.add(find(root,start));
         q.offer(find(root,start));
+        int level=0;
         while(!q.isEmpty()){
             int size=q.size();
             while(size-->0){
                 TreeNode curr=q.remove();
-                if(curr.left!=null && !visit.contains(curr.left)){
+                if(curr.left!=null&&!set.contains(curr.left)){
                     q.offer(curr.left);
-                    visit.add(curr.left);
+                    set.add(curr.left);
                 }
-                if(curr.right!=null && !visit.contains(curr.right)){
+                if(curr.right!=null&&!set.contains(curr.right)){
                     q.offer(curr.right);
-                    visit.add(curr.right);
+                    set.add(curr.right);
                 }
                 TreeNode parent=map.get(curr);
-                if(parent!=null && !visit.contains(parent)){
+                if(parent!=null&&!set.contains(parent)){
                     q.offer(parent);
-                    visit.add(parent);
+                    set.add(parent);
                 }
             }
-            distance++;
-            
+            level++;
         }
-        return distance-1;
-    }public void buildParentMap(TreeNode node,TreeNode parent,HashMap<TreeNode,TreeNode>map){
-        if(node==null)return;
+        return level-1;
+
+    }public void build(TreeNode node,TreeNode parent,HashMap<TreeNode,TreeNode>map){
+        if(node==null)return;//root nahi node se check hoga 
         map.put(node,parent);
-        buildParentMap(node.left,node,map);
-        buildParentMap(node.right,node,map);
+        build(node.left,node,map);
+        build(node.right,node,map);
     }
 }
