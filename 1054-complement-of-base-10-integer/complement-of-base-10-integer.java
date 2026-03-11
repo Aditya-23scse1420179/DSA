3324ab214e74f12c40cc3f74b3665cd61d13f95f
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-11 22:56:54 +0530

    Time: 1 ms (24.44%) | Memory: 42.3 MB (32.44%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        String s=Integer.toBinaryString(n);
+        StringBuilder sb=new StringBuilder();
+        for(int a:s.toCharArray()){
+            if(a=='0')sb.append('1');
+            else sb.append('0');
+        }
+        int ans=Integer.parseInt(sb.toString(),2);
+        return ans;
+    }
+}
