fee61ac66ac1000765d7e93991071e246088d13b
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-12 11:05:57 +0530

    Time: 23 ms (82.01%) | Memory: 54.9 MB (6.66%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -3,12 +3,10 @@ class Solution {
         int count=0,sum=0;
         HashMap<Integer,Integer>map=new HashMap<>();
         map.put(0,1);
-        for(int num:nums){
-            sum+=num;
+        for(int x:nums){
+            sum+=x;
             int prod=((sum%k)+k)%k;
-            if(map.containsKey(prod)){
-                count+=map.get(prod);
-            }
+            count+=map.getOrDefault(prod,0);
             map.put(prod,map.getOrDefault(prod,0)+1);
         }
         return count;
