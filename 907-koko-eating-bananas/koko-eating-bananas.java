1ecb634b1034b3d7191605ab4d009c0a65e2890e
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-14 13:56:50 +0530

    Time: 12 ms (55.46%) | Memory: 47.5 MB (97.34%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,29 +1,28 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int n=(int)1e9;
-        for(int x:piles){
-            n=Math.max(x,n);
+        int n=-(int)1e9;
+        for(int a:piles){
+            n=Math.max(a,n);
         }
         int low=1;
-        int high =n;
+        int high=n;
         int ans=high;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(helper(piles,h,mid)){
-                ans =mid;
+            if(helper(piles,mid,h)){
+                ans=mid;
                 high=mid-1;
             }else{
                 low=mid+1;
             }
         }
-        return ans ;
-    }
-    public static boolean helper(int[]piles,int h,int k){
+        return ans;
+    }public boolean helper(int[]piles,int k,int h){
         long hours=0;
-        for (int pile : piles) {
-            hours += pile / k;
-            if (pile % k != 0) hours++;
+        for(int a:piles){
+            hours+=a/k;
+            if(a%k!=0)hours++;
         }
-        return hours<= h;
+        return hours<=h;
     }
 }
