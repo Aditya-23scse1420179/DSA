154e7e5afcf1916165ac3a9b1d039d5febb26c4b
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-14 23:24:10 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (65.86%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int low=0,high=0;
+        for(int a:nums){
+            low=Math.max(a,low);
+            high+=a;
+        }
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(helper(nums,k,mid)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+
+    }public boolean helper(int[]nums,int k,int mid){
+        int day=1,m=0;
+        for(int a:nums){
+            m+=a;
+            if(m>mid){
+                day++;
+                m=a;
+            }
+        }
+        return day<=k;
+
+    }
+}
