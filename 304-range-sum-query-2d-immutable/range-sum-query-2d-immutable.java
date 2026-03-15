2eae3f0c5edda7d1d83a9f78f0ca219d39c2a08a
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-15 20:30:05 +0530

    Time: 102 ms (75.99%) | Memory: 142.1 MB (43.26%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -4,32 +4,18 @@ class NumMatrix {
     public NumMatrix(int[][] matrix) {
         n=matrix.length;
         m=matrix[0].length;
-        mat=new int[n][m];
-        mat[0][0]=matrix[0][0];
-        for(int i=1;i<n;i++){
-            mat[i][0]=mat[i-1][0]+matrix[i][0];
-        }for(int j=1;j<m;j++){
-            mat[0][j]=mat[0][j-1]+matrix[0][j];
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                mat[i][j]=matrix[i][j]+mat[i-1][j]+mat[i][j-1]-mat[i-1][j-1];
+        mat=new int[n+1][m+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                mat[i][j] = matrix[i - 1][j - 1]+ mat[i-1][j]+ mat[i][j-1]-mat[i - 1][j - 1];
             }
         }
-
     }
     
     public int sumRegion(int r1, int c1, int r2, int c2) {
         int ans=0;
-        ans+=mat[r2][c2];
-        if(r1>0){
-            ans-=mat[r1-1][c2];
-        }
-        if(c1>0){
-            ans-=mat[r2][c1-1];
-        }if(r1>0&&c1>0){
-            ans+=mat[r1-1][c1-1];
-        }
+        r1++;c1++;r2++;c2++;
+        ans=mat[r2][c2]-mat[r1-1][c2]-mat[r2][c1-1]+mat[r1-1][c1-1];
         return ans;
     }
 }
