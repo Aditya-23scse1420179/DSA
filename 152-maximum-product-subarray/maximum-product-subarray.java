b3dbb392bb5b691cab228bedbd3e84d2e2dc59a4
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-15 11:08:00 +0530

    Time: 1 ms (90.46%) | Memory: 47.1 MB (91.97%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -4,11 +4,11 @@ class Solution {
         int max=1;
         int ans=-(int)1e9;
         for(int i=0;i<nums.length;i++){
-            if(min==0)min=1;
-            if(max==0)max=1;
-            min*=nums[i];
-            max*=nums[nums.length-i-1];
-            ans=Math.max(ans,Math.max(min,max));
+            if(min==0)min=1;//0 milt hi dusra subarray start kyunki 0 ke baad sab aage 0 hi ho jayega 
+            if(max==0)max=1;// isiliye 0 milte hi reset 0 ke baad waale element ko 1 se mult karke naya subarray suru karo 
+            min*=nums[i];//aage se sabka product 
+            max*=nums[nums.length-i-1];//piche se sabka prod taki beech ke 
+            ans=Math.max(ans,Math.max(min,max));//ans ke sath max lo ans me har iteration ke baad ak value ayegi 
         }
         return ans;
     }
