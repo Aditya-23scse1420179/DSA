f44a0581b555e62867918f5fa424ee685997148f
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-15 11:04:30 +0530

    Time: 1 ms (90.46%) | Memory: 47.1 MB (84.45%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,16 +1,15 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int pre_sum = 1;
-        int suf_sum = 1;
-        int maxSum = Integer.MIN_VALUE;
-        int n = nums.length;
-        for(int i=0; i<nums.length; i++){
-            if(pre_sum == 0) pre_sum = 1;
-            if(suf_sum == 0) suf_sum = 1;
-            pre_sum *= nums[i];
-            suf_sum *= nums[n-1-i];
-            maxSum = Math.max(maxSum,Math.max(pre_sum,suf_sum));
-        } 
-        return maxSum;
+        int min=1;
+        int max=1;
+        int ans=-(int)1e9;
+        for(int i=0;i<nums.length;i++){
+            if(min==0)min=1;
+            if(max==0)max=1;
+            min*=nums[i];
+            max*=nums[nums.length-i-1];
+            ans=Math.max(ans,Math.max(min,max));
+        }
+        return ans;
     }
 }
