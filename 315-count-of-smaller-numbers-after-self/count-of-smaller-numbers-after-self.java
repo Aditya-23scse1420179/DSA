622a9d9b0f064dc5d0e3b34bf797ba3d381fa59f
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-19 10:25:47 +0530

    Time: 156 ms (26.66%) | Memory: 108.5 MB (29.28%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
@@ -1,46 +1,52 @@
 class Solution {
-    static int[] st;
-    int size;
+    static class SegTree{
+        int n;
+        int[]tree;
+        public SegTree(int n){
+            this.n=n;
+            tree=new int[4*n];
+        }
+        public int query(int idx,int l,int r,int qs,int qe){
+            if(r<qs||l>qe)return 0;
+            if(l>=qs&&r<=qe)return tree[idx];
+            int mid=l+(r-l)/2;
+            return query(2*idx+1,l,mid,qs,qe)+query(2*idx+2,mid+1,r,qs,qe);
+        }public void update(int idx,int l,int r,int val){
+            if(l==r){
+                tree[idx]++;
+                return;
+            }
+            int mid=l+(r-l)/2;
+            if(val<=mid){
+                update(2*idx+1,l,mid,val);
+            }else{
+                update(2*idx+2,mid+1,r,val);
+            }
+            tree[idx]=tree[2*idx+1]+tree[2*idx+2];
+
+        }
+    }
     public List<Integer> countSmaller(int[] nums) {
-        int n = nums.length;
-        int[] sorted = nums.clone();
+        int n=nums.length;
+        int[]sorted=nums.clone();
         Arrays.sort(sorted);
-        Map<Integer,Integer> rank = new HashMap<>();
-        int id = 0;
-        for (int x : sorted) {
-            if (!rank.containsKey(x)) {
-                rank.put(x, id++);
+        int id=0;
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int a:sorted){
+            if(!map.containsKey(a)){
+                map.put(a,id++);
             }
         }
-        size = id;
-        st = new int[4 * size];
-        Integer[] ans = new Integer[n];
-        for (int i = n - 1; i >= 0; i--) {
-            int r = rank.get(nums[i]);
-            ans[i] = query(0, 0, size - 1, 0, r - 1); 
-            update(0, 0, size - 1, r, 1);    
+
+        //segment tree
+        SegTree seg=new SegTree(id);
+        Integer[]ans=new Integer[n];
+        for(int i=n-1;i>=0;i--)
+        {
+            int r=map.get(nums[i]);
+            ans[i]=seg.query(0,0,id-1,0,r-1);
+            seg.update(0,0,id-1,r);
         }
         return Arrays.asList(ans);
-    }
-    public void update(int idx, int l, int r, int i, int val) {
-        if (l == r) {
-            st[idx] += val;
-            return;
-        }
-        int mid = (l + r) / 2;
-        if (i <= mid) {
-            update(2 * idx + 1, l, mid, i, val);
-        } else {
-            update(2 * idx + 2, mid + 1, r, i, val);
-        }
-        st[idx] = st[2 * idx + 1] + st[2 * idx + 2];
-    }
-    public int query(int idx, int l, int r, int qs, int qe) {
-        if (r < qs || l > qe) return 0; 
-        if (l >= qs && r <= qe) return st[idx]; 
-        int mid = (l + r) / 2;
-        int left = query(2 * idx + 1, l, mid, qs, qe);
-        int right = query(2 * idx + 2, mid + 1, r, qs, qe);
-        return left + right;
     }
 }
