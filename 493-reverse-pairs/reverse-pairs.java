39ee1de33e2c96f12267d7872fb4afe9d79422fa
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-20 11:07:00 +0530

    Time: 228 ms (5.16%) | Memory: 103.9 MB (8.65%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -1,24 +1,21 @@
-// 3 main kaam hain 
-// segment tree me convert 
-//query me kya value pass 
-// kya value update 
 class Solution {
-    static class SegTree{
+    static class segTree{
         int n;
-        int[]tree;
-        public SegTree(int n){
+        int tree[];
+        public segTree(int n){
             this.n=n;
             tree=new int[4*n];
-        }
-        public int query(int idx,int l,int r,int qs,int qe){
+        }public int query(int idx,int l,int r,int qs,int qe){
             if(r<qs||l>qe)return 0;
-            if(l>=qs&&r<=qe)return tree[idx];
+            if(l>=qs&&r<=qe){
+                return tree[idx];
+            }
             int mid=l+(r-l)/2;
             return query(2*idx+1,l,mid,qs,qe)+query(2*idx+2,mid+1,r,qs,qe);
-        }public void update(int idx,int l,int r,int val){
+        }public void update(int idx,int l,int r ,int val){
             if(l==r){
                 tree[idx]++;
-                return;
+                return ;
             }
             int mid=l+(r-l)/2;
             if(val<=mid){
@@ -27,7 +24,6 @@ class Solution {
                 update(2*idx+2,mid+1,r,val);
             }
             tree[idx]=tree[2*idx+1]+tree[2*idx+2];
-
         }
     }
     public int reversePairs(int[] nums) {
@@ -38,22 +34,21 @@ class Solution {
             set.add(2L*a);
         }
         List<Long>list=new ArrayList<>(set);
-        Map<Long,Integer>map=new HashMap<>();
         int id=0;
-        for(int i=0;i<list.size();i++){
-            if(!map.containsKey(list.get(i))){
-                map.put(list.get(i),id++);
+        Map<Long,Integer>map=new HashMap<>();
+        for(long a:list){
+            if(!map.containsKey(a)){
+                map.put(a,id++);
             }
         }
-        SegTree seg=new SegTree(id);
+        segTree seg=new segTree(id);
         int ans=0;
-        for (int j = 0; j < nums.length; j++) {
-            int pos2 = map.get(2L * nums[j]);
-            ans += seg.query(0, 0, id-1, pos2+1, id-1);
-            int pos = map.get((long)nums[j]);
-            seg.update(0, 0, id-1, pos);
+        for(int i=0;i<nums.length;i++){
+            int r=map.get(2L*nums[i]);
+            ans+=seg.query(0,0,id-1,r+1,id-1);
+            int pos=map.get((long)nums[i]);
+            seg.update(0,0,id-1,pos);
         }
-
         return ans;
     }
 }
