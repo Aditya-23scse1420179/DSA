2bf033f300266593c47b135b04462ab538c810f5
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-20 10:04:28 +0530

    Time: 162 ms (25.51%) | Memory: 108.2 MB (30.43%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
@@ -1,52 +1,48 @@
 class Solution {
-    static class SegTree{
+    static class segTree{
         int n;
         int[]tree;
-        public SegTree(int n){
+        public segTree(int n){
             this.n=n;
             tree=new int[4*n];
-        }
-        public int query(int idx,int l,int r,int qs,int qe){
+        }public int query(int idx,int l,int r,int qs,int qe){
             if(r<qs||l>qe)return 0;
-            if(l>=qs&&r<=qe)return tree[idx];
+            if(l>=qs&&r<=qe){
+                return tree[idx];
+            }
             int mid=l+(r-l)/2;
             return query(2*idx+1,l,mid,qs,qe)+query(2*idx+2,mid+1,r,qs,qe);
         }public void update(int idx,int l,int r,int val){
             if(l==r){
                 tree[idx]++;
-                return;
-            }
-            int mid=l+(r-l)/2;
+                return ;
+            }int mid=l+(r-l)/2;
             if(val<=mid){
                 update(2*idx+1,l,mid,val);
             }else{
                 update(2*idx+2,mid+1,r,val);
             }
             tree[idx]=tree[2*idx+1]+tree[2*idx+2];
-
         }
     }
     public List<Integer> countSmaller(int[] nums) {
         int n=nums.length;
-        int[]sorted=nums.clone();
-        Arrays.sort(sorted);
+        int[]sort=nums.clone();
+        Arrays.sort(sort);
         int id=0;
         Map<Integer,Integer>map=new HashMap<>();
-        for(int a:sorted){
+    
+        for(int a:sort){
             if(!map.containsKey(a)){
                 map.put(a,id++);
             }
-        }
-
-        //segment tree
-        SegTree seg=new SegTree(id);
+        }segTree seg=new segTree(id);
         Integer[]ans=new Integer[n];
-        for(int i=n-1;i>=0;i--)
-        {
+        for(int i=n-1;i>=0;i--){
             int r=map.get(nums[i]);
             ans[i]=seg.query(0,0,id-1,0,r-1);
             seg.update(0,0,id-1,r);
         }
-        return Arrays.asList(ans);
+        return Arrays.asList(ans) ;
     }
 }
