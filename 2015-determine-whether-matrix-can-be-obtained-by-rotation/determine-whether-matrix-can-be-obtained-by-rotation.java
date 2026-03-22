ca2ef0c0ceba96f29f10a59ab016e2a43c24efb8
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-22 12:25:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (32.39%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,15 +1,19 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
-        int n = mat.length;
-        for (int k = 0;k < 4;k++) {
-            if (areSame(mat, target, n)) {
-                return true;
-            }
-            rotate90(mat, n);
+        int n=mat.length;
+        for(int i=0;i<4;i++){// 4 bebcause all 4 degree 0,90,180,270
+            if(same(mat,target,n))return true;
+            rotate(mat,n);
         }
         return false;
-    }
-    private void rotate90(int[][] mat, int n) {
+    }public boolean same(int[][]mat,int[][]target,int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]!=target[i][j])return false;
+            }
+        }
+        return true;
+    }public void rotate(int[][] mat, int n) {
         for(int i =0;i< n/2;i++){
             int[]temp =mat[i];
             mat[i] =mat[n -1 -i];
@@ -22,13 +26,5 @@ class Solution {
                 mat[j][i] = temp;
             }
         }
-    }
-    private boolean areSame(int[][] a, int[][] b, int n) {
-        for(int i= 0; i <n;i++){
-            for (int j = 0; j <n;j++){
-                if (a[i][j]!=b[i][j]) return false;
-            }
-        }
-        return true;
     }
 }
