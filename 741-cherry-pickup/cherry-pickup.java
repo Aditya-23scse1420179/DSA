e96e26f3ec67882fd6e1ef95b16115184564340a
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-22 16:23:29 +0530

    Time: 17 ms (42.44%) | Memory: 47.6 MB (28.57%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.java
+++ 741-cherry-pickup/cherry-pickup.java
@@ -14,7 +14,7 @@ class Solution {
         if(dp[r1][c1][c2]!=null)return dp[r1][c1][c2];
         int ans=grid[r1][c1];
         if(c1!=c2)ans+=grid[r2][c2];
-        int temp=Math.max(helper(r1,c1+1,c2+1),helper(r1+1,c1,c2));//right right 
+        int temp=Math.max(helper(r1,c1+1,c2+1),helper(r1+1,c1,c2));//right right r1c1r2c2 ko ak sath leke visualise kar
         temp=Math.max(temp,helper(r1+1,c1,c2+1));//temp aur down right ka max
         temp=Math.max(temp,helper(r1,c1+1,c2));//temp aur right down ka max
         ans+=temp;// fir temp me add kar do 
