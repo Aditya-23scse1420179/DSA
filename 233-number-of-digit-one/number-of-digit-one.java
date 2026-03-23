4afcbbaff1a79daf31fb3a35c3e9ebfff2323918
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-23 20:53:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (65.04%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.java
+++ 233-number-of-digit-one/number-of-digit-one.java
@@ -1,20 +1,21 @@
 class Solution {
+    Integer[][][]dp;
     public int countDigitOne(int n) {
         String s=String.valueOf(n);
-        Integer[][][]dp=new Integer[s.length()+1][2][11];
-        return helper(s,0,1,0,dp);        
-    }public int helper(String s,int idx,int tight,int count,Integer[][][]dp ){
+        dp=new Integer[s.length()+1][2][11];
+        return helper(s,0,1,0,dp);
+    }public int helper(String s,int idx,int tight,int count,Integer[][][]dp){
         if(idx==s.length())return count;
         int lb=0;
-        int ub=(tight==1)?s.charAt(idx)-'0':9;
-        int res=0;
         if(dp[idx][tight][count]!=null)return dp[idx][tight][count];
-        for(int digit =lb;digit<=ub;digit++){
-            int newtight=(tight==1&&digit==ub)?1:0;
+        int res=0;
+        int ub=tight==1?s.charAt(idx)-'0':9;
+        for(int digit=lb;digit<=ub;digit++){
+            int nt=tight==1&&digit==ub?1:0;
             if(digit==1){
-                res+=helper(s,idx+1,newtight,count+1,dp);
+                res+=helper(s,idx+1,nt,count+1,dp);
             }else{
-                res+=helper(s,idx+1,newtight,count,dp);
+                res+=helper(s,idx+1,nt,count,dp);
             }
         }
         return dp[idx][tight][count]=res;
