d8cc9f7db03a2604137b7ed235d6d0bbc786d0df
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-23 23:54:21 +0530

    Time: 3 ms (39.13%) | Memory: 45 MB (5.51%) - LeetSync

--- 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.java
+++ 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.java
@@ -1,22 +1,20 @@
 class Solution {
+    Integer[][][]dp;
     public int findIntegers(int n) {
         String s=Integer.toBinaryString(n);
-        Integer[][][]dp=new Integer[s.length()][2][2];
-        int ans =solve(s,0,1,0,dp);
-        return ans ;
-
-    }
-    public int solve(String s,int idx,int tight,int prev,Integer[][][]dp){
+        dp=new Integer[s.length()+1][2][11];
+        return helper(s,0,1,-1);
+    }public int helper(String s,int idx,int tight,int prev){
         if(idx==s.length())return 1;
-        if(dp[idx][tight][prev]!=null)return dp[idx][tight][prev];
         int lb=0;
-        int ub=(tight==1)?s.charAt(idx)-'0':1;
+        if(dp[idx][tight][prev+1]!=null)return dp[idx][tight][prev+1];
+        int ub=tight==1?s.charAt(idx)-'0':1;
         int res=0;
-        for(int digit =lb;digit<=ub;digit++){
-            if(prev==1&&digit==1)continue;
-            int newtight=(tight==1&&digit==ub)?1:0;
-            res+=solve(s,idx+1,newtight,digit,dp);
+        for(int digit=lb;digit<=ub;digit++){
+            int nt=tight==1&&digit==ub?1:0;
+            if(digit==1&&prev==1)continue;
+            res+=helper(s,idx+1,nt,digit);
         }
-        return dp[idx][tight][prev]= res;
+        return dp[idx][tight][prev+1]=res;
     }
 }
