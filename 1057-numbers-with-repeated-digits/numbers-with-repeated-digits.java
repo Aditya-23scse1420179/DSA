a9a3b483fd8a54975e38b4446ee232a740ce825b
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-23 22:55:02 +0530

    Time: 91 ms (14.49%) | Memory: 50.2 MB (8.70%) - LeetSync

--- 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.java
+++ 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.java
@@ -1,29 +1,25 @@
 class Solution {
-    public int numDupDigitsAtMostN(int n){
+    Integer[][][][][]dp;
+    public int numDupDigitsAtMostN(int n) {
         String s=String.valueOf(n);
-        Integer[][][][][]dp=new Integer[s.length()+1][2][2][1024][2];
-        int ans=solve(s,0,1,0,0,1,dp);
-        return ans;
-    }
-    public static int solve(String s,int idx,int tight,int repeated,int mask,int lz,Integer[][][][][]dp){
-        if(idx==s.length()){
-            return repeated;
-        }
-        if(dp[idx][tight][repeated][mask][lz]!=null)return dp[idx][tight][repeated][mask][lz];
+        dp=new Integer[s.length()+1][2][2][1023][2];
+        return helper(s,0,1,0,0,1);
+    }public int helper(String s,int idx,int tight,int repeated,int mask,int lz){
+        if(idx==s.length())return repeated;
         int lb=0;
-        int ub=(tight==1)?s.charAt(idx)-'0':9;
+        if(dp[idx][tight][repeated][mask][lz]!=null)return dp[idx][tight][repeated][mask][lz];
+        int ub=tight==1?s.charAt(idx)-'0':9;
         int res=0;
-        for(int digit =lb;digit<=ub;digit++){
-            int newtight=(tight==1&&digit==ub)?1:0;
-            int nlz=(lz==1&&digit==0)?1:0;
-            if(nlz==1){
-                res+=solve(s,idx+1,newtight,repeated,mask,nlz,dp);
+        for(int digit=lb;digit<=ub;digit++){
+            int nt=tight==1&&digit==ub?1:0;
+            if(lz==1&&digit==0){
+                res+=helper(s,idx+1,nt,repeated,mask,lz);
             }else{
-                int isUsed =1&(mask>>digit);
-                int newReapeted=(repeated==1||isUsed==1)?1:0;
-                int newMask=(1<<(digit)|mask);
-                res+=solve(s,idx+1,newtight,newReapeted,newMask,nlz,dp);
-            }    
+                int isused=((mask>>digit)&1);
+                int nr=repeated==1||isused==1?1:0;
+                int nm=(1<<digit)|mask;
+                res+=helper(s,idx+1,nt,nr,nm,0);
+            }
         }
         return dp[idx][tight][repeated][mask][lz]=res;
     }
