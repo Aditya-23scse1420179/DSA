4a5835a9aaa2cd3b6a2dd8bc4e11924461358de4
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-25 23:29:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (87.18%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,26 +1,30 @@
 class Solution {
-    public void gameOfLife(int[][] b) {
-        int arr[][]=new int[b.length][b[0].length];
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
+    public void gameOfLife(int[][] board) {
+        int n=board.length,m=board[0].length;
+        int[][]b=new int[n][m];
+        for(int i=0;i<n;i++){
+            // int count=0;
+            for(int j=0;j<m;j++){
                 int count=0;
-                if(i>0 && b[i-1][j]==1) count++;
-                if(i<b.length-1 && b[i+1][j]==1) count++;
-                if(j>0 && b[i][j-1]==1) count++;
-                if(j<b[0].length-1 && b[i][j+1]==1) count++;
-                if(i>0 && j>0 && b[i-1][j-1]==1) count++;
-                if(i>0 && j<b[0].length-1 && b[i-1][j+1]==1) count++;
-                if(i<b.length-1 && j>0 && b[i+1][j-1]==1) count++;
-                if(i<b.length-1 && j<b[0].length-1 && b[i+1][j+1]==1) count++;
+                if(i>0&&j>0&&board[i-1][j-1]==1)count++;//uL
+                if(i>0&&board[i-1][j]==1)count++;//u
+                if(i>0&&j<m-1&&board[i-1][j+1]==1)count++;//ur
+                if(j>0&&board[i][j-1]==1)count++;//l
+                if(j<m-1&&board[i][j+1]==1)count++;//r
+                if(i<n-1&&j>0&&board[i+1][j-1]==1)count++;//ll
+                if(i<n-1&&board[i+1][j]==1)count++;//down
+                if(i<n-1&&j<m-1&&board[i+1][j+1]==1)count++;
                 
-                if(b[i][j]==1 && count==2 || count==3) arr[i][j]=1;
-                else if(b[i][j]==0 && count==3) arr[i][j]=1;
-                else arr[i][j]=0;
+                if(board[i][j]==1&&count==2||count==3){
+                    b[i][j]=1;
+                }
+                else if(board[i][j]==0&&count==3)b[i][j]=1;
+                else b[i][j]=0;
             }
         }
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                b[i][j]=arr[i][j];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                board[i][j]=b[i][j];
             }
         }
     }
