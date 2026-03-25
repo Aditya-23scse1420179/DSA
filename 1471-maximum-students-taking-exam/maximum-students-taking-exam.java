e105f9ee69fbed89cbdb452add5589cd4a02177d
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-25 10:29:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (84.31%) - LeetSync

--- 1471-maximum-students-taking-exam/maximum-students-taking-exam.java
+++ 1471-maximum-students-taking-exam/maximum-students-taking-exam.java
@@ -1,40 +1,42 @@
 class Solution {
-    int n,m;
-    int [][]matr,matc;
-    static int[]dr={0,0,-1,-1,1,1};
+    static int []dr={0,0,-1,-1,1,1};
     static int[]dc={-1,1,-1,1,-1,1};
+    static int n;
+    static int m;
+    static int[][]matA;
+    static int[][]matB;
+    static boolean[][]visit;
     public int maxStudents(char[][] seats) {
         n=seats.length;
         m=seats[0].length;
-        matr=new int[n][m];
-        matc=new int[n][m];
         int count=0;
         int total=0;
+        matA=new int[n][m];
+        matB=new int[n][m];
         for(int i=0;i<n;i++){
-            Arrays.fill(matr[i],-1);
-            Arrays.fill(matc[i],-1);
+            Arrays.fill(matA[i],-1);
+            Arrays.fill(matB[i],-1);
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(seats[i][j]=='#')continue;
                 if(j%2==0){
-                    boolean[][]visit=new boolean[n][m];
-                    if(dfs(i,j,seats,visit))count++;
+                    visit=new boolean[n][m];
+                    if(dfs(i,j,seats))count++;
                 }
                 total++;
             }
         }
         return total-count;
-    }public boolean dfs(int r,int c,char[][]seats,boolean[][]visit){
+    }public boolean dfs(int r,int c,char[][]seats){
         for(int i=0;i<6;i++){
             int nr=r+dr[i];
             int nc=c+dc[i];
-            // int u=nr*m+nc;
-            if(nr>=0&&nc>=0&&nr<n&&nc<m&&!visit[nr][nc]&&seats[nr][nc]!='#'){
-                visit[nr][nc]=true;
-                if(matr[nr][nc]==-1||dfs(matr[nr][nc],matc[nr][nc],seats,visit)){
-                    matr[nr][nc]=r;
-                    matc[nr][nc]=c;//idx store kar lo 
+            if(nr>=0&&nr<n&&nc>=0&&nc<m&&seats[nr][nc]=='.'&&!visit[nr][nc]){
+                 visit[nr][nc]=true;
+                if(matA[nr][nc]==-1||dfs(matA[nr][nc],matB[nr][nc],seats)){
+                    matA[nr][nc]=r;
+                    matB[nr][nc]=c;
                     return true;
                 }
             }
