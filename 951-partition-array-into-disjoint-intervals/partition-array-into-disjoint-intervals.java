5a7eaa06656c59e09d71245dea7683c6d07bd7c4
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-27 22:46:04 +0530

    Time: 3 ms (88.96%) | Memory: 86.4 MB (70.04%) - LeetSync

--- 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.java
+++ 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.java
@@ -1,14 +1,13 @@
 class Solution {
     public int partitionDisjoint(int[] nums) {
-        int ans=1,lmax=nums[0],max=nums[0];
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]<lmax){
-                ans=i+1;
-                lmax=max;
-            }else{
-                max=Math.max(max,nums[i]);
+        int ans = 0, lmax = nums[0], max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);   // hamesha update karo
+            if (nums[i] < lmax) {
+                ans = i;        // partition index update
+                lmax = max;     // left max ko extend karo
             }
         }
-        return ans;
+        return ans + 1;
     }
 }
