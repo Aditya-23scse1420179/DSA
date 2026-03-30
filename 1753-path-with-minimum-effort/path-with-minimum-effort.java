946338f4c8a0a2fae4d29b61db267530eb6413d5
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-30 14:34:24 +0530

    Time: 41 ms (62.53%) | Memory: 47.9 MB (68.24%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,6 +1,6 @@
 class Solution {
-    static int[]dr={-1,1,0,0};
-    static int []dc={0,0,-1,1};
+    static int []dr={-1,1,0,0};
+    static int[]dc={0,0,-1,1};
     class pair{
         int row,col,dist;
         public pair(int row,int col,int dist){
@@ -14,21 +14,22 @@ class Solution {
         int m=heights[0].length;
         int[][]dist=new int[n][m];
         for(int[]a:dist){
-            Arrays.fill(a,(int)1e9);
-        }dist[0][0]=0;
+            Arrays.fill(a,Integer.MAX_VALUE);
+        }
         PriorityQueue<pair>q=new PriorityQueue<>((a,b)->Integer.compare(a.dist,b.dist));
         q.offer(new pair(0,0,0));
+        dist[0][0]=0;
         while(!q.isEmpty()){
             pair curr=q.remove();
             int r=curr.row;
             int c=curr.col;
             int w=curr.dist;
+            if(r==n-1&&c==m-1)return w; 
             if(dist[r][c]<w)continue;
-            if(r==n-1&&c==m-1)return w;
             for(int i=0;i<4;i++){
                 int nr=r+dr[i];
                 int nc=c+dc[i];
-                if(nr<0||nr>=n||nc<0||nc>=m)continue;
+                if(nr<0||nr>=n||nc<0||nc>=m)continue;//agar >=0 to and condition 
                 int d=Math.max(w,Math.abs(heights[nr][nc]-heights[r][c]));
                 if(d<dist[nr][nc]){
                     dist[nr][nc]=d;
