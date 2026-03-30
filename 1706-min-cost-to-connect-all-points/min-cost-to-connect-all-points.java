643ac8d99ff1a5fa8a1e2ba33ce81d77b675690a
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-03-30 12:25:09 +0530

    Time: 1280 ms (5.65%) | Memory: 128.8 MB (24.71%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -0,0 +1,36 @@
+class Solution {
+    class pair{
+        int node;
+        int cost;
+        public pair(int node,int cost){
+            this.node=node;
+            this.cost=cost;
+        }
+    }
+    public int minCostConnectPoints(int[][] points) {
+        int n=points.length;
+        boolean[]visit=new boolean[n];
+        PriorityQueue<pair>q=new PriorityQueue<>((a,b)->a.cost-b.cost);
+        q.offer(new pair(0,0));
+           int ans=0;
+        while(!q.isEmpty()){
+            pair curr=q.remove();
+            int u=curr.node;
+            int w=curr.cost;
+            
+            if(visit[u]==true)continue;
+            if(!visit[u]){
+                ans+=w;
+            }
+
+            visit[u]=true;
+
+            for(int i=0;i<n;i++){
+                int cost=Math.abs(points[u][0]-points[i][0])+
+                        Math.abs(points[u][1]-points[i][1]);
+                        q.offer(new pair(i,cost));
+            }
+        }
+        return ans;
+    }
+}
