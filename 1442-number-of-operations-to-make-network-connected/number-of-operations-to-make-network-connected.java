ec28f60ad8d9296027b0efac16968c818c6617ff
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-01 20:02:15 +0530

    Time: 3 ms (99.40%) | Memory: 65 MB (98.50%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -0,0 +1,35 @@
+class Solution {
+    int[] par;
+
+    private int find(int x) {
+        if(par[x] == x) return x;
+        par[x] = find(par[x]);
+        return par[x];
+    }
+
+    private void union(int x, int y) {
+        int parX = find(x);
+        int parY = find(y);
+
+        if(parX != parY) {
+            par[parY] = parX;
+        }
+    }
+
+    public int makeConnected(int n, int[][] connections) {
+        par = new int[n];
+        for(int i=0;i<n;i++) par[i] = i;
+        if(connections.length < n-1) return -1;
+
+        for(int[] conn:connections) {
+            union(conn[0], conn[1]);
+        }
+
+        int rem = 0;
+        for(int i=0;i<n;i++) {
+            if(par[i] == i) rem++;
+        }
+
+        return rem-1;
+    }
+}
