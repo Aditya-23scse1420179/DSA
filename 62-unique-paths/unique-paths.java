276eaf170fd7fdd28b923b58cabddc5589514d98
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-05 22:54:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (72.18%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,17 +1,13 @@
 class Solution {
+    Integer[][]dp;
     public int uniquePaths(int m, int n) {
-        int[][]dp = new int[m][n];
-        for(int j=0;j<n;j++){
-            dp[m-1][j]=1;
-        }
-        for(int i=0;i<m;i++){
-            dp[i][n-1]=1;
-        }
-        for(int i=m-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                dp[i][j]=dp[i+1][j]+dp[i][j+1];
-            }
-        }
-        return dp[0][0];
+        dp=new Integer[n+1][m+1];
+        return helper(m,n,0,0);
+    }public int helper(int m,int n,int r,int c){
+        if(r==n-1||c==m-1)return 1;
+        if(dp[r][c]!=null)return dp[r][c];
+        int right=helper(m,n,r+1,c);
+        int left=helper(m,n,r,c+1);
+        return dp[r][c]=left+right;
     }
 }
