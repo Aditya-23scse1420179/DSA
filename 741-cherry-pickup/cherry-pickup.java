2d608996d06f152234c30c46c90b1a61ebf08ea0
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-06 14:29:01 +0530

    Time: 261 ms (5.08%) | Memory: 283.8 MB (9.06%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.java
+++ 741-cherry-pickup/cherry-pickup.java
@@ -1,23 +1,27 @@
 class Solution {
-    Integer[][][]dp=new Integer[50][50][50];
-    int[][]grid;
     int n;
+    int m;
+    int[][]grid;
+    Integer[][][][]dp;
     public int cherryPickup(int[][] g) {
+        n=g.length;
+        m=g[0].length;
         grid=g;
-        n=grid.length;
-        return Math.max(0,helper(0,0,0));
-
-    }public int helper(int r1,int c1,int c2){
-        int r2=r1+c1-c2;
-        if(r1>=n||c1>=n||r2>=n||c2>=n||grid[r1][c1]==-1||grid[r2][c2]==-1)return Integer.MIN_VALUE;
-        if(r1==n-1&&c1==n-1)return grid[r1][c1];
-        if(dp[r1][c1][c2]!=null)return dp[r1][c1][c2];
+        dp=new Integer[50][50][50][50];
+        return Math.max(0,helper(0,0,0,0));
+    }public int helper(int r1,int c1,int r2,int c2){
+        if(r1>=n||c1>=m||r2>=n||c2>=m||grid[r1][c1]==-1||grid[r2][c2]==-1)return Integer.MIN_VALUE;
         int ans=grid[r1][c1];
+        if(r1==n-1&&c1==m-1)return grid[r1][c1];
+        if(dp[r1][c1][r2][c2]!=null)return dp[r1][c1][r2][c2];
         if(c1!=c2)ans+=grid[r2][c2];
-        int temp=Math.max(helper(r1,c1+1,c2+1),helper(r1+1,c1,c2));//right right r1c1r2c2 ko ak sath leke visualise kar
-        temp=Math.max(temp,helper(r1+1,c1,c2+1));//temp aur down right ka max
-        temp=Math.max(temp,helper(r1,c1+1,c2));//temp aur right down ka max
-        ans+=temp;// fir temp me add kar do 
-        return dp[r1][c1][c2]=ans;
+        int temp=Math.max(
+            helper(r1+1,c1,r2+1,c2),
+            Math.max(helper(r1,c1+1,r2,c2+1),
+            Math.max(helper(r1+1,c1,r2,c2+1),
+            helper(r1,c1+1,r2+1,c2)))
+        );
+        ans+=temp;
+        return dp[r1][c1][r2][c2]=ans;
     }
 }
