b7bdd9cef91168139d79f6329cb33a6aaa87f877
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-07 12:29:05 +0530

    Time: 78 ms (70.90%) | Memory: 200.2 MB (12.71%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,36 +1,42 @@
-class Solution {
+class Solution {//)(2^n*n)Tle use bsearch 2*nlogn
     int[]st;
     int[]et;
-    int []p;
+    int[]p;
+    int n;
     Integer[][]dp;
-    public int maxValue(int[][] e, int k) {
-        int n=e.length;
-        dp=new Integer[n][k+1];
+    public int maxValue(int[][] events, int k) {
+        n=events.length;
+        dp=new Integer[n+1][k+1];
         st=new int[n];
         et=new int[n];
         p=new int[n];
-        Arrays.sort(e,(a,b)->a[0]-b[0]);
+        Arrays.sort(events,(a,b)->a[0]-b[0]);//to array events like timeline so when we skip idx+1 then we can direct access next 
         for(int i=0;i<n;i++){
-            st[i]=e[i][0];
-            et[i]=e[i][1];
-            p[i]=e[i][2];
+            st[i]=events[i][0];
+            et[i]=events[i][1];
+            p[i]=events[i][2];
         }
         return helper(0,k);
     }public int helper(int idx,int k){
-        if(idx==st.length||k==0)return 0;
+        if(idx==n||k==0)return 0;
         if(dp[idx][k]!=null)return dp[idx][k];
         int ntake=helper(idx+1,k);
-        int nidx=bs(idx,et[idx]);
-        int take=helper(nidx,k-1)+p[idx];
-        return dp[idx][k]=Math.max(ntake,take);
-    }public int bs(int idx,int ett){
-        int l=idx+1,r=st.length-1;
+        int next = bsearch(idx,et[idx]);
+        // while(next<n&&st[next]<=et[idx]){
+        //     next++;
+        // }
+        int take=helper(next,k-1)+p[idx];
+        
+        return dp[idx][k]=Math.max(take,ntake);
+    }public int bsearch(int idx,int et){
+        int l=idx+1;
+        int h=n-1;
         int ans=st.length;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(st[mid]>=ett+1){
+        while(l<=h){
+            int mid=l+h>>1;
+            if(st[mid]>=et+1){
                 ans=mid;
-                r=mid-1;
+                h=mid-1;
             }else{
                 l=mid+1;
             }
