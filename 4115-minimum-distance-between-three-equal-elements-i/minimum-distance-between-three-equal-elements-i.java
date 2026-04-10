b10fd075268bc678987300ef5d6fab7e183505d1
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-10 11:38:42 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (91.00%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
@@ -5,10 +5,12 @@ class Solution {
         
         for(int i=0;i<n-2;i++){
             for(int j=i+1;j<n-1;j++){
+                if(nums[i]!=nums[j])continue;
                 for(int k=j+1;k<n;k++){
                     if(nums[i]==nums[j]&&nums[j]==nums[k]){
                         int res=Math.abs(i-j)+Math.abs(j-k)+Math.abs(k-i);
                         min=Math.min(min,res);
+                        break;
                     }
                 }
             }
