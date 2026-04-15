710ff8ce249ec74697a9bd97cc418ebc851f13a2
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-15 16:40:12 +0530

    Time: 2586 ms (5.01%) | Memory: 45.3 MB (44.26%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,23 +1,23 @@
 class Solution {
     public String longestPalindrome(String s) {
-        // if (s == null||s.length()< 1) return "";
-        int start =0,end =0;
-        for (int i =0;i <s.length();i++){
-            int len1 =expandAroundCenter(s, i,i);     
-            int len2 =expandAroundCenter(s, i,i+1); 
-            int len =Math.max(len1, len2);   
-            if (len>end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+        String ans="";
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                String sub=s.substring(i,j+1);
+                if(ispal(sub)&&sub.length()>ans.length()){
+                    ans=sub;
+                }
             }
         }
-        return s.substring(start,end+1);
-    }
-    private int expandAroundCenter(String s, int left, int right) {
-        while (left>=0&&right<s.length() &&s.charAt(left)== s.charAt(right)) {
-            left--;
-            right++;
+        return ans;
+    }public boolean ispal(String s){
+        int l=0,r=s.length()-1;
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r))return false;
+            l++;
+            r--;
         }
-        return right-left-1;
+        return true;
     }
 }
