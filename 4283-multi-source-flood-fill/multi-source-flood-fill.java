5e41757331ca27c07500ad95a858dd83e26ba30c
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-19 20:00:46 +0530

    Time: 83 ms (51.56%) | Memory: 94.3 MB (82.75%) - LeetSync

--- 4283-multi-source-flood-fill/multi-source-flood-fill.java
+++ 4283-multi-source-flood-fill/multi-source-flood-fill.java
@@ -2,10 +2,11 @@ class Solution {
     static int []dr={-1,1,0,0};
     static int[]dc={0,0,-1,1};
     class pair{
-        int row,col;
-        public pair(int row,int col){
+        int row,col,color;
+        public pair(int row,int col,int color){
             this.row=row;
             this.col=col;
+            this.color=color;
         }
     }
     public int[][] colorGrid(int n, int m, int[][] sources) {
@@ -14,22 +15,21 @@ class Solution {
         Arrays.sort(sources,(a,b)->b[2]-a[2]);
         for(int[]a:sources){
             int r=a[0],c=a[1],col=a[2];
-            q.offer(new pair(r,c));
+            q.offer(new pair(r,c,col));
             grid[r][c]=col;
         }
         while(!q.isEmpty()){
             pair curr=q.remove();
             int r=curr.row;
             int c=curr.col;
-            // int col=curr.color;
-            int color=grid[r][c];
+            int col=curr.color;
             for(int i=0;i<4;i++){
                 int nr=r+dr[i];
                 int nc=c+dc[i];
                 if(nr>=0&&nr<n&&nc>=0&&nc<m&&grid[nr][nc]==0){
                     // if(sources[nr][nc]<col){
-                        grid[nr][nc]=color;
-                        q.offer(new pair(nr,nc));
+                        grid[nr][nc]=col;
+                        q.offer(new pair(nr,nc,col));
                     //}
                 }
             }
