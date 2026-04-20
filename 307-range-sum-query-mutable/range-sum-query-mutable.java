5e7b7633b85bcd0b14c49ccc736cc5c72b211a5d
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-20 20:50:53 +0530

    Time: 90 ms (67.65%) | Memory: 137.1 MB (6.55%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/range-sum-query-mutable.java
@@ -1,33 +1,33 @@
 class NumArray {
-    static int[]st;
+    static int st[];
     int n;
     public NumArray(int[] nums) {
         n=nums.length;
         st=new int[4*n];
-        build(nums,0,0,n-1);
-    }public void build(int[]nums,int idx,int l,int r){
+        build(0,0,n-1,nums);
+    }public void build(int idx,int l,int r,int []nums){
         if(l==r){
             st[idx]=nums[l];
             return;
         }
-        int mid=l+(r-l)/2;
-        build(nums,2*idx+1,l,mid);
-        build(nums,2*idx+2,mid+1,r);
+        int mid=l+r>>1;
+        build(2*idx+1,l,mid,nums);
+        build(2*idx+2,mid+1,r,nums);
         st[idx]=st[2*idx+1]+st[2*idx+2];
     }
     
     public void update(int index, int val) {
-        updateT(0,index,val,0,n-1);
-    }public void updateT(int idx,int i,int val,int l,int r){
+        updatee(0,0,n-1,val,index);
+    }public void updatee(int idx,int l,int r,int val,int i){
         if(l==r){
             st[idx]=val;
             return;
         }
-        int mid=l+(r-l)/2;
+        int mid=l+r>>1;
         if(i<=mid){
-            updateT(2*idx+1,i,val,l,mid);
+            updatee(2*idx+1,l,mid,val,i);
         }else{
-            updateT(2*idx+2,i,val,mid+1,r);
+            updatee(2*idx+2,mid+1,r,val,i);
         }
         st[idx]=st[2*idx+1]+st[2*idx+2];
     }
@@ -35,9 +35,9 @@ class NumArray {
     public int sumRange(int left, int right) {
         return sum(0,0,n-1,left,right);
     }public int sum(int idx,int l,int r,int qs,int qe){
-        if(r<qs||l>qe)return 0;
+        if(qs>r||l>qe)return 0;
         if(l>=qs&&r<=qe)return st[idx];
-        int mid=l+(r-l)/2;
+        int mid=l+r>>1;
         int left=sum(2*idx+1,l,mid,qs,qe);
         int right=sum(2*idx+2,mid+1,r,qs,qe);
         return left+right;
