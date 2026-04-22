3e32fbf7bef686d6ae86ed70838ec7540d225f05
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-22 22:07:58 +0530

    Time: 96 ms (18.85%) | Memory: 72.7 MB (25.97%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,22 +1,22 @@
 class Solution {
+    Boolean[][]dp;
     public boolean canPartition(int[] nums) {
-
-        int sum =0;
-        for(int x:nums){
-            sum+=x;
+        int sum=0;
+        for(int a:nums){
+            sum+=a;
         }
+        
         if(sum%2!=0)return false;
-        int target = sum/2;
-        Boolean[][]dp= new Boolean[nums.length][target+1];
-        return helper(target,nums,0,dp);
-    }
-    public boolean helper(int target,int[]nums,int idx,Boolean[][]dp){
+        int target=sum/2;
+        dp=new Boolean[nums.length][target+1];
+        return helper(0,target,nums);
+    }public boolean helper(int idx,int target,int[]nums){
         if(target<0)return false;
         if(target==0)return true;
-        if(idx>=nums.length)return false;
-        if(dp[idx][target]!=null) return dp[idx][target];
-        boolean res=helper(target-nums[idx],nums,idx+1,dp)||helper(target,nums,idx+1,dp);
-        dp[idx][target]=res;
-        return res;
+        if(idx==nums.length)return false;
+        if(dp[idx][target]!=null)return dp[idx][target];
+        boolean take=helper(idx+1,target-nums[idx],nums);
+        boolean ntake=helper(idx+1,target,nums);
+        return dp[idx][target]=take||ntake;
     }
 }
