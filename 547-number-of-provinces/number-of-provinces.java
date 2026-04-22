6d36d7445dce7c25eaebcf8121eefcb465f7d585
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-22 14:39:14 +0530

    Time: 2 ms (43.11%) | Memory: 47.2 MB (75.35%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,38 +1,37 @@
 class Solution {
-    int[]parent;
-    int[]rank;
-    public int findCircleNum(int[][]connect) {
-        int n=connect.length;
+    int []parent;
+    int []rank;
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
         parent=new int[n];
+        rank=new int[n];
         for(int i=0;i<n;i++){
             parent[i]=i;
         }
-        rank=new int[n];
         for(int i=0;i<n;i++){
-            for(int j=1;j<n;j++){
-                if(connect[i][j]==1){
-                    unionrank(i,j);
-                }
+            for(int j=0;j<isConnected[0].length;j++){
+                if(isConnected[i][j]==1)union(i,j);
             }
-        }int count=0;
-        for(int i=0;i<n;i++){
-            if(parent[i]==i)count++;
         }
-        return count;        
+        int comp=0;
+        for(int i=0;i<n;i++){
+            if(parent[i]==i)comp++;
+        }
+        return comp;
     }public int find(int x){
         if(x==parent[x])return x;
         return parent[x]=find(parent[x]);
-    }public boolean unionrank(int x,int y){
-        int p_x=find(x);
-        int p_y=find(y);
-        if(p_x==p_y)return false;//cycle
-        if(rank[p_x]>rank[p_y]){
-            parent[p_y]=p_x;
-        }else if(rank[p_y]>rank[p_x]){
-            parent[p_x]=p_y;
+    }public boolean union(int x,int y){
+        int px=find(x);
+        int py=find(y);
+        if(px==py)return false;
+        if(rank[px]>rank[py]){
+            parent[py]=px;
+        }else if(rank[py]>rank[px]){
+            parent[px]=py;
         }else{
-            parent[p_x]=p_y;
-            rank[p_x]++;
+            parent[px]=py;
+            rank[py]++;
         }
         return true;
     }
