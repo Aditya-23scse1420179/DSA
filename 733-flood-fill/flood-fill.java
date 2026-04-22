793d6360af317b2862b5bd0472e893078ecabeed
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-23 00:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (64.26%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,22 +1,20 @@
 class Solution {
-    static int[] dr ={-1, 1,0, 0};
-    static int[] dc ={0,0,1,-1};
-    static int rowL;
-    static int colL;
+    static int dr[]={-1,1,0,0};
+    static int dc[]={0,0,-1,1};
+    int n;
+    int m;
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        rowL=image.length;
-        colL =image[0].length;
-        int originalColor =image[sr][sc];
-        if (originalColor!=color) {
-            dfs(sr,sc,image,originalColor,color);
+        n=image.length;
+        m=image[0].length;
+        if(image[sr][sc]!=color){
+            dfs(sr,sc,image,color,image[sr][sc]);
         }
         return image;
-    }
-    public void dfs(int sr,int sc,int[][] image,int originalColor,int newColor) {
-        if (sr<0||sc<0||sr>=rowL||sc >=colL||image[sr][sc]!= originalColor) return;
-        image[sr][sc] =newColor;
-        for (int i=0;i< 4;i++) {
-            dfs(sr+dr[i],sc+dc[i],image,originalColor,newColor);
+    }public void dfs(int sr,int sc,int[][]image,int color,int ncolor){
+        if(sr<0||sr>=n||sc<0||sc>=m||image[sr][sc]!=ncolor)return;
+        image[sr][sc]=color;
+        for(int i=0;i<4;i++){
+            dfs(sr+dr[i],sc+dc[i],image,color,ncolor);
         }
     }
 }
