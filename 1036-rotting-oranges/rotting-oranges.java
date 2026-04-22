4457d389c340c11efac8343bcb64da2c5def49a8
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-22 15:06:37 +0530

    Time: 2 ms (90.36%) | Memory: 44.2 MB (58.49%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,46 +2,49 @@ class Solution {
     static int[]dr={-1,1,0,0};
     static int[]dc={0,0,-1,1};
     static class pair{
-        int r,c;
-        public pair(int r,int c){
+        int r,c,t;
+        public pair(int r,int c,int t){
             this.r=r;
             this.c=c;
+            this.t=t;
         }
     }
+    int max=0;
     public int orangesRotting(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
         int fresh=0;
         Queue<pair>q=new LinkedList<>();
+        boolean[][]visit=new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.offer(new pair(i,j));
+                    q.offer(new pair(i,j,0));
+                    visit[i][j]=true;
                 }else if(grid[i][j]==1)fresh++;
             }
-        }int level=0;
+        }
         while(!q.isEmpty()){
-            boolean found=false;
             int size=q.size();
             while(size-->0){
                 pair curr=q.poll();
                 int row=curr.r;
                 int col=curr.c;
+                int ti=curr.t;
+                max=Math.max(max,ti);
                 for(int i=0;i<4;i++){
                     int nr=row+dr[i];
                     int nc=col+dc[i];
                     if(nr<0||nr>=n||nc<0||nc>=m)continue;
-                    if(grid[nr][nc]==1){
+                    if(grid[nr][nc]==1&&!visit[nr][nc]){
                         fresh--;
-                        grid[nr][nc]=2;
-                        found=true;
-                        q.offer(new pair(nr,nc));
+                        visit[nr][nc]=true;
+                        q.offer(new pair(nr,nc,ti+1));
                     }
                 }
             }
-            if(found)level++;
         }
-        return fresh==0?level:-1;
+        return fresh==0?max:-1;
 
     }
 }
