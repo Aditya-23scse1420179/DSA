b7068ba9dd4ca5c5d4d5f39a104d4d9418896bc1
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-22 22:30:02 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (55.38%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,14 +11,11 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode>set=new HashSet<>();
-        ListNode node=head;
-        while(node!=null){
-            if(set.contains(node)){
-                return true;
-            }
-            set.add(node);
-            node=node.next;
+        ListNode fast=head,slow=head;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(slow==fast)return true;
         }
         return false;
     }
