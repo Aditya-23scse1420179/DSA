4eb0a7f72e1c6168fdfd8828b98629ff82e63038
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-22 15:00:08 +0530

    Time: 19 ms (40.57%) | Memory: 46.9 MB (28.84%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -2,20 +2,21 @@ class Solution {
     public String longestPalindrome(String s) {
         String ans="";
         for(int i=0;i<s.length();i++){
-            String odd=ispal(s,i,i);
-            String even=ispal(s,i,i+1);
+            String odd=pal(s,i,i);
+            String even=pal(s,i,i+1);
             if(odd.length()>ans.length()){
                 ans=odd;
-            }if(even.length()>ans.length()){
+            }
+            if(even.length()>ans.length()){
                 ans=even;
             }
         }
         return ans;
-    }public String ispal(String s,int l,int r){
-        while(l>=0&&r<s.length()&&s.charAt(l)==s.charAt(r)){
-            l--;;
-            r++;
+    }public String pal(String s,int i,int j){
+        while(i>=0&&j<s.length()&&s.charAt(i)==s.charAt(j)){
+            i--;
+            j++;
         }
-        return s.substring(l+1,r);
+        return s.substring(i+1,j);
     }
 }
