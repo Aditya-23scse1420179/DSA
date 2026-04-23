b8d697aff13cd527e24d62f1ab5db697d0174588
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-23 16:16:21 +0530

    Time: 9 ms (95.52%) | Memory: 47.2 MB (37.84%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -1,23 +1,17 @@
 class Solution {
-public int findLongestChain(int[][] pairs) {   
-    Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
-    int memo[] = new int[pairs.length];
-    Arrays.fill(memo,-1);
-    int res = helper(pairs, 0, Integer.MIN_VALUE, memo);
-    return res;
-}
-public int helper(int pairs[][], int index, int prevPairStartingVal, int memo[]){
-    if(index >= pairs.length){
-        return 0;
+    Integer[]dp;
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs,(a,b)->a[1]-b[1]);
+        dp=new Integer[pairs.length];
+        return helper(0,pairs,-(int)1e9);
+    }public int helper(int idx,int [][]pairs,int pre){
+        if(idx>=pairs.length)return 0;
+        if(dp[idx]!=null)return dp[idx];
+        int inc=0,skip=0;
+        if(pairs[idx][0]>pre){
+            inc=1+helper(idx+1,pairs,pairs[idx][1]);
+        }
+        skip=helper(idx+1,pairs,pre);
+        return dp[idx]=Math.max(inc,skip);
     }
-    if(memo[index]!=-1)
-        return memo[index];
-    int include = 0,  skip =0;
-    if(pairs[index][0] > prevPairStartingVal){
-        include = 1 + helper(pairs,index+1,pairs[index][1],memo);
-    }
-    skip = helper(pairs, index+1, prevPairStartingVal,memo);
-    memo[index] = Math.max(include, skip);
-    return memo[index];
-}
 }
