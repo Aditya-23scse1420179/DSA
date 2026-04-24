7c3225a337575dd474b3d17eb34fa48a179b5fd0
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-25 02:41:01 +0530

    Time: 1 ms (38.05%) | Memory: 45.2 MB (11.69%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,11 +1,13 @@
 class Solution {
+    Integer[]dp;
     public int minCostClimbingStairs(int[] cost) {
-        int n=cost.length;
-        int[]dp=new int[n];
-        for(int i=0;i<n;i++){
-            if(i<2)dp[i]=cost[i];
-            else dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
-        }
-        return Math.min(dp[n-1],dp[n-2]);
+        dp=new Integer[cost.length];
+        return Math.min(helper(cost,0),helper(cost,1));
+    }public int helper(int[]cost,int idx){
+        if(idx>=cost.length)return 0;
+        if(dp[idx]!=null)return dp[idx];
+        int take1=cost[idx]+helper(cost,idx+1);
+        int take2=cost[idx]+helper(cost,idx+2);
+        return dp[idx]=Math.min(take1,take2);
     }
 }
