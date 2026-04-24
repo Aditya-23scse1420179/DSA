33c102116f8ce2a36eb1dfcfbb4bd4eb8eb05962
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-25 02:25:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (73.81%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,11 +1,15 @@
 class Solution {
+    Integer[]dp;
     public int climbStairs(int n) {
-        int[]dp = new int [n+1];
-        dp[0]=1;
-        dp[1]=2;
-        for(int i=2;i<n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n-1];
+        dp=new Integer[n];
+        return helper(n,0);
+    }public int helper(int n,int jump){
+        if(n==0)return 0;
+        if(n==1)return 1;
+        if(n==2)return 2;
+        if(dp[jump]!=null)return dp[jump];
+        int jump1=helper(n-1,jump+1);
+        int jump2=helper(n-2,jump+2);
+        return dp[jump]=jump1+jump2;
     }
 }
