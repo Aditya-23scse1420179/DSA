d748981a9e27c726bdc624e3307e6ca9f1525778
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-25 21:46:22 +0530

    Time: 1 ms (99.80%) | Memory: 49.7 MB (95.30%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,21 +1,18 @@
 class Solution {
+    int n;
+    int m;
+    Integer[][]dp;
     public int minPathSum(int[][] grid) {
-        int m=grid.length;
-        int n= grid[0].length;
-        int [][]dp=new int[m][n];
-        dp[0][0]=grid[0][0];
-        for(int j=1;j<n;j++){
-            dp[0][j]=dp[0][j-1]+grid[0][j];
-        }
-        for(int i=1;i<m;i++){
-            dp[i][0]= dp[i-1][0]+grid[i][0];
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] =grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-
-        return dp[m - 1][n - 1];
+        n=grid.length;
+        m=grid[0].length;
+        dp=new Integer[n+1][m+1];
+        return helper(grid,0,0);
+    }public int helper(int[][]grid,int i,int j){
+        if(i==n-1&&j==m-1)return grid[i][j];
+        if(i>=n||j>=m)return (int)1e9;
+        if(dp[i][j]!=null)return dp[i][j];
+        int down=helper(grid,i+1,j);
+        int right=helper(grid,i,j+1);
+        return dp[i][j]=grid[i][j]+Math.min(down,right);
     }
 }
