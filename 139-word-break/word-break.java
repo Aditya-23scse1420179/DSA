1cac11b34110c09f784c5e226239c172a77acebf
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-26 16:54:23 +0530

    Time: 10 ms (11.79%) | Memory: 46.1 MB (46.20%) - LeetSync

---
+++ 139-word-break/word-break.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String>set=new HashSet<>(wordDict);
+        boolean[]dp=new boolean[s.length()+1];
+        dp[0]=true;
+        for(int i=0;i<s.length();i++){
+            if(!dp[i])continue;
+            for(int j=i+1;j<=s.length();j++){
+                if(set.contains(s.substring(i,j))){
+                    dp[j]=true;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
