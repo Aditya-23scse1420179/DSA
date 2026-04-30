e258b40154b7a84322e5a73fbf4bbbb29bfa0691
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-04-30 23:41:09 +0530

    Time: 40 ms (7.70%) | Memory: 79.8 MB (7.53%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,11 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        int[][] dp = new int[n + 1][2];
-        for (int i = n-1;i>=0;i--){
-            dp[i][1] = Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);          // buy or skip
-            dp[i][0] = Math.max(prices[i]+dp[i+1][1]-fee,dp[i+1][0]);     // sell (pay fee) or skip
+        int[][] dp = new int[n][2];
+
+        for(int[] row : dp) Arrays.fill(row, -1);
+
+        return findMax(dp, prices, 0, 1, fee);
+    }
+
+    private int findMax(int[][] dp, int[] prices, int i, int canBuy, int fee) {
+        if (i == prices.length) return 0;
+
+        if (dp[i][canBuy] != -1) return dp[i][canBuy];
+
+        if (canBuy == 1) {
+            return dp[i][canBuy] = Math.max(
+                -prices[i] + findMax(dp, prices, i + 1, 0, fee),   // buy
+                findMax(dp, prices, i + 1, 1, fee)                   // skip
+            );
+        } else {
+            return dp[i][canBuy] = Math.max(
+                prices[i] - fee + findMax(dp, prices, i + 1, 1, fee),  // sell (pay fee)
+                findMax(dp, prices, i + 1, 0, fee)                       // skip
+            );
         }
-        return dp[0][1];
     }
 }
