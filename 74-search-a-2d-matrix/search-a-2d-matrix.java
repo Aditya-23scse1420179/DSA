ba3a406178844ad8ca0d09ba1cf0f196c8a5db98
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-02 19:00:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (61.26%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,35 +1,32 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) { 
-            int low=0;int high=matrix.length-1;
-            int row=-1;
-            while(low<=high){
-                int mid=low + (high-low)/2;
-                if(matrix[mid][matrix[0].length-1]==target)return true;
-                else if(matrix[mid][matrix[0].length-1]>target){
-                    row=mid;
-                    high=mid-1;
-                }else{
-                    low=mid+1;
-                }
-            }  
-            if(row==-1)return false;
- 
-                int left=0;
-                int right=matrix[0].length-1;
-                while(left<=right){
-                    int mid = left + (right - left) / 2;
-                    if(matrix[row][mid]==target)return true;
-                    else if(matrix[row][mid]<target){
-                        left = mid+1;
-                    }
-                    else if(matrix[row][mid]>target){
-                        right=mid-1;
-                    }
-                }
-                return false;
-            
-            
-       
-        
+    public boolean searchMatrix(int[][] mat, int target) { 
+        int n=mat.length;
+        int m=mat[0].length;
+        int low=0, high=n-1;
+        int row=-1;
+        while(low<=high){
+            int mid=low + (high-low)/2;
+            if(mat[mid][m-1]>target){
+                row=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+            if(mat[mid][m-1]==target)return true;
+        }  
+        if(row==-1)return false;
+        int left=0;
+        int right=m-1;
+        while(left<=right){
+            int mid = left + (right - left) / 2;
+            if(mat[row][mid]<target){
+                left = mid+1;
+            }
+            else if(mat[row][mid]>target){
+                right=mid-1;
+            }
+            if(mat[row][mid]==target)return true;
+        }
+        return false;
     }
 }
