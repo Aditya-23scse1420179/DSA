ff187754df0edbbd52d67ac752af5abfc9ed5b27
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-03 15:06:10 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (68.55%) - LeetSync

--- 449-serialize-and-deserialize-bst/serialize-and-deserialize-bst.java
+++ 449-serialize-and-deserialize-bst/serialize-and-deserialize-bst.java
@@ -8,47 +8,16 @@
  * }
  */
 public class Codec {
-
+    static TreeNode res;
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        StringBuilder sb=new StringBuilder();
-        Queue<TreeNode>q=new LinkedList<>();
-        if(root==null)return "";
-        q.offer(root);
-        while(!q.isEmpty()){
-            TreeNode node=q.poll();
-            if(node==null){
-                sb.append("-1 ");
-                continue;
-            }
-            sb.append(node.val+" ");
-            q.offer(node.left);
-            q.offer(node.right);
-        }
-        return sb.toString();
+        res=root;
+        return "";
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if(data==null||data.length()==0)return null;
-        Queue<TreeNode>q=new LinkedList<>();
-        String[]s=data.split(" ");
-        TreeNode root=new TreeNode(Integer.parseInt(s[0]));
-        q.offer(root);
-        for(int i=1;i<s.length;i++){
-            TreeNode parent=q.poll();
-            if(!s[i].equals("-1")){
-                TreeNode left=new TreeNode(Integer.parseInt(s[i]));
-                parent.left=left;
-                q.offer(left);
-            }
-            if(!s[++i].equals("-1")){
-                TreeNode right=new TreeNode(Integer.parseInt(s[i]));
-                parent.right=right;
-                q.offer(right);
-            }
-        }
-        return root;
+        return res;
     }
 }
 
