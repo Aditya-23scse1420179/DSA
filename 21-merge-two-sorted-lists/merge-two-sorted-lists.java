ecd12403f76075d6cc42a78d7ff1a3442caca1ba
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-03 13:41:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (93.60%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -9,15 +9,21 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1==null)return list2;
-        if(list2==null)return list1;
-        if(list1.val<=list2.val){
-            list1.next=mergeTwoLists(list1.next,list2);
-            return list1;
-        }else{
-            list2.next=mergeTwoLists(list2.next,list1);
-            return list2;
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode(0);
+        ListNode temp=dummy;
+        while(l1!=null&&l2!=null){
+            if(l1.val<=l2.val){
+                temp.next=l1;
+                l1=l1.next;
+            }else{
+                temp.next=l2;
+                l2=l2.next;
+            }
+            temp=temp.next;
         }
+        if(l1!=null)temp.next=l1;
+        if(l2!=null)temp.next=l2;
+        return dummy.next;
     }
 }
