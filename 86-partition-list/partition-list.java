13dae19a284a308e4b9a091b151156dbbf69808e
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-03 14:23:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (67.31%) - LeetSync

---
+++ 86-partition-list/partition-list.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode left=new ListNode(0);
+        ListNode right=new ListNode(0);
+        ListNode l=left;
+        ListNode r=right;
+        while(head!=null){
+            if(head.val<x){
+                l.next=head;
+                l=l.next;
+            }else{
+                r.next=head;
+                r=r.next;
+            }
+            head=head.next;
+        }
+        r.next=null;
+        l.next=right.next;
+        return left.next;
+    }
+}
