22bfc89b95194ad0b7fb768919a7dd1e054b6354
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-04 23:16:28 +0530

    Time: 2 ms (98.50%) | Memory: 50.1 MB (43.39%) - LeetSync

---
+++ 687-longest-univalue-path/longest-univalue-path.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count=0;
+    public int longestUnivaluePath(TreeNode root) {
+        helper(root);
+        return count;
+    }public int helper(TreeNode root){
+        if(root==null)return 0;
+        int left=helper(root.left);
+        int right=helper(root.right);
+        int leftp=0,rightp=0;
+        if(root.left!=null&&root.val==root.left.val){
+            leftp=left+1;
+        }
+        if(root.right!=null&&root.val==root.right.val){
+            rightp=right+1;
+        }
+        count=Math.max(count,leftp+rightp);
+        return Math.max(leftp,rightp);
+
+    }
+}
