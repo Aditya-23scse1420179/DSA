c2eeb332c02f25b41e9a6c9aaa93430ca9f1d1bf
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-05 01:12:00 +0530

    Time: 5 ms (65.34%) | Memory: 47.1 MB (41.88%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,32 +1,27 @@
 class Solution {
     public int[] asteroidCollision(int[] ass) {
-            List<Integer> list = new ArrayList<>();
-    for (int x : ass) list.add(x);
-
-    boolean changed = true;
-    while (changed) {
-        changed = false;
-        for (int i = 0; i < list.size() - 1; i++) {
-            int a = list.get(i), b = list.get(i+1);
-            if (a > 0 && b < 0) { // collision
-                if (Math.abs(a) > Math.abs(b)) {
-                    list.remove(i+1); // remove b
-                } else if (Math.abs(a) < Math.abs(b)) {
-                    list.remove(i);   // remove a
-                } else {
-                    list.remove(i+1);
-                    list.remove(i);
+        Stack<Integer>st=new Stack<>();
+        for(int a:ass){
+            if(a>0){
+                st.add(a);
+            }else{
+                while(!st.isEmpty()&&st.peek()>0&&st.peek()<Math.abs(a)){
+                    st.pop();
                 }
-                changed = true;
-                break; // restart loop
+                if(!st.isEmpty()&&st.peek()>0&&st.peek()==Math.abs(a)){
+                    st.pop();
+                    continue;
+                }
+                if(st.isEmpty()||st.peek()<0){
+                    st.push(a);
+                }
             }
         }
-    }
-
-    // convert list back to array
-    int[] res = new int[list.size()];
-    for (int i = 0; i < list.size(); i++) res[i] = list.get(i);
-    return res;
-
+        int size=st.size();
+        int[]ans=new int[size];
+        for(int i=size-1;i>=0;i--){
+            ans[i]=st.pop();
+        }
+        return ans;
     }
 }
