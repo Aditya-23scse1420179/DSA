b52856598d444dd86dbc4bd7c3de823e1e3cdd70
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-08 18:48:57 +0530

    Time: 119 ms (38.19%) | Memory: 78.3 MB (41.81%) - LeetSync

---
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function(root) {
+    function dfs(root,max){
+        if(root==null)return 0;
+        let count=0;
+        if(root.val>=max)count++;
+        max=Math.max(root.val,max);
+        count+=dfs(root.left,max);
+        count+=dfs(root.right,max);
+        return count;
+    }
+    return dfs(root,-Infinity);
+};
