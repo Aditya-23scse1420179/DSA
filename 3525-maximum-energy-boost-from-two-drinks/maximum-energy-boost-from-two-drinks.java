1910dd8ed7fb311ec2d741f988a3290c26741f2e
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-15 00:12:08 +0530

    Time: 6 ms (58.56%) | Memory: 156 MB (65.77%) - LeetSync

--- 3525-maximum-energy-boost-from-two-drinks/maximum-energy-boost-from-two-drinks.java
+++ 3525-maximum-energy-boost-from-two-drinks/maximum-energy-boost-from-two-drinks.java
@@ -1,14 +1,20 @@
 class Solution {
-    public long maxEnergyBoost(int[] energyDrinkA, int[] energyDrinkB) {
-        int n = energyDrinkA.length;
-        
-        long maxA = 0, maxB=0;
-        for(int i=0; i<n; i++){
-            long newMaxA = Math.max(maxA+energyDrinkA[i], maxB);
-            long newMaxB = Math.max(maxB+energyDrinkB[i], maxA);
-            maxA = newMaxA;
-            maxB = newMaxB;
+    public long maxEnergyBoost(int[] eA, int[] eB) {
+        int n=eA.length;
+        long[]dpA=new long[eA.length];
+        long[]dpB=new long[eB.length];
+        dpA[0]=eA[0];
+        dpB[0]=eB[0];
+        for(int i=1;i<n;i++){
+            dpA[i]=dpA[i-1]+eA[i];
+            dpB[i]=dpB[i-1]+eB[i];
+            if (i > 1) 
+            {
+                dpA[i] = Math.max(dpA[i], dpB[i-2] + eA[i]);
+                dpB[i] = Math.max(dpB[i], dpA[i-2] + eB[i]);
+            }
+
         }
-        return Math.max(maxA, maxB);
+        return Math.max(dpA[n-1],dpB[n-1]);
     }
 }
