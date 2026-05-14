27025ec59cd11a317cc07dc0ee86a7e8324ee914
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-14 22:54:13 +0530

    Time: 3 ms (36.65%) | Memory: 43.5 MB (62.94%) - LeetSync

--- 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.java
+++ 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.java
@@ -3,13 +3,12 @@ class Solution {
         int n=s.length();
         int ans=0;
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
                 int zero=0;
                 int one=0;
-                for(int x=i;x<=j;x++){
-                    if(s.charAt(x)=='0')zero++;
+            for(int j=i;j<n;j++){
+                    if(s.charAt(j)=='0')zero++;
                     else one++;
-                }
+                
                 if(zero<=k||one<=k)ans++;
             }
         }
