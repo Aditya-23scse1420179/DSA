69a0fcc9ae252d105820edf1559febb92009203c
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-18 00:18:27 +0530

    Time: 2 ms (23.87%) | Memory: 42.4 MB (92.26%) - LeetSync

---
+++ 1010-powerful-integers/powerful-integers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        HashSet<Integer>set=new HashSet<>();
+        for(int i=0;i<=20;i++){
+            int a=(int)Math.pow(x,i);
+            if(a>bound)break;
+            for(int j=0;j<=20;j++){
+                int b=(int)Math.pow(y,j);
+                if(a+b<=bound){
+                    set.add(a+b);
+                }else{
+                    break;
+                }
+            }
+
+        }
+        return new ArrayList<>(set);
+    }
+}
