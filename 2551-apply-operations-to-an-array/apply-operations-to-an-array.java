f527557af47dbef53a8b219c3bd3b1b1ce826747
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-18 14:20:52 +0530

    Time: 1 ms (95.08%) | Memory: 45.5 MB (7.82%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]*=2;
+                nums[i+1]=0;
+            }
+        }
+        move(nums);
+        return nums;
+    }public void move(int[] nums) {
+        int insertPos = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertPos++] = num;
+            }
+        }
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+}
