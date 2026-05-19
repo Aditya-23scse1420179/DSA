10e41aa7721853beb1ae70d402c12f4d596c1d5b
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-19 15:07:22 +0530

    Time: 753 ms (38.36%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,9 +1,9 @@
 SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
-FROM Employee e
-JOIN Department d ON e.departmentId = d.id
-WHERE e.salary IN (
-    SELECT MAX(salary)
-    FROM Employee
-    WHERE departmentId = e.departmentId
-);
- 
+FROM (
+    select e.*,
+    dense_rank()over(partition by departmentId order by salary desc)as rnk
+    from Employee e
+)e
+join Department d
+on d.id=e.departmentId
+where rnk=1;
