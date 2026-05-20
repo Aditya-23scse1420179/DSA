66c3533c29ad62fe570b6657eefdcde7da37aa08
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-20 15:01:14 +0530

    Time: 1 ms (96.45%) | Memory: 43 MB (67.74%) - LeetSync

---
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mctFromLeafValues(int[] arr) {
+        Stack<Integer>st=new Stack<>();
+        st.add((int)1e9);
+        int ans=0;
+        for(int a:arr){
+            while(st.peek()<=a){
+                ans+=st.pop()*Math.min(st.peek(),a);
+            }
+            st.push(a);
+        }
+        while(st.size()>2){
+            ans+=st.pop()*st.peek();
+        }
+        return ans;
+    }
+}
