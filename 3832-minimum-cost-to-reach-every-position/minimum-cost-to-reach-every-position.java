78d174eccba3244c005e6479bb322e182ac27edb
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-21 16:00:34 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (70.75%) - LeetSync

--- 3832-minimum-cost-to-reach-every-position/minimum-cost-to-reach-every-position.java
+++ 3832-minimum-cost-to-reach-every-position/minimum-cost-to-reach-every-position.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] minCosts(int[] cost) {
         int n=cost.length;
-        int[]arr=new int[n];
         int min=Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
             min=Math.min(min,cost[i]);
-            arr[i]=min;
+            cost[i]=min;
         }
-        return arr;
+        return cost;
     }
 }
