33c17d4dbde01063d7c4e48261c0a0718f9ffbf6
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-23 12:41:46 +0530

    Time: 4 ms (94.24%) | Memory: 90.5 MB (65.01%) - LeetSync

---
+++ 396-rotate-function/rotate-function.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+        long sum = 0, F0 = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            F0 += (long)i * nums[i];
+        }
+        long max = F0;
+        long curr = F0;
+        for (int k = 1; k < n; k++) {
+            curr = curr + sum - (long)n * nums[n - k];
+            max = Math.max(max, curr);
+        }
+        
+        return (int)max;
+    }
+}
