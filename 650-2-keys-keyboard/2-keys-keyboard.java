847c7321705f100987cbe5597335c5835735814c
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-05-28 18:23:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (92.48%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -9,12 +9,14 @@ class Solution {
             sum+=2;
             n/=2;
         }
+
         for(int i= 3;i*i<=n;i+=2){
             while(n%i==0) {
                 sum+=i;
                 n/=i;
             }
         }
+        
         if (n>2){
             sum+=n;
         }
