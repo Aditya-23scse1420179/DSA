2a19855a317eba834017f7aba14fa312a3e78bf0
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-06-03 00:36:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (15.39%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,13 +1,14 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
+        //rotate straight to vericle 
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
-        }
+        }//them swap and rotate row by row 
         for (int k = 0; k < n; k++) {
             int i = 0, j = n - 1;
             while (i < j) {  
