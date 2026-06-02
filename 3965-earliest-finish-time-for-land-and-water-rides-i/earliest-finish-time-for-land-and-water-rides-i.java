726522bb22a9328a1bf542dc838fd16c23600437
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-06-02 10:57:55 +0530

    Time: 4 ms (26.19%) | Memory: 46.9 MB (39.29%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -3,11 +3,8 @@ class Solution {
         int min=(int)1e9;
         for(int i=0;i<lST.length;i++){
             for(int j=0;j<wST.length;j++){
-                int l=lST[i]+lD[i];
-                int t=Math.max(l,wST[j])+wD[j];
-
-                int w=wST[j]+wD[j];
-                int wt=Math.max(w,lST[i])+lD[i];
+                int t=Math.max(lST[i]+lD[i],wST[j])+wD[j];
+                int wt=Math.max(wST[j]+wD[j],lST[i])+lD[i];
                 min=Math.min(min,Math.min(t,wt));
             }
         }
