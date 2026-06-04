d12dca44d9c02272474ef0f144f1296aa3bcb8a3
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-06-04 09:45:15 +0530

    Time: 26 ms (72.86%) | Memory: 46.3 MB (76.95%) - LeetSync

---
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int totalWaviness(int num1, int num2) {
+        if(num2<100)return 0;
+        int ans=0;
+        for(int p=num1;p<=num2;p++){
+            String s=String.valueOf(p);
+            for(int i=1;i<s.length()-1;i++){
+                ans+=valley(s.charAt(i-1)-'0',s.charAt(i)-'0',s.charAt(i+1)-'0');
+            }
+        }
+        return ans;
+    }public int valley(int prev,int curr,int nxt){
+        int count=0;
+        if(prev<curr&&curr>nxt)count++;
+        else if(prev>curr&&curr<nxt)count++;
+        
+        return count;
+    }
+}
