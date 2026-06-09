f31213381957a9fc5e26643199a5926827702015
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-06-09 18:37:58 +0530

    Time: 1 ms (100.00%) | Memory: 62.5 MB (20.14%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
@@ -1,11 +1,11 @@
 class Solution {
     public long maxTotalValue(int[] nums, int k) {
-        long mx=0;
-        long min=(int)1e9;
+        int mx=-(int)1e9;
+        int min=(int)1e9;
         for(int a:nums){
             if(a>mx)mx=a;
             if(a<min)min=a;
         }
-        return (mx-min)*1L*k;
+        return (long)(mx-min)*k;
     }
 }
