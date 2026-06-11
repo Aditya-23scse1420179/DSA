f5dc0314fdc73fb07b11b0d10522abd1eafbbf76
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-06-11 10:33:11 +0530

    Time: 107 ms (54.76%) | Memory: 297.9 MB (38.09%) - LeetSync

---
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.java
@@ -0,0 +1,29 @@
+class Solution {
+     List<List<Integer>>adj;
+    public int assignEdgeWeights(int[][] edges) {
+        int n=edges.length+1;
+        adj=new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int []a:edges){
+            int u=a[0];
+            int v=a[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        int dept=dfs(1,-1);
+        int res=1;
+        for(int i=1;i<dept;i++){
+            res=(res*2)%1000000007;
+        }
+        return res;
+    }public int dfs(int node,int parent){
+        int depth=0;
+        for(int a:adj.get(node)){
+            if(a==parent)continue;
+            depth=Math.max(depth,1+dfs(a,node));
+        }
+        return depth;
+    }
+}
