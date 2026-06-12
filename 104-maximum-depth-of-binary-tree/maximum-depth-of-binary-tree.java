0075a475fb4d1596928fa1b48209f47d261be7be
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-06-12 09:53:22 +0530

    Time: 1 ms (10.47%) | Memory: 44.5 MB (45.65%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -16,8 +16,18 @@
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root==null)return 0;
-        int left=maxDepth(root.left);
-        int right=maxDepth(root.right);
-        return Math.max(left,right)+1;
+        Queue<TreeNode>q=new LinkedList<>();
+        q.offer(root);
+        int level=0;
+        while(!q.isEmpty()){
+            int size=q.size();
+            while(size-->0){
+                TreeNode node=q.remove();
+                if(node.left!=null)q.offer(node.left);
+                if(node.right!=null)q.offer(node.right);
+            }
+            level++;
+        }
+        return level;
     }
 }
