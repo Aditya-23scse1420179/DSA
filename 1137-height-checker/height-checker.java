39c095439a5d6123b9e3f35114db8f5d73f1bb07
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-06-16 16:42:18 +0530

    Time: 4 ms (80.16%) | Memory: 43.5 MB (27.79%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,23 +1,7 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[]expect=new int[heights.length];
-        int max=-(int)1e9;
-        int min=(int)1e9;
-        for(int a:heights){
-            max=Math.max(a,max);
-            min=Math.min(a,min);
-        }
-        int[]freq=new int[max-min+1];
-        for(int a:heights){
-            freq[a-min]++;
-        }
-        int idx=0;
-        for(int i=0;i<freq.length;i++){
-            while(freq[i]>0){
-                expect[idx++]=i+min;
-                freq[i]--;
-            }
-        }
+        int[]expect=heights.clone();
+        Arrays.sort(expect);
 
         int count=0;
         for(int i=0;i<heights.length;i++){
