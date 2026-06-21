8b734c428f95a3c9624017df70d45cb2109163c4
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-06-22 00:26:54 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (92.65%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
@@ -1,11 +1,27 @@
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
-        Arrays.sort(seats);
-        Arrays.sort(students);
+        helper(seats);
+        helper(students);
         int sum=0;
         for(int i=0;i<seats.length;i++){
             sum+=Math.abs(seats[i]-students[i]);
         }
         return sum;
+    }public void helper(int[]arr){
+        int min=(int)1e9,max=-(int)1e9;
+        for(int x:arr){
+            min=Math.min(x,min);
+            max=Math.max(x,max);
+        }
+        int[]freq=new int[max-min+1];
+        for(int a:arr){
+            freq[a-min]++;
+        }int idx=0;
+        for(int i=0;i<freq.length;i++){
+            while(freq[i]>0){
+                arr[idx++]=i+min;
+                freq[i]--;
+            }
+        }
     }
 }
