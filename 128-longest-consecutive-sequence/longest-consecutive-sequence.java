bae5be60714f201efd958160c2433747d0ff083e
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-06-27 16:15:08 +0530

    Time: 21 ms (96.13%) | Memory: 77.6 MB (85.00%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,18 +1,22 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int max=0;
-        Set<Integer>set=new HashSet<>();
-        for(int num:nums){
-            set.add(num);
-        }
-        for(int num:set){
-            if(!set.contains(num-1)){
-                int count=1;
-                while(set.contains(num+count)){
-                    count++;
-                }
-                max=Math.max(max,count);
+        if(nums.length==0)return 0;
+        Arrays.sort(nums);
+        int max = 1;
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                // duplicate skip karo
+                continue;
+            } else if (nums[i] == nums[i - 1] + 1) {
+                // consecutive mila toh count++
+                count++;
+            } else {
+                // sequence break ho gaya, reset count
+                count = 1;
             }
+            max = Math.max(max, count);
         }
         return max;
     }
