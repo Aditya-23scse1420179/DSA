d0e11378cfa60eaaadc577f83bd285f6531f7212
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-06-28 13:51:59 +0530

    Time: 38 ms (6.67%) | Memory: 90.7 MB (5.00%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -1,6 +1,19 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
+    Map<Integer, Integer> freq = new HashMap<>();
+    for (int x:arr) {
+        freq.put(x,freq.getOrDefault(x,0)+1);
+    }
+    List<Integer> sortedKeys=new ArrayList<>(freq.keySet());
+    Collections.sort(sortedKeys);
+    int idx = 0;
+    for(int key:sortedKeys) {
+        int count=freq.get(key);
+        while(count-->0) {
+            arr[idx++]= key;
+        }
+    }
         int res=1;
         for(int i=1;i<arr.length;i++){
             if(arr[i]>=res+1)res++;
