89773c6c4c834a0fb36c18609e06800ca37670f6
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-06-29 00:55:38 +0530

    Time: 12 ms (99.94%) | Memory: 149.2 MB (20.96%) - LeetSync

--- 4348-maximum-total-sum-of-k-selected-elements/maximum-total-sum-of-k-selected-elements.java
+++ 4348-maximum-total-sum-of-k-selected-elements/maximum-total-sum-of-k-selected-elements.java
@@ -1,6 +1,20 @@
 class Solution {
     public long maxSum(int[] nums, int k, int mul) {
-        Arrays.sort(nums);
+        int max=-(int)1e9,min=(int)1e9;
+        for(int x:nums){
+            max=Math.max(max,x);
+            min=Math.min(min,x);
+        }
+        int[]freq=new int[max-min+1];
+        for(int x:nums){
+            freq[x-min]++;
+        }int idx=0;
+        for(int i=0;i<freq.length;i++){
+            while(freq[i]>0){
+                nums[idx++]=i+min;
+                freq[i]--;
+            }
+        }
         long sum=0;
         int i=0;
         while(i<k){
