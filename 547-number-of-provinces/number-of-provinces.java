34b94fd83f03ce4ba10ba3b2d04e9fb4603a210c
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-01 00:33:54 +0530

    Time: 2 ms (43.26%) | Memory: 47.1 MB (87.50%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,16 +1,16 @@
 class Solution {
     int []parent;
-    int []rank;
+    // int []rank;
     public int findCircleNum(int[][] isConnected) {
         int n=isConnected.length;
         parent=new int[n];
-        rank=new int[n];
+        // rank=new int[n];
         for(int i=0;i<n;i++){
             parent[i]=i;
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<isConnected[0].length;j++){
-                if(isConnected[i][j]==1)union(i,j);
+                if(isConnected[i][j]==1)parent[find(i)]=parent[find(j)];
             }
         }
         int comp=0;
@@ -21,18 +21,18 @@ class Solution {
     }public int find(int x){
         if(x==parent[x])return x;
         return parent[x]=find(parent[x]);
-    }public boolean union(int x,int y){
-        int px=find(x);
-        int py=find(y);
-        if(px==py)return false;
-        if(rank[px]>rank[py]){
-            parent[py]=px;
-        }else if(rank[py]>rank[px]){
-            parent[px]=py;
-        }else{
-            parent[px]=py;
-            rank[py]++;
-        }
-        return true;
+    // }public boolean union(int x,int y){
+    //     int px=find(x);
+    //     int py=find(y);
+    //     if(px==py)return false;
+    //     if(rank[px]>rank[py]){
+    //         parent[py]=px;
+    //     }else if(rank[py]>rank[px]){
+    //         parent[px]=py;
+    //     }else{
+    //         parent[px]=py;
+    //         rank[py]++;
+    //     }
+    //     return true;
     }
 }
