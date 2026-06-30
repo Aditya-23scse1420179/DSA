7f1d8dcb0bb3e075f6515197215335347a344a5a
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-01 00:22:31 +0530

    Time: 1 ms (96.79%) | Memory: 43.7 MB (56.12%) - LeetSync

---
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
@@ -0,0 +1,41 @@
+class Solution {
+    int[]parent=new int[26];
+    int[] rank=new int[26];
+    public boolean equationsPossible(String[] equations) {
+        for(int i=0;i<26;i++){
+            parent[i]=i;
+        }
+        for(String eq:equations){
+            if(eq.charAt(1)=='='){
+                int u=eq.charAt(0)-'a';
+                int v=eq.charAt(3)-'a';
+                union(u,v);
+            }
+        }
+        for(String eq:equations){
+            if(eq.charAt(1)=='!'){
+                int u=eq.charAt(0)-'a';
+                int v=eq.charAt(3)-'a';
+                if(find(u)==find(v))return false;
+            }
+        }
+        return true;
+    }public int find(int x){
+        if(x==parent[x])return x;
+        return parent[x]=find(parent[x]);
+    }
+    public boolean union(int x,int y){
+        int px=find(x);
+        int py=find(y);
+        if(px==py)return true;
+        if(rank[px]>py){
+            parent[py]=px;
+        }else if(rank[py]>px){
+            parent[px]=py;
+        }else{
+            parent[px]=py;
+            rank[py]++;
+        }
+        return false;
+    }
+}
