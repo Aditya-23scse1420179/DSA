0e72012d7fa9ef9c4b26337e6028170ecf2ec627
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-02 15:08:52 +0530

    Time: 28 ms (42.62%) | Memory: 46.9 MB (85.25%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
@@ -1,43 +1,41 @@
 class Solution {
-    static int []dr={-1,1,0,0};
-    static int[]dc={0,0,-1,1};
-    static class pair {
-        int row;
-        int col;
-        int dist;
-        public pair(int row,int col,int dist){
+    int[]dr={-1,1,0,0};
+    int[]dc={0,0,-1,1};
+    class pair{
+        int row,col,d;
+        public pair(int row,int col,int d){
             this.row=row;
             this.col=col;
-            this.dist=dist;
+            this.d=d;
         }
     }
     public boolean findSafeWalk(List<List<Integer>> grid, int health) {
         int n=grid.size();
-        int m=grid.get(0).size();//arraylist me aise hi lete hain 
-        int [][]dist=new int[n][m];
+        int m=grid.get(0).size();
+        int[][]dist=new int[n][m];
         for(int[]a:dist){
-            Arrays.fill(a,-(int)1e9);
+            Arrays.fill(a,-1);
         }
-        int sh=health-grid.get(0).get(0);
-        dist[0][0]=sh;
         Queue<pair>q=new LinkedList<>();
-        q.offer(new pair(0,0,sh));
+        dist[0][0]=health-grid.get(0).get(0);
+        q.offer(new pair(0,0,health-grid.get(0).get(0)));
         while(!q.isEmpty()){
-            pair curr=q.remove();
+            pair curr=q.poll();
             int r=curr.row;
             int c=curr.col;
-            int w=curr.dist;
+            int h=curr.d;
             for(int i=0;i<4;i++){
                 int nr=r+dr[i];
                 int nc=c+dc[i];
-                if(nr<0||nr>=n||nc<0||nc>=m)continue;
-                    if(w-grid.get(nr).get(nc)>=0&&dist[nr][nc]<w-grid.get(nr).get(nc)){
-                        dist[nr][nc]=w-grid.get(nr).get(nc);
-                        q.offer(new pair(nr,nc,dist[nr][nc]));
+                if(nr>=0&&nr<n&&nc>=0&&nc<m){
+                    int nhealth=h-grid.get(nr).get(nc);
+                    if(nhealth>=1&&dist[nr][nc]<nhealth){
+                        dist[nr][nc]=nhealth;
+                        q.offer(new pair(nr,nc,nhealth));
                     }
-                
+                }
             }
         }
-        return dist[n-1][m-1]>=1?true:false;
+        return dist[n-1][m-1]>=1;
     }
 }
