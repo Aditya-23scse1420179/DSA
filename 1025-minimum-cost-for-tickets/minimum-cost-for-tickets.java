295e9acf09da1fa594b155d55e128d3787f5a8b3
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-03 12:54:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (84.32%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,19 +1,18 @@
 class Solution {
-    public int helper(int []days,int []costs,int idx,Integer[]dp){
+    Integer[]dp;
+    public int mincostTickets(int[] days, int[] costs) {
+        dp=new Integer[days.length+1];
+        return helper(0,days,costs);
+    }public int helper(int idx,int[]days,int[]costs){
         if(idx>=days.length)return 0;
         if(dp[idx]!=null)return dp[idx];
-        int trip1=costs[0]+helper(days,costs,idx+1,dp);
-        int i = idx;
-        while (i < days.length && days[i] <= days[idx] + 6) i++;
-        int trip2=costs[1]+helper(days,costs,i,dp);
-        i = idx;
-        while (i < days.length && days[i] <= days[idx] + 29) i++;
-        int trip3=costs[2]+helper(days,costs,i,dp);
+        int trip1=costs[0]+helper(idx+1,days,costs);
+        int i=idx;
+        while(i<days.length&&days[i]<=days[idx]+6)i++;
+        int trip2=costs[1]+helper(i,days,costs);
+        i=idx;
+        while(i<days.length&&days[i]<=days[idx]+29)i++;
+        int trip3=costs[2]+helper(i,days,costs);
         return dp[idx]=Math.min(trip1,Math.min(trip2,trip3));
-
-    }
-    public int mincostTickets(int[] days, int[] costs) {
-        Integer[]dp= new Integer[days.length];
-        return helper(days,costs,0,dp);
     }
 }
