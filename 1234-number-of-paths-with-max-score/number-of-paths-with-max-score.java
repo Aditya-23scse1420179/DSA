f531db97ce3a852e23cb996a36ce6027ce67659c
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-05 20:07:26 +0530

    Time: 14 ms (58.82%) | Memory: 44.3 MB (97.65%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
@@ -1,42 +1,36 @@
 class Solution {
-    class pair{
-        int i,j,score;
-        public pair(int i,int j,int score){
-            this.i=i;
-            this.j=j;
-            this.score=score;
-        }
-    }
     int[]dr={-1,0,-1};
     int[]dc={0,-1,-1};
     public int[] pathsWithMaxScore(List<String> board) {
         int n=board.size();
-        Queue<pair>q=new LinkedList<>();
+
         int[][]dist=new int[n][n];
         for(int[]a:dist){
             Arrays.fill(a,-1);
         }
         int[][]count=new int[n][n];
-        q.offer(new pair(n-1,n-1,0));
         dist[n-1][n-1]=0;
         count[n-1][n-1]=1;
-        while(!q.isEmpty()){
-            pair curr=q.poll();
-            for(int d=0;d<3;d++){
-                int r=curr.i+dr[d];
-                int c=curr.j+dc[d];
-                if(r<0||r>=n||c<0||c>=n)continue;
-                char ch=board.get(r).charAt(c);
-                if(ch=='X')continue;
-                int val=ch=='E'?0:ch-'0';
-                int score=val+curr.score;
-                if(score>dist[r][c]){
-                    dist[r][c]=score;
-                    count[r][c]=count[curr.i][curr.j];
-                    q.offer(new pair(r,c,dist[r][c]));
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(board.get(i).charAt(j)=='X')continue;
+                if (dist[i][j] == -1) continue;
+                for(int d=0;d<3;d++){
+                    int r=i+dr[d];
+                    int c=j+dc[d];
+                    if(r<0||r>=n||c<0||c>=n)continue;
+                    char ch=board.get(r).charAt(c);
+                    if(ch=='X') continue;
 
-                }else if(score==dist[r][c]){
-                    count[r][c]=(count[r][c]+count[curr.i][curr.j])%1000000007;;
+                    int val=ch=='E'||ch=='S'?0:ch-'0';
+                    int score=val+dist[i][j];
+                    if(score>dist[r][c]){
+                        dist[r][c]=score;
+                        count[r][c]=count[i][j];
+
+                    }else if(score==dist[r][c]){
+                        count[r][c]=(count[r][c]+count[i][j])%1000000007;;
+                    }
                 }
             }
         }
