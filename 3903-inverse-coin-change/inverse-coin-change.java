be31a2d4c4308ea868297e89e5953643fd911902
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-11 21:32:16 +0530

    Time: 79 ms (7.64%) | Memory: 47.3 MB (12.10%) - LeetSync

--- 3903-inverse-coin-change/inverse-coin-change.java
+++ 3903-inverse-coin-change/inverse-coin-change.java
@@ -1,20 +1,35 @@
 class Solution {
-    public List<Integer> findCoins(int[] numWays) {
-    int n = numWays.length;
-    long[] myWays = new long[n + 1];
-    myWays[0] = 1;
-    List<Integer> res = new ArrayList<>();
-    for (int i =1;i<=n;i++){
-        if (myWays[i]==numWays[i-1]) continue;
-        if (numWays[i-1]-myWays[i]==1){
-            res.add(i);
-            for (int j =i;j <=n;j++){
-                myWays[j]+= myWays[j-i];
+    public List<Integer> findCoins(int[] nums) {
+        List<Integer> coins = new ArrayList<>();
+        for(int i = 1;i<=nums.length;i++){
+            int[][] dp = new int[coins.size()+1][i+1];
+            for(int x = 0;x<dp.length;x++){
+                for(int j = 0;j<dp[x].length;j++){
+                    dp[x][j] = -1;
+                }
             }
-        } else {
-            return Collections.emptyList();
+            int ways = helper(0,i,coins,dp);
+            if(ways+1 == nums[i-1]){
+                coins.add(i);
+            }
+            else if(ways == nums[i-1]){
+                continue;
+            }
+            else{
+                return new ArrayList<>();
+            }
         }
+        return coins;
     }
-    return res;
-}
+    public int helper(int i, int amount, List<Integer> coins, int[][] dp){
+        if(amount == 0) return 1;
+        if(amount<0 || i>= coins.size()) return 0;
+        if(dp[i][amount] != -1) return dp[i][amount];
+        int notpick = helper(i+1,amount,coins,dp);
+        int pick = 0;
+        if(coins.get(i)<=amount){
+            pick = helper(i,amount-coins.get(i),coins,dp);
+        }
+        return dp[i][amount] = pick + notpick;
+    }
 }
