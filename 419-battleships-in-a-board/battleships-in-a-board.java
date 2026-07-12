587e1fcbce71cec9bc7ec15228b330814c83625e
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-12 22:54:00 +0530

    Time: 2 ms (7.28%) | Memory: 47.9 MB (7.67%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.java
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -1,15 +1,16 @@
 class Solution {
     int[]dr={-1,1,0,0};
     int[]dc={0,0,-1,1};
-    // class pair{
-    //     int row,int col;
-    //     public pair(int row,int col){
-    //         this.row=row;
-    //         this.col=col;
-    //     }
-    // }
+    class pair{
+        int row,col;
+        public pair(int row,int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
     int rl,cl;
     boolean[][]visit;
+    
     public int countBattleships(char[][] board) {
         rl=board.length;
         cl=board[0].length;
@@ -18,18 +19,31 @@ class Solution {
         for(int i=0;i<rl;i++){
             for(int j=0;j<cl;j++){
                 if(board[i][j]=='X'&&!visit[i][j]){
-                    dfs(i,j,board);
+                    bfs(i,j,board);
                     comp++;
                 }
             }
         }
         return comp; 
 
-    }public void dfs(int r,int c,char[][]board){
-        if(r<0||r>=rl||c<0||c>=cl||visit[r][c]||board[r][c]=='.')return ;
+    }
+    public void bfs(int r,int c,char[][]grid){
+        Queue<pair>q=new LinkedList<>();
         visit[r][c]=true;
-        for(int i=0;i<4;i++){
-            dfs(r+dr[i],c+dc[i],board);
+        q.offer(new pair(r,c));
+        while(!q.isEmpty()){
+            pair curr=q.remove();
+            int r1=curr.row;
+            int c1=curr.col;
+        
+            for(int i=0;i<4;i++){
+                int nrow=r1+dr[i];
+                int ncol=c1+dc[i];
+                if(nrow>=0&&ncol>=0&&nrow<rl&&ncol<cl&&!visit[nrow][ncol]&&grid[nrow][ncol]=='X'){
+                    visit[nrow][ncol]=true;
+                    q.offer(new pair(nrow,ncol));
+                }
+            }
         }
     }
 } 
