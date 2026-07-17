77bb97e6e470f1fb0c353dd53ae47fdf8371a6fd
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-17 15:12:49 +0530

    Time: 111 ms (37.22%) | Memory: 47.4 MB (5.98%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,16 +1,17 @@
 class Solution {
-    int count=0;
+    int[]nums;
+    Map<String,Integer>dp=new HashMap<>();
     public int findTargetSumWays(int[] nums, int target) {
-        helper(0,nums,target,0);
-        return count;
-    }public void helper(int idx,int[]nums,int target,int sum){
-        if(idx==nums.length){
-            if(sum==target){
-                count++;
-            }
-            return;
-        }
-        helper(idx+1,nums,target,sum+nums[idx]);
-        helper(idx+1,nums,target,sum-nums[idx]);
+        this.nums=nums;
+        // dp=new Integer[nums.length+1][target+1];
+        return helper(0,target,0);
+    }public int helper(int idx,int target,int sum){
+        if(idx==nums.length)return target==sum?1:0;
+        String key=idx+","+sum;
+        if(dp.containsKey(key))return dp.get(key);
+        int take1=helper(idx+1,target,sum+nums[idx]);
+        int take2=helper(idx+1,target,sum-nums[idx]);
+        dp.put(key,take1+take2);
+        return take1+take2;
     }
 }
