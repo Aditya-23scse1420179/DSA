11b7fef7c6fcb0dbb6c665f25813987d6a05c8c3
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-19 13:49:26 +0530

    Time: 1 ms (76.35%) | Memory: 43.9 MB (91.22%) - LeetSync

--- 4369-even-number-of-knight-moves/even-number-of-knight-moves.java
+++ 4369-even-number-of-knight-moves/even-number-of-knight-moves.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canReach(int[] start, int[] target) {
-        int dis=Math.abs(start[0]-target[0])+Math.abs(start[1]-target[1]);
-        return dis%2==0?true:false;
+       
+        return ((start[0]+start[1])%2)==((target[0]+target[1])%2);
     }
 }
