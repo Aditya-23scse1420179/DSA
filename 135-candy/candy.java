d72299d7b01b9d494b8c0e20a1f31897b7b70a59
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-20 14:07:00 +0530

    Time: 3 ms (87.12%) | Memory: 48 MB (6.34%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,27 +1,26 @@
 class Solution { 
     public int candy(int[] ratings) {
         int n=ratings.length;
-        int[]left=new int[n];
-        int[]right=new int[n];
-        left[0]=1;
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                left[i]=left[i-1]+1;
-            }else{
-                left[i]=1;
+        int i=1;
+        int sum=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum+=1;
+                i++;
+                continue;
+            }int peak=1;
+            while(i<n&&ratings[i]>ratings[i-1]){
+                peak+=1;
+                sum+=peak;
+                i++;
+            }int down=1;
+            while(i<n&&ratings[i]<ratings[i-1]){
+                sum+=down;
+                i++;
+                down++;   
             }
-        }
-        right[n-1]=1;
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
-            }else{
-                right[i]=1;
-            }
-        }
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=Math.max(left[i],right[i]);
+            if(down>peak)sum+=down-peak;
+
         }
         return sum;
     }
