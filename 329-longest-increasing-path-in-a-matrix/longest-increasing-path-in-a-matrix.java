7cd53f7510a658b980427777a2f7c154789d3832
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-21 16:07:19 +0530

    Time: 8 ms (89.50%) | Memory: 46.9 MB (81.70%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,47 +1,30 @@
 class Solution {
-    int[]dr={-1,1,0,0};
+    int []dr={-1,1,0,0};
     int[]dc={0,0,-1,1};
-
+    int n,m;
+    Integer[][]dp;
     public int longestIncreasingPath(int[][] matrix) {
-        int n=matrix.length,m=matrix[0].length;
-        int[][]in=new int[n][m];
+        n=matrix.length;
+        m=matrix[0].length;
+        dp=new Integer[n][m];
+        int max=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                int r=i;
-                int c=j;
-                for(int d=0;d<4;d++){
-                    int nr=r+dr[d];
-                    int nc=c+dc[d];
-                    if(nr>=0&&nr<n&&nc>=0&&nc<m&&matrix[nr][nc]<matrix[r][c]){
-                        in[r][c]++;
-                    }
-                }
-            }
-        }Queue<int[]>q=new LinkedList<>();
-        int[][]dist=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                dist[i][j]=1;
-                if(in[i][j]==0){
-                    q.offer(new int[]{i,j});
-                }
-            }
-        }int maxl=0;
-        while(!q.isEmpty()){
-            int[]curr=q.poll();
-            int r=curr[0];
-            int c=curr[1];
-            maxl=Math.max(maxl,dist[r][c]);
-            for(int i=0;i<4;i++){
-                int nr=r+dr[i];
-                int nc=c+dc[i];
-                if(nr>=0&&nr<n&&nc>=0&&nc<m&&matrix[nr][nc]>matrix[r][c]){
-                    dist[nr][nc]=Math.max(dist[r][c]+1,dist[nr][nc]);
-                    in[nr][nc]--;
-                    if(in[nr][nc]==0)q.offer(new int[]{nr,nc});
-                }
+                max=Math.max(max,dfs(i,j,matrix));
             }
         }
-        return maxl;
+        return max;
+    }
+    public int dfs(int r,int c,int[][]matrix){
+        if(dp[r][c]!=null)return dp[r][c];
+        int cmax=1;
+        for(int i=0;i<4;i++){
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            if(nr>=0&&nr<n&&nc>=0&&nc<m&&matrix[nr][nc]>matrix[r][c]){
+                cmax=Math.max(cmax,1+dfs(nr,nc,matrix));
+            }
+        }
+        return dp[r][c]=cmax;
     }
 }
