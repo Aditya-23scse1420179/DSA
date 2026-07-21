a89ac78d1af40c474efd0e3fa1b11f809e2b22dc
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-22 00:12:54 +0530

    Time: 28 ms (6.86%) | Memory: 122.6 MB (40.42%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
         if(nums==null||nums.length<3)return false;
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MAX_VALUE;
-        for(int a:nums){
-            if(a<=min){
-                min=a;
-            }else if(a<=max){
-                max=a;
-            }else return true;
+        List<Integer>list=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            int idx=Collections.binarySearch(list,nums[i]);
+            if(idx<0)idx=-(idx+1);
+            if(idx==list.size()){
+                list.add(nums[i]);
+            }else{
+                list.set(idx,nums[i]);
+            }
         }
-        return false;
+        return list.size()>=3;
     }
 }
