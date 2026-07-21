643b100a89ce4d054988b58fbae334f5c7a031ad
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-21 22:01:54 +0530

    Time: 14 ms (87.56%) | Memory: 47.5 MB (40.15%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -1,40 +1,41 @@
 class Solution {
-    static int []dr={-1,1,0,0};
-    static int[]dc={0,0,-1,1};
+    int[]dr={-1,1,0,0};
+    int[]dc={0,0,-1,1};
     class pair{
-        int row,col;
-        public pair(int row,int col){
-            this.row=row;
-            this.col=col;
+        int ro,co;
+        public pair(int ro,int co){
+            this.ro=ro;
+            this.co=co;
         }
     }
     public int maxDistance(int[][] grid) {
         int n=grid.length;
+        int m=grid[0].length;
         Queue<pair>q=new LinkedList<>();
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==1)q.offer(new pair(i,j));
             }
-        }int res=-1;
+        }int count=-1;
         if(q.isEmpty()||q.size()==n*n)return -1;
         while(!q.isEmpty()){
-            res++;
             int size=q.size();
             while(size-->0){
-                
-                pair curr=q.remove();
-                int r=curr.row;
-                int c=curr.col;
+                pair curr=q.poll();
+                int r=curr.ro;
+                int c=curr.co;
                 for(int i=0;i<4;i++){
                     int nr=r+dr[i];
                     int nc=c+dc[i];
-                    if(nr>=0&&nr<n&&nc>=0&&nc<n&&grid[nr][nc]==0){
+                    if(nr>=0&&nr<n&&nc>=0&&nc<m&&grid[nr][nc]==0){
                         grid[nr][nc]=1;
                         q.offer(new pair(nr,nc));
                     }
                 }
             }
+            count++;
         }
-        return res;
+        return count;
     }
+    
 }
