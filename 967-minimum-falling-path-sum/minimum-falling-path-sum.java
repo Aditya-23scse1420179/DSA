fe45495dc946dc5791831afa23197a32bce11d7a
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-22 15:22:19 +0530

    Time: 1 ms (99.61%) | Memory: 47.2 MB (63.44%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,25 +1,22 @@
 class Solution {
-    int n;
-    int m;
+    int n,m;
     Integer[][]dp;
-    public int minFallingPathSum(int[][] mat) {
-        n=mat.length;
-        m=mat[0].length;
-        dp=new Integer[n+1][m+1];
-        int ans=(int)1e9;
-        for(int j=0;j<m;j++){
-            ans=Math.min(ans,helper(mat,0,j));
+    public int minFallingPathSum(int[][] matrix) {
+        n=matrix.length;
+        m=matrix[0].length;
+        dp=new Integer[n][m];
+        int max=(int)1e9;
+        for(int i=0;i<m;i++){
+            max=Math.min(max,helper(0,i,matrix));
         }
-        return ans; 
-    }public int helper(int [][]mat,int i,int j){
-        
-        if(j>=m||j<0)return (int)1e9;//boundary
-        if(i==n-1)return mat[i][j];//dest
-        if(dp[i][j]!=null)return dp[i][j]; 
-        int down=helper(mat,i+1,j);
-        int diaR=helper(mat,i+1,j-1);
-        int diaL=helper(mat,i+1,j+1);
-        return dp[i][j]=Math.min(down,Math.min(diaR,diaL))+mat[i][j];
-
+        return max;
+    }public int helper(int r,int c,int[][]matrix){
+        if(r>=n||c>=m||c<0)return (int)1e9;
+        if(r==n-1)return matrix[r][c];
+        if(dp[r][c]!=null)return dp[r][c];
+        int d=helper(r+1,c,matrix);
+        int dl=helper(r+1,c-1,matrix);
+        int dr=helper(r+1,c+1,matrix);
+        return dp[r][c]=Math.min(d,Math.min(dl,dr))+matrix[r][c];
     }
 }
