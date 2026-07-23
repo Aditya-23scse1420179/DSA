1995505f410d0ddf2e09e59042195d3ecec3fe6d
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-23 21:15:38 +0530

    Time: 6 ms (98.75%) | Memory: 65.4 MB (24.68%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,26 +1,27 @@
 class Solution {
-    List<List<String>>res=new ArrayList<>();
-    List<String>list=new ArrayList<>();
     public List<List<String>> partition(String s) {
-        helper(s,0);
-        return res;
-    }public void helper(String s,int start){
+        List<List<String>>list=new ArrayList<>();
+        helper(s,list,new ArrayList<>(),0);
+        return list;
+    }public void helper(String s,List<List<String>>list,List<String>temp,int start){
         if(start==s.length()){
-            res.add(new ArrayList<>(list));
+            list.add(new ArrayList<>(temp));
         }
         for(int i=start+1;i<=s.length();i++){
             if(ispal(s,start,i-1)){
-                list.add(s.substring(start,i));
-                helper(s,i);
-                list.remove(list.size()-1);
+                temp.add(s.substring(start,i));
+                helper(s,list,temp,i);
+                temp.remove(temp.size()-1);
             }
         }
-    }public boolean ispal(String s,int start,int end){
-        while(start<end){
-            if(s.charAt(start++)!=s.charAt(end--)){
+    }
+    public boolean ispal(String s,int i,int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
                 return false;
             }
-            
+            i++;
+            j--;
         }
         return true;
     }
