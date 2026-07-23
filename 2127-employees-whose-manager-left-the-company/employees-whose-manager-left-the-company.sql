781622ef54012b4ce1f9c8b2ac07aee7d46b544b
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-23 21:53:00 +0530

    Time: 436 ms (10.70%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
 select e.employee_id
 from Employees e
-where e.salary<30000 and e.manager_id is not null and not exists(
-    select 1
-    from Employees m
-    where m.employee_id =e.manager_id
-)
-order by employee_id asc
+left join Employees m
+on e.manager_id=m.employee_id
+where e.salary<30000
+and e.manager_id is not null
+and m.employee_id is null
+order by employee_id
