de7f5e84300f2a103901eb02379e06c446a3ca65
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-24 21:51:56 +0530

    Time: 34 ms (39.67%) | Memory: 48.4 MB (12.50%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.java
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.java
@@ -3,25 +3,25 @@ class Solution {
         int n=quality.length;
         double res=1e18;
         double[][]workers=new double[n][2];
-        for (int i =0;i<n;i++){
-            workers[i][0]=(double) wage[i]/quality[i]; 
+        for (int i=0;i<n;i++){
+            workers[i][0]=(double)wage[i]/quality[i]; 
             workers[i][1]=quality[i];
         }
         Arrays.sort(workers,(a,b)->Double.compare(a[0],b[0]));
-        PriorityQueue<Double>pq =new PriorityQueue<>(Collections.reverseOrder());
-        double sumQuality=0;
-        for (int i =0;i<n;i++){
-            double q=workers[i][1];
-            pq.add(q);
-            sumQuality+=q;
-            if (pq.size()>k) {
-                sumQuality-=pq.poll();
+    
+        PriorityQueue<Double>q=new PriorityQueue<>(Collections.reverseOrder());
+        double ans=0;
+        for(int i=0;i<n;i++){
+            double r=workers[i][1];
+            q.add(r);
+            ans+=r;
+            if(q.size()>k){
+                ans-=q.poll();
             }
-            if (pq.size()==k){
-                res=Math.min(res,workers[i][0]*sumQuality);
+            if(q.size()==k){
+                res=Math.min(res,workers[i][0]*ans);
             }
         }
-
         return res;
     }
 }
