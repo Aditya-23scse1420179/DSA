3bfb5294bce7e0d057447e4b8c27d5d9f31698ff
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-25 14:40:20 +0530

    Time: 8 ms (93.78%) | Memory: 46.4 MB (58.61%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,56 +1,42 @@
 class Solution {
-    class pair{
-        int node,weight;
-        public pair(int node,int weight){
-            this.node=node;
-            this.weight=weight;
-        }
-    }
-    static int m;
+
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        m=edges[0].length;
-        ArrayList<ArrayList<pair>>list=new ArrayList<>();
+        int m=edges[0].length;
+        int[][]dist=new int[n][n];
+        for(int[]a:dist){
+            Arrays.fill(a,(int)1e9);
+        }
         for(int i=0;i<n;i++){
-            list.add(new ArrayList<>());
+            dist[i][i]=0;
         }
         for(int[]a:edges){
             int u=a[0];
             int v=a[1];
             int w=a[2];
-            list.get(u).add(new pair(v,w));
-            list.get(v).add(new pair(u,w));
+            dist[u][v]=w;
+            dist[v][u]=w;
         }
-        int result=-1;
-        int replace=(int)1e9;
-        for(int start=0;start<n;start++){
-            int []dist=new int[n];
-            Arrays.fill(dist,(int)1e9);
-            PriorityQueue<pair>q=new PriorityQueue<>((a,b)->Integer.compare(a.weight,b.weight));
-            q.offer(new pair(start,0));
-            dist[start]=0;
-            while(!q.isEmpty()){
-                pair curr=q.remove();
-                int u=curr.node;
-                int w=curr.weight;
-                if(dist[u]<w)continue;
-                for(pair a:list.get(u)){
-                    int v=a.node;
-                    int W=a.weight;
-                    if(dist[v]>dist[u]+W){
-                        dist[v]=dist[u]+W;
-                        q.offer(new pair(v,dist[v]));
-                    }
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);
                 }
             }
-            int rep=0;
-            for(int i=0;i<n;i++){
-                if(i!=start&&dist[i]<=distanceThreshold)rep++;
+        }
+        int res=-1,rep=(int)1e9;
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(i!=j&&dist[i][j]<=distanceThreshold){
+                    count++;
+                }
             }
-            if(rep<=replace){
-                replace=rep;
-                result=start;
+            if(count<=rep){
+                rep=count;
+                res=i;
             }
         }
-        return result;
+        return res;
+
     }
 }
