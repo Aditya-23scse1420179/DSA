fe197939c7644e38339c19437ad8207a6e28accb
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-26 16:50:14 +0530

    Time: 31 ms (68.81%) | Memory: 184.8 MB (5.07%) - LeetSync

---
+++ 2751-sliding-subarray-beauty/sliding-subarray-beauty.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] getSubarrayBeauty(int[] nums, int k, int x) {
+        int n = nums.length;
+        int[] arr = new int[n-k+1];
+        int[] freq = new int[101];
+        for (int i=0;i<k;i++){
+            freq[nums[i]+50]++;
+        }
+        int id=0;
+        arr[id++] =helper(freq,x);
+        int i=1,j=k;
+        while(j<n){
+            freq[nums[j]+50]++;   
+            freq[nums[i-1]+50]--; 
+            arr[id++]=helper(freq, x);
+            i++;
+            j++;
+        }
+        return arr;
+    }
+    public int helper(int[]freq,int x) {
+        int count=0;
+        for(int idx=0;idx<50;idx++){
+            count+=freq[idx];
+            if(count>=x) {
+                return idx-50;
+            }
+        }
+        return 0;
+    }
+}
