8f7cae4e07324e9305256d5b53af481fcd9a4b9a
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-28 13:28:39 +0530

    Time: 1 ms (100.00%) | Memory: 106.4 MB (57.65%) - LeetSync

--- 2878-apply-operations-to-make-all-array-elements-equal-to-zero/apply-operations-to-make-all-array-elements-equal-to-zero.java
+++ 2878-apply-operations-to-make-all-array-elements-equal-to-zero/apply-operations-to-make-all-array-elements-equal-to-zero.java
@@ -1,18 +1,18 @@
 class Solution {
-    public boolean checkArray(int[] nums, int k) {
+    public boolean checkArray(int[] nums, int k) {//is waana print opp
         int n=nums.length;
         int[]arr=new int[n+1];
-        int opp=0;
+        int opp=0;//one more topp
         for(int i=0;i<n;i++){
             opp+=arr[i];
             int val=nums[i]-opp;
-            if(val<0)return false;
+            if(val<0)return false;//-1
             if(val>0){
-                if(i+k>n)return false;
+                if(i+k>n)return false;//-1
                 opp+=val;
-                arr[i+k]-=val;
+                arr[i+k]-=val;//topp+=val;
             }
         }
-        return true;
+        return true;//return topp
     }
 }
