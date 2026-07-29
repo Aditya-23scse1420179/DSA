fd7360de3e02532c6f0b07fc1066357afec279a1
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-30 00:44:56 +0530

    Time: 16 ms (99.90%) | Memory: 65.8 MB (84.27%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numSubseq(int[] nums, int target) {
-        Arrays.sort(nums);
+        sort(nums);
         int l=0,r=nums.length-1,res=0;
         int[]pow = new int[nums.length];
         pow[0] = 1;
@@ -16,5 +16,21 @@ class Solution {
             }
         }
         return res;
+    }public void sort(int[]nums){
+        int min=(int)1e9,max=-(int)1e9;
+        for(int a:nums){
+            min=Math.min(min,a);
+            max=Math.max(max,a);
+        }
+        int[]freq=new int[max-min+1];
+        for(int a:nums){
+            freq[a-min]++;
+        }int id=0;
+        for(int i=0;i<freq.length;i++){
+            while(freq[i]>0){
+                nums[id++]=i+min;
+                freq[i]--;
+            }
+        }
     }
 }
