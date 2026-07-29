682362c272f2ff4405025a2ab59b1e7bd83bc3f6
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-30 00:14:17 +0530

    Time: 5 ms (98.15%) | Memory: 74.4 MB (52.75%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,6 +1,22 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
+        sort(nums);
         return nums[nums.length-k];
+    }public void sort(int[]nums){
+        int min=(int)1e9,max=-(int)1e9;
+        for(int a:nums){
+            min=Math.min(min,a);
+            max=Math.max(max,a);
+        }
+        int[]freq=new int[max-min+1];
+        for(int a:nums){
+            freq[a-min]++;
+        }int id=0;
+        for(int i=0;i<freq.length;i++){
+            while(freq[i]>0){
+                nums[id++]=i+min;
+                freq[i]--;
+            }
+        }
     }
 }
