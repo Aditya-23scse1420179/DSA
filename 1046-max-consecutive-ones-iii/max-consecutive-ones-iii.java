1bca2fac93d73518acc92daf208cede0df3eabf6
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-31 01:25:51 +0530

    Time: 3 ms (95.95%) | Memory: 52 MB (89.54%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -4,8 +4,9 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(nums[i]==0)zero++;
             while(zero>k){
-                if(nums[left]==0)
-                zero--;
+                if(nums[left]==0){
+                    zero--;
+                }
                 left++;
             }
             max=Math.max(max,i-left+1);
