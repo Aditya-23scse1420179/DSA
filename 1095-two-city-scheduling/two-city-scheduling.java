d4faebc68237cbd9d2f96c731877fcc94e208e75
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-07-31 01:47:13 +0530

    Time: 2 ms (97.39%) | Memory: 43.4 MB (80.89%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.java
+++ 1095-two-city-scheduling/two-city-scheduling.java
@@ -1,12 +1,18 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
-        Arrays.sort(costs,(a,b)->(a[0]-a[1])-(b[0]-b[1]));
-        int n=costs.length/2;
-        int sum=0;
-        for(int i=0;i<costs.length;i++){
-            if(i<n)sum+=costs[i][0];
-            else sum+=costs[i][1];
+        int n=costs.length;
+        Arrays.sort(costs,(a,b)->{//sort on the basis of costs
+            return (a[0]-a[1])-(b[0]-b[1]);
+        });
+        int ans=0;
+        for(int i=0;i<n/2;i++){
+            ans+=costs[i][0];
         }
-        return sum;
+        for(int i=n/2;i<n;i++){
+            ans+=costs[i][1];
+        }
+        return ans;
+
+
     }
 }
