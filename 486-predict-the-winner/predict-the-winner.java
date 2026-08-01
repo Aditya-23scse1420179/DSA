25a718a8f4bd0759b1bc036214c576ecb18e598f
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-01 12:45:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (23.56%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,10 +1,13 @@
 class Solution {
+    Integer[][]dp;
     public boolean predictTheWinner(int[] nums) {
+        dp=new Integer[nums.length][nums.length];
         return  helper(nums,0,nums.length-1)>=0;
     }public int helper(int[]nums,int i,int j){
         if(i==j)return nums[i];
+        if(dp[i][j]!=null)return dp[i][j];
         int A=nums[i]-helper(nums,i+1,j);
         int b=nums[j]-helper(nums,i,j-1);
-        return Math.max(A,b);
+        return dp[i][j]=Math.max(A,b);
     }
 }
