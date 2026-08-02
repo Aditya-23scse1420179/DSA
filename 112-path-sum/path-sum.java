74fed994f26e745f6de4f176bed5d86757fc5798
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-02 13:37:36 +0530

    Time: 1 ms (4.73%) | Memory: 45.5 MB (14.72%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -14,10 +14,24 @@
  * }
  */
 class Solution {
+    class pair{
+        TreeNode node;
+        int sum;
+        public pair(TreeNode node,int sum){
+            this.node=node;
+            this.sum=sum;
+        }
+    }
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root==null)return false;
-        targetSum-=root.val;
-        if(root.left==null&&root.right==null)return targetSum==0;
-        return hasPathSum(root.left,targetSum)||hasPathSum(root.right,targetSum);
+        Queue<pair>q=new LinkedList<>();
+        q.offer(new pair(root,root.val));
+        while(!q.isEmpty()){
+            pair curr=q.poll();
+            if(curr.node.left==null&&curr.node.right==null&&curr.sum==targetSum)return true;
+            if(curr.node.left!=null)q.offer(new pair(curr.node.left,curr.sum+curr.node.left.val));
+            if(curr.node.right!=null)q.offer(new pair(curr.node.right,curr.sum+curr.node.right.val));
+        }
+        return false;
     }
 }
