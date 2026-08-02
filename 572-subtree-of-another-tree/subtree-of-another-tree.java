02651fb0db1be55c746041121f2f36dc689305e4
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-02 13:57:46 +0530

    Time: 3 ms (64.37%) | Memory: 46.3 MB (88.29%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null)return false;
+        if(helper(root,subRoot))return true;
+        return isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot);
+    }public boolean helper(TreeNode root,TreeNode sub){
+        if(root==null&&sub==null)return true;
+        if(root==null||sub==null)return false;
+        if(root.val!=sub.val)return false;
+        return helper(root.left,sub.left)&&helper(root.right,sub.right);
+    }
+}
