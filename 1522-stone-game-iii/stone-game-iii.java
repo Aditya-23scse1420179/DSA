5c1dd38beff739f88349b27fac8e36667b075cbd
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-03 12:48:37 +0530

    Time: 10 ms (83.24%) | Memory: 84.9 MB (91.76%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -1,19 +1,17 @@
 class Solution {
-    Integer[]dp;
     public String stoneGameIII(int[] s) {
-        dp=new Integer[s.length];
-        int ans=helper(0,s);
-        if(ans>0)return "Alice";
-        else if(ans<0)return "Bob";
-        else return "Tie";
-    }public int helper(int i,int[]stone){
-        if(i>=stone.length)return 0;
-        int take=0,best=-(int)1e9;
-        if(dp[i]!=null)return dp[i];
-        for(int k=0;k<3&&i+k<stone.length;k++){
-            take+=stone[i+k];
-            best=Math.max(best,take-helper(i+k+1,stone));
+        int[]dp=new int[s.length+1];
+        Arrays.fill(dp,-(int)1e9);
+        dp[s.length]=0;
+        for(int i=s.length-1;i>=0;i--){
+            int take=0;
+            for(int k=0;k<3&&i+k<s.length;k++){
+                take+=s[i+k];
+                dp[i]=Math.max(dp[i],take-dp[i+k+1]);
+            }
         }
-        return dp[i]=best;
+        if(dp[0]>0)return "Alice";
+        else if(dp[0]<0)return "Bob";
+        else return "Tie";
     }
 }
