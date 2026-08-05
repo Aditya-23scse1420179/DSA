06004d2c42338d9ca66c4ee6c0f6f7ff43c02aca
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-05 23:40:08 +0530

    Time: 14 ms (76.75%) | Memory: 59.4 MB (38.38%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -6,9 +6,9 @@ class Solution {
             else {
                 o--;
             }
-            if(o<0){
-                b++;
-                o=1;
+            if(o<0){// Agar balance negative ho gaya, matlab extra ']' aa gaya
+                b++;// 1. Ek swap badha do
+                o=1;// 2. Swap karne ke baad wo ']' ab '[' ban gaya, toh balance 1 kar do
             }
         }
         return b;
