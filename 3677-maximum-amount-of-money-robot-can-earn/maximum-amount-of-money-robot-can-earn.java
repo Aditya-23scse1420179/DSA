3a1bb91724ca650f0728788288c26973544bbdee
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-09 00:41:24 +0530

    Time: 76 ms (85.66%) | Memory: 189.9 MB (80.38%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
@@ -1,23 +1,45 @@
 class Solution {
-    int n,m;
-    Integer[][][]dp;
     public int maximumAmount(int[][] coins) {
-        n=coins.length;
-        m=coins[0].length;
-        dp=new Integer[n][m][3];
-        return helper(n-1,m-1,coins,2);
-    }public int helper(int i,int j,int[][]coins,int k){
-        if(i<0||j<0)return -(int)1e9;
-        if(i==0&&j==0){
-            if(coins[i][j]<0&&k>0)return 0;
-            return coins[i][j];
+        int n=coins.length,m=coins[0].length;
+        int[][][]dp=new int[n][m][3];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                for(int k=0;k<3;k++){
+                    dp[i][j][k]=-(int)1e9;
+                }
+            }
         }
-        if(dp[i][j][k]!=null)return dp[i][j][k];
-        int ntake=coins[i][j]+Math.max(helper(i,j-1,coins,k),helper(i-1,j,coins,k));
-        int take=-(int)1e9;
-        if(coins[i][j]<0&&k>0){
-            take=Math.max(helper(i,j-1,coins,k-1),helper(i-1,j,coins,k-1));
+        for(int k=0;k<3;k++){
+            if(coins[0][0]<0&&k>0)dp[0][0][k]=0;
+            else dp[0][0][k]=coins[0][0];
         }
-        return dp[i][j][k]= Math.max(ntake,take);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0&&j==0)continue;
+                for(int k=0;k<3;k++){
+                    
+                    int ntake=-(int)1e9;
+                    int val=coins[i][j];
+                    if(i>0)ntake=Math.max(ntake,dp[i-1][j][k]);
+                    if(j>0)ntake=Math.max(ntake,dp[i][j-1][k]);
+                    if(ntake>-(int)1e9){
+                        dp[i][j][k]=Math.max(dp[i][j][k],val+ntake);
+                    }
+                    if(val<0&&k>0){
+                        int take=-(int)1e9;
+                        if(i>0)take=Math.max(take,dp[i-1][j][k-1]);
+                        if(j>0)take=Math.max(take,dp[i][j-1][k-1]);
+                        if(take>-(int)1e9){
+                            dp[i][j][k]=Math.max(dp[i][j][k],take);
+                        }
+                    }
+                }
+            }
+        }
+        int ans=-(int)1e9;
+        for(int i=0;i<3;i++){
+            ans=Math.max(ans,dp[n-1][m-1][i]);
+        }
+        return ans;
     }
 }
