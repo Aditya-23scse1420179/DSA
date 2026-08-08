af69d53ca1251cc291fd1c007a9072ce0bec9750
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-09 02:45:06 +0530

    Time: 24 ms (49.51%) | Memory: 46.1 MB (95.74%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -1,28 +1,42 @@
 class Solution {
-    public int[] findEvenNumbers(int[] digits) {
-        int[] count = new int[10];
-        for (int d : digits) count[d]++;
-        List<Integer> result = new ArrayList<>();
-        for (int num = 100; num < 1000; num += 2) {
-            int a = num / 100;
-            int b = (num / 10) % 10;
-            int c = num % 10;
-            if (a == 0) continue;
-            int[] req = new int[10];
-            req[a]++;
-            req[b]++;
-            req[c]++;
-            boolean ok = true;
-            for (int d = 0; d < 10; d++) {
-                if (req[d] > count[d]) {
-                    ok = false;
-                    break;
+    public int[] findEvenNumbers(int[] digit) {
+        int n=digit.length;
+        List<Integer>list=new ArrayList<>();
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int a:digit){
+            map.put(a,map.getOrDefault(a,0)+1);
+        }
+        for(int i=100;i<999;i+=2){
+            int d=i;
+            int c=d%10;
+            d/=10;
+            int b=d%10;
+            d/=10;
+            int a=d;
+            if(map.containsKey(a)){
+                int fa=map.get(a);
+                map.put(a,map.getOrDefault(a,0)-1);
+                if(fa==1)map.remove(a);
+                if(map.containsKey(b)){
+                    int fb=map.get(b);
+                    map.put(b,map.getOrDefault(b,0)-1);
+                    if(fb==1)map.remove(b);
+                    if(map.containsKey(c)){
+                        // int fc=map.get(c);
+                        // map.put(c,map.getOrDefault(c,0)-1);
+                        // if(fc==1)map.remove(c);
+                        list.add(i);
+                    }
+                    map.put(b,fb);
                 }
-            }
-            if (ok) {
-                result.add(num);
+                map.put(a,fa);
             }
         }
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        int[]ans=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            ans[i]=list.get(i);
+        }
+        return ans;
+        
     }
 }
