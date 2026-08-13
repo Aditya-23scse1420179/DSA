33dfbfd6c7473c8b02d319c4a10e8ab793fe0c37
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-13 03:22:29 -0700

    Time: 2 ms (92.03%) | Memory: 47.5 MB (93.82%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,14 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int i=0,j=0;
-        while(i<nums.length&&j<nums.length){
-            if(nums[i]!=0&&nums[j]!=0){
-                i++;
-                j++;
-            }else if(nums[i]==0&&nums[j]!=0){
+        while(j<nums.length){
+            if(nums[j]!=0){
                 int temp=nums[i];
                 nums[i]=nums[j];
                 nums[j]=temp;
+                i++;
+                j++;
+            }else if(nums[i]!=0&&nums[j]==0){
                 i++;
                 j++;
             }else{
