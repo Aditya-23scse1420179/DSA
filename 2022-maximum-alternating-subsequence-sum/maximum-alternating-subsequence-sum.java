37197dd9d8bbfb67f0a17b1565b8020f8bba4c99
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-13 23:22:38 -0700

    Time: 69 ms (18.35%) | Memory: 177.3 MB (5.71%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -1,18 +1,18 @@
 class Solution {
     Long[][]dp;
     public long maxAlternatingSum(int[] nums) {
-        dp=new Long[nums.length+1][2];
-        return helper(nums,0,0);
-    }public long helper(int[]nums,int idx,int isodd){
+        dp=new Long[nums.length][3];
+        return helper(0,nums,0);
+    }public long helper(int idx,int[]nums,int odd){
         if(idx>=nums.length)return 0;
-        if(dp[idx][isodd]!=null)return dp[idx][isodd];
+        if(dp[idx][odd]!=null)return dp[idx][odd];
         long pick;
-        if(isodd==0){
-            pick=nums[idx]+helper(nums,idx+1,1);
+        if(odd==0){
+            pick=nums[idx]+helper(idx+1,nums,1);
         }else{
-            pick=-nums[idx]+helper(nums,idx+1,0);
+            pick=-nums[idx]+helper(idx+1,nums,0);
         }
-        long skip=helper(nums,idx+1,isodd);
-        return dp[idx][isodd]=Math.max(pick,skip);
+        long skip=helper(idx+1,nums,odd);
+        return dp[idx][odd]=Math.max(pick,skip);
     }
 }
