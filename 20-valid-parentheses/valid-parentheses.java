2951544a6bbf079752f7d726572741b32effb836
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-22 15:10:07 +0530

    Time: 3 ms (86.03%) | Memory: 43.2 MB (73.12%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character>st=new Stack<>();
-        for(char ch:s.toCharArray()){
-            if(ch=='('||ch=='{'||ch=='['){
-                st.push(ch);
-            }else{
+        for(char c:s.toCharArray()){
+            char ch=c;
+            if(ch=='('||ch=='{'||ch=='[')st.push(ch);
+            else {
                 if(st.isEmpty())return false;
-                char top=st.pop();
-                if((ch==')'&&top!='(')||
-                (ch=='}'&&top!='{')||
-                (ch==']'&&top!='['))return false;
-
+                char x=st.pop();
+                if(ch==')'&&x!='(')return false;
+                if(ch=='}'&&x!='{')return false;
+                if(ch==']'&&x!='[')return false;
             }
-
         }
         return st.isEmpty();
     }
