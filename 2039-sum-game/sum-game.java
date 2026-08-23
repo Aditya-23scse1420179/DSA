a5045e678a84ccd5fe07ab4aa5b7fc33459ddded
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-23 21:49:56 +0530

    Time: 9 ms (29.93%) | Memory: 47.1 MB (26.28%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n=num.length();
+        int ls=0,lq=0,rs=0,rq=0;
+        for(int i=0;i<n;i++){
+            if(num.charAt(i)=='?'){
+                if(i<n/2)lq++;
+                else rq++;
+            }else{
+                if(i<n/2){
+                ls+=num.charAt(i)-'0';
+                }
+                
+                else
+                rs+=num.charAt(i)-'0';
+            }
+        }
+        if((lq+rq)%2!=0)return true;
+        int left=2*ls+9*lq;
+        int right=2*rs+9*rq;
+        if(left==right)return false;
+        else return true;
+    }
+}
