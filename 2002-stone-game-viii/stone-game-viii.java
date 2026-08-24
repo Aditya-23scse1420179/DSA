05e4675d13e2f9efabe210b451ec3b4bbb2ab46e
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-24 12:28:10 +0530

    Time: 168 ms (5.11%) | Memory: 193.1 MB (5.11%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.java
+++ 2002-stone-game-viii/stone-game-viii.java
@@ -1,9 +1,9 @@
 class Solution {
     int[]pre;
     int[]arr;
-    Integer[]dp;
+    HashMap<String,Integer>map=new HashMap<>();
     public int stoneGameVIII(int[] stones) {
-        dp=new Integer[stones.length];
+        // dp=new Integer[stones.length];
         this.arr=stones;
         pre=new int[stones.length];
         pre[0]=stones[0];
@@ -13,9 +13,13 @@ class Solution {
         return helper(1);
     }public int helper(int i){
         if(i==arr.length-1)return pre[i];
-        if(dp[i]!=null)return dp[i];
+        // if(dp[i]!=null)return dp[i];
+        String key=String.valueOf(i);
+        if(map.containsKey(key))return map.get(key);
         int ntake=helper(i+1);
         int take=pre[i]-helper(i+1);
-        return dp[i]=Math.max(ntake,take);
+        int ans=Math.max(ntake,take);
+        map.put(key,ans);
+        return ans;
     }
 }
