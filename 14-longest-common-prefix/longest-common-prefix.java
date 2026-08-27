8f6fc8de19eae1e368442fc7aaa1bb3a664b6de9
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-08-27 20:39:42 +0530

    Time: 1 ms (60.79%) | Memory: 43.2 MB (50.67%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,13 +1,12 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs== null|| strs.length ==0) return "";
-        String prefix = strs[0];
-        for(int i = 1;i< strs.length;i++){
-            while(!strs[i].startsWith(prefix)){
-                prefix = prefix.substring(0,prefix.length()- 1);
-                // if(prefix.isEmpty()) return "";
-            }
+        Arrays.sort(strs);
+        String s=strs[0];
+        String t=strs[strs.length-1];
+        int i=0;
+        while(i<s.length()&&s.charAt(i)==t.charAt(i)){
+            i++;
         }
-        return prefix;
+        return s.substring(0,i);
     }
 }
