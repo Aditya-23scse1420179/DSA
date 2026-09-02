44b1581119acec8b88075a599778999ea7600eaa
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-03 00:08:19 +0530

    Time: 1 ms (92.38%) | Memory: 78.7 MB (48.43%) - LeetSync

---
+++ 1346-maximize-the-topmost-element-after-k-moves/maximize-the-topmost-element-after-k-moves.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumTop(int[] nums, int k) {
+        if(k==0)return nums[0];
+        if(nums.length==1){
+            return(k%2!=0)?-1:nums[0];
+        }
+        int max=-1;
+        for(int i=0;i<Math.min(nums.length,k-1);i++){
+            max=Math.max(max,nums[i]);
+        }
+        if(k<nums.length)max=Math.max(max,nums[k]);
+        return max;
+    }
+}
