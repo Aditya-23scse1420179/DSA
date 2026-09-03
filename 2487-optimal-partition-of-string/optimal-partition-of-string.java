ceead1a310630b90954c740109af1acb3d23af6c
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-03 14:18:34 +0530

    Time: 34 ms (29.64%) | Memory: 47.1 MB (27.20%) - LeetSync

---
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int partitionString(String s) {
+        Set<Character>set=new HashSet<>();
+        int count=1;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(set.contains(c)){
+                count++;
+                set.clear();
+            }
+            set.add(c);
+        }
+        return count;
+    }
+}
