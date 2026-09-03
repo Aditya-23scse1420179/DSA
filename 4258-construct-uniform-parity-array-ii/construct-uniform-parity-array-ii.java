0d910546fcfb38fe778602e15fc7984ff6c11d18
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-03 13:20:12 +0530

    Time: 34 ms (7.25%) | Memory: 118.4 MB (99.28%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -1,16 +1,13 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n=nums1.length;
-        int c1=0;
-        int c2=0;
-        for(int i=0;i<n;i++){
-            if(nums1[i]%2==0)c1++;
-            else c2++;
+        int e=0,o=0;
+        for(int a:nums1){
+            if(a%2==0)e++;
+            else o++;
         }
-        if(c1==n||c2==n)return true;
+        if(e==nums1.length||o==nums1.length)return true;
         Arrays.sort(nums1);
         if(nums1[0]%2!=0)return true;
         return false;
     }
-
 }
