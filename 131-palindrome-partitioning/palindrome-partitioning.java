29cbe803cfff78a8df30109801e95850b9330afc
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-05 02:52:53 -0700

    Time: 8 ms (74.29%) | Memory: 65.2 MB (58.54%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,27 +1,22 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>>list=new ArrayList<>();
-        helper(s,list,new ArrayList<>(),0);
-        return list;
-    }public void helper(String s,List<List<String>>list,List<String>temp,int start){
-        if(start==s.length()){
-            list.add(new ArrayList<>(temp));
+        List<List<String>>ans=new ArrayList<>();
+        helper(s,ans,new ArrayList<>(),0);
+        return ans;
+    }public void helper(String s,List<List<String>>ans,List<String>list,int idx){
+        if(idx==s.length()){
+            ans.add(new ArrayList<>(list));
         }
-        for(int i=start+1;i<=s.length();i++){
-            if(ispal(s,start,i-1)){
-                temp.add(s.substring(start,i));
-                helper(s,list,temp,i);
-                temp.remove(temp.size()-1);
+        for(int i=idx+1;i<=s.length();i++){
+            if(ispal(s,idx,i-1)){
+                list.add(s.substring(idx,i));
+                helper(s,ans,list,i);
+                list.remove(list.size()-1);
             }
         }
-    }
-    public boolean ispal(String s,int i,int j){
+    }public boolean ispal(String s,int i,int j){
         while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
+            if(s.charAt(i++)!=s.charAt(j--))return false;
         }
         return true;
     }
