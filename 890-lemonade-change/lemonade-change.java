5362afa16a3de6c4cdea71e51348406d0bdc2175
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-06 02:03:00 -0700

    Time: 2 ms (97.81%) | Memory: 72.3 MB (68.90%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,26 +1,22 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
         int f=0,t=0;
-        for(int i=0;i<bills.length;i++){
-            if(bills[i]==5)f++;
-            else if(bills[i]==10){
+        for(int a:bills){
+            if(a==5)f++;
+            if(a==10){
                 if(f>0){
                     f-=1;
                     t++;
-                }else{
-                    return false;
+                }else return false;
+            }
+            if(a==20){
+                if(f>0&&t>0){
+                    t--;
+                    f--;
                 }
-            }else{
-                if(bills[i]==20){
-                    if(t>0&&f>0){
-                        t-=1;
-                        f-=1;
-                    }else if(f>=3){
-                        f-=3;
-                    }else{
-                        return false;
-                    }
-                }
+                else if(f>=3){
+                    f-=3;
+                }else return false;
             }
         }
         return true;
