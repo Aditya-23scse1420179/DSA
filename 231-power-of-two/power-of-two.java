858120b793d68f96aae03d63f10f667754c17835
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-10 23:58:31 +0530

    Time: 1 ms (96.64%) | Memory: 42.7 MB (18.06%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -4,6 +4,6 @@ class Solution {
             return false;
         }
 
-        return (n & (n - 1)) == 0;
+        return (n & (n - 1)) == 0;//2 ki power ke jitne bhi number hain sabke binary me bs ak 1 hota hai aur agar n se -1 lar denge to ulta hoga sab one hoga aur vo ak 1 0 hoga unka and humesa 0 dega 
     }
 }
