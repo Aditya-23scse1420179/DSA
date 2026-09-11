87cd1c8c1991b0baf523875d895d3de9746f81db
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-11 20:00:53 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (8.42%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,20 +1,24 @@
 class Solution {
-    public static int helper(int[]nums,int s,int l){
-        int prev2=nums[s];
-        int prev1=Math.max(nums[s],nums[s+1]);
-        for(int i=s+2;i<l;i++){
-            int curr=Math.max(prev1,nums[i]+prev2);
-            prev2=prev1;
-            prev1=curr;
-        }
-        return prev1;
-    }
     public int rob(int[] nums) {
-        int n=nums.length;
-        if(n==1)return nums[0];
-        if(n==2)return Math.max(nums[0],nums[1]);
-        int ans1 = helper(nums,1,nums.length);
-        int ans2=helper(nums,0,nums.length-1);
-        return Math.max(ans1,ans2);
+        if (nums == null || nums.length == 0) return 0;
+        if(nums.length==1)return nums[0];
+        int[]arr=new int[nums.length-1];
+        for(int i=0;i<nums.length-1;i++){
+            arr[i]=nums[i];
+        }
+        int[]ar=new int[nums.length-1];
+        int id=0;
+        for(int i=1;i<nums.length;i++){
+            ar[id++]=nums[i];
+        }
+        Integer[]dp1=new Integer[arr.length];
+        Integer[]dp2=new Integer[ar.length];
+        return Math.max(helper(0,arr,dp1),helper(0,ar,dp2));
+    }public int helper(int idx,int[]nums,Integer[]dp){
+        if(idx>=nums.length)return 0;
+        if(dp[idx]!=null)return dp[idx];
+        int take=nums[idx]+helper(idx+2,nums,dp);
+        int take2=helper(idx+1,nums,dp);
+        return dp[idx]=Math.max(take,take2);
     }
 }
