c1c54f4a383e6df4ced83c69be3c4e6d5b563e5a
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-11 19:47:43 +0530

    Time: 101 ms (39.89%) | Memory: 47.2 MB (7.42%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,17 +1,16 @@
 class Solution {
-    int[]nums;
-    Map<String,Integer>dp=new HashMap<>();
+    HashMap<String,Integer>map=new HashMap<>();
     public int findTargetSumWays(int[] nums, int target) {
-        this.nums=nums;
-        // dp=new Integer[nums.length+1][target+1];
-        return helper(0,target,0);
-    }public int helper(int idx,int target,int sum){
-        if(idx==nums.length)return target==sum?1:0;
-        String key=idx+","+sum;
-        if(dp.containsKey(key))return dp.get(key);
-        int take1=helper(idx+1,target,sum+nums[idx]);
-        int take2=helper(idx+1,target,sum-nums[idx]);
-        dp.put(key,take1+take2);
-        return take1+take2;
+        return helper(0,nums,target,0);
+    }public int helper(int idx,int[]nums,int target,int sum){
+        if(idx>=nums.length){
+            return sum==target?1:0;
+        }
+        String key=idx+" "+sum;
+        if(map.containsKey(key))return map.get(key);
+        int t1=helper(idx+1,nums,target,sum+nums[idx]);
+        int t2=helper(idx+1,nums,target,sum-nums[idx]);
+        map.put(key,t1+t2);
+        return t1+t2;
     }
 }
