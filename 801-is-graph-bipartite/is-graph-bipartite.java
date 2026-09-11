982e2c7bfcbdf72385e10925bcf8de22ddf43655
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-11 19:38:38 +0530

    Time: 2 ms (38.75%) | Memory: 47.3 MB (19.56%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n=graph.length;
-        int []color=new int[n];
+        int[]col=new int[n];
         for(int i=0;i<n;i++){
-            if(color[i]==0){//if uncolored then proceed
+            if(col[i]==0){
                 Queue<Integer>q=new LinkedList<>();
+                col[i]=1;
                 q.offer(i);
-                color[i]=1;
                 while(!q.isEmpty()){
-                    int node=q.remove();
+                    int node=q.poll();
                     for(int a:graph[node]){
-                        if(color[a]==0){//check for color 
-                            color[a]=-color[node];//reverse of color 
+                        if(col[a]==0){
+                            col[a]-=col[node];
                             q.offer(a);
-                        }else if(color[a]==color[node])return false;//same color 
+                        }else if(col[a]==col[node])return false;
                     }
                 }
             }
