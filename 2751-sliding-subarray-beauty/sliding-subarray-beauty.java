c138d58fcbffb5101d25955f97ff6c7ec8f22cac
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-14 16:27:02 +0530

    Time: 33 ms (48.45%) | Memory: 184.4 MB (53.53%) - LeetSync

--- 2751-sliding-subarray-beauty/sliding-subarray-beauty.java
+++ 2751-sliding-subarray-beauty/sliding-subarray-beauty.java
@@ -1,29 +1,28 @@
 class Solution {
     public int[] getSubarrayBeauty(int[] nums, int k, int x) {
-        int n = nums.length;
-        int[] arr = new int[n-k+1];
-        int[] freq = new int[101];
-        for (int i=0;i<k;i++){
+        int n=nums.length;
+        int[]arr=new int[n-k+1];
+        int[]freq=new int[101];
+        for(int i=0;i<k;i++){
             freq[nums[i]+50]++;
         }
         int id=0;
-        arr[id++] =helper(freq,x);
+        arr[id++]=helper(freq,x);
         int i=1,j=k;
         while(j<n){
-            freq[nums[j]+50]++;   
-            freq[nums[i-1]+50]--; 
-            arr[id++]=helper(freq, x);
+            freq[nums[j]+50]++;
+            freq[nums[i-1]+50]--;
+            arr[id++]=helper(freq,x);
             i++;
             j++;
         }
         return arr;
-    }
-    public int helper(int[]freq,int x) {
+    }public int helper(int[]freq,int x){
         int count=0;
-        for(int idx=0;idx<50;idx++){
-            count+=freq[idx];
-            if(count>=x) {//loop sirf -ve pe chalega aur josa chahiye hoga utna count karke ruk jayega 
-                return idx-50;
+        for(int i=0;i<50;i++){
+            count+=freq[i];
+            if(count>=x){
+                return i-50;
             }
         }
         return 0;
