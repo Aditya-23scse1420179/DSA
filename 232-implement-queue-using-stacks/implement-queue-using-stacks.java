ea4ee0402daf33fab4b363b4c9dc592f1224383c
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-15 22:39:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (69.59%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,36 +1,35 @@
 class MyQueue {
-    Stack <Integer> st1 ;
-    Stack <Integer> st2 ;
-
+    Stack<Integer>s1;
+    Stack<Integer>s2;
     public MyQueue() {
-        st1=new Stack<>();
-        st2=new Stack<>();
+        s1=new Stack<>();
+        s2=new Stack<>();
     }
     
     public void push(int x) {
-        st1.push(x);
+        s1.push(x);
     }
     
     public int pop() {
-        if(st2.isEmpty()){
-            while(!st1.isEmpty()){
-                st2.push(st1.pop());
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
             }
         }
-        return st2.pop();
+        return s2.pop();
     }
     
     public int peek() {
-        if(st2.isEmpty()){
-            while(!st1.isEmpty()){
-                st2.push(st1.pop());
+         if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
             }
         }
-        return st2.peek();
+        return s2.peek();
     }
     
     public boolean empty() {
-        return st1.size()==0 && st2.size()==0;
+        return s1.isEmpty()&&s2.isEmpty();
     }
 }
 
