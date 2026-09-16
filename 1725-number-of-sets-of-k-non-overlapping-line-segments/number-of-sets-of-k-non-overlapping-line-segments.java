302b4edd3c119404dced8cd0a0661ac70110ee01
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-16 12:42:57 +0530

    Time: 2895 ms (5.16%) | Memory: 63 MB (66.45%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -9,7 +9,7 @@ class Solution {
         if(dp[i][k]!=null)return dp[i][k];
         long skip=helper(i+1,k,n);
         long take=0L;
-        for(int j=i+1;j<=n-1;j++){
+        for(int j=i+1;j<=n-1;j++){//end points 
             take+=helper(j,k-1,n);
         }
         return dp[i][k]=(int)((skip+take)%1000000007);
