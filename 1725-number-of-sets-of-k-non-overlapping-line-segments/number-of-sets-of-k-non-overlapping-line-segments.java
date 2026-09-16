0558b95e2aab2686fda50ae46052b6330c7223ce
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-16 12:40:55 +0530

    Time: 2739 ms (5.16%) | Memory: 63.2 MB (63.87%) - LeetSync

---
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -0,0 +1,17 @@
+class Solution {
+    Integer[][]dp;
+    public int numberOfSets(int n, int k) {
+        dp=new Integer[n+1][k+1];
+        return helper(0,k,n); 
+    }public int helper(int i,int k,int n){
+        if(k==0)return 1;
+        if(i>=n)return 0;
+        if(dp[i][k]!=null)return dp[i][k];
+        long skip=helper(i+1,k,n);
+        long take=0L;
+        for(int j=i+1;j<=n-1;j++){
+            take+=helper(j,k-1,n);
+        }
+        return dp[i][k]=(int)((skip+take)%1000000007);
+    }
+}
