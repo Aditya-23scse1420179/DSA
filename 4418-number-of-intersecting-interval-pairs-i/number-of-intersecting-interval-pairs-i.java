a186c4e821d414a089f33d1221cc7940f5256888
Aditya Pratap Singh <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-09-20 21:53:45 +0530

    Time: 4 ms (99.92%) | Memory: 47 MB (40.57%) - LeetSync

--- 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.java
+++ 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.java
@@ -4,7 +4,6 @@ class Solution {
         for (int i = 0; i < intervals.length; i++) {
             int start1 = intervals[i][0];
             int end1 = intervals[i][1];
-            
             for (int j = i + 1; j < intervals.length; j++) {
                 int start2 = intervals[j][0];
                 int end2 = intervals[j][1];
